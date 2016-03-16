f804601d02f26c8d234a0ae53ec3493ac22d6382
Greg Magarshak <greg@qbix.com>
2016-03-16 01:06:29 -0400

    Fixed options in Q/columns tool

--- platform/plugins/Q/handlers/Q/columns/tool.php
+++ platform/plugins/Q/handlers/Q/columns/tool.php
@@ -42,8 +42,8 @@ function Q_columns_tool($options)
 	$result = '<div class="Q_columns_container Q_clearfix">';
 	$columns = array();
 	$i=0;
-	$closeSrc = Q::ifset($options, 'close', 'src', 'plugins/Q/img/x.png"');
-	$backSrc = Q::ifset($options, 'back', 'src', 'plugins/Q/img/back-v.png"');
+	$closeSrc = Q::ifset($options, 'close', 'src', 'plugins/Q/img/x.png');
+	$backSrc = Q::ifset($options, 'back', 'src', 'plugins/Q/img/back-v.png');
 	foreach ($options['columns'] as $name => $column) {
 		$close = Q::ifset($column, 'close', $i > 0);
 		$Q_close = Q_Request::isMobile() ? 'Q_close' : 'Q_close Q_back';
