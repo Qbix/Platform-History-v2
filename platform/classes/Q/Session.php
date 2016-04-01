51fc945b36f21c728598d9e0fdc05d143f8797eb
Greg Magarshak <greg@qbix.com>
2016-04-01 15:11:18 -0400

    Fixed setting of session id properly after refactoring

--- platform/classes/Q/Session.php
+++ platform/classes/Q/Session.php
@@ -271,7 +271,7 @@ class Q_Session
 						self::$session_db_row = $row;
 					} else {
 						// Start a new session with our own id
-						$row->$id_field = self::generateId();
+						$id = $row->$id_field = self::generateId();
 						$row->$data_field = "";
 						$row->$updated_field = date('Y-m-d H:i:s');
 						$row->$duration_field = Q_Config::get(
@@ -294,9 +294,8 @@ class Q_Session
 							self::$session_db_row = $row;
 						}
 					}
-				} else {
-					self::id($id);
 				}
+				self::id($id);
 			}
 			if (!empty($_SERVER['HTTP_HOST'])) {
 				$durationName = self::durationName();
