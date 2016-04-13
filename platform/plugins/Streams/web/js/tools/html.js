514bd5c3887732ddf64f9e86e2f05c6e70ef41f4
Greg Magarshak <greg@qbix.com>
2016-04-13 16:59:50 -0400

    Adjusted default froala buttons for Streams/html tool editor

--- platform/plugins/Streams/web/js/tools/html.js
+++ platform/plugins/Streams/web/js/tools/html.js
@@ -241,10 +241,11 @@ Q.Tool.define("Streams/html", function (options) {
 		imagePaste: true,
 		toolbarButtons: [
 			"bold", "italic", "underline", "strikeThrough", "subscript", "superscript", "|",
-			"fontFamily", "fontSize", "color", "|",
+			"fontFamily", "fontSize", "color", "-",
 			"inlineStyle", "paragraphStyle", "paragraphFormat", "|",
 			"align", "formatOL", "formatUL", "|",
-			"outdent", "indent", "quote", "insertHR", "createLink", "|",
+			"outdent", "indent", "quote", "-", 
+			"insertHR", "createLink", "|",
 			"insertImage", "insertTable", "|", "html"
 		],
 		fontFamily: {
