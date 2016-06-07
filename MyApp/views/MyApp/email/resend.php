e1ae6ed43c496c3a3568161be6e577b2cb40d82a
Greg Magarshak <greg@qbix.com>
2016-06-07 18:35:15 -0400

    Improved transactional email copy

--- MyApp/views/MyApp/email/resend.php
+++ MyApp/views/MyApp/email/resend.php
@@ -6,15 +6,11 @@
 	You are receiving this email because someone entered your email address
 	and clicked "forgot passphrase". If it wasn't you, simply ignore this message.
 	
- 	To reset your passphrase, click <?php echo Q_Html::a(
-		'Users/activate?p=1&code='.urlencode($email->activationCode)
-		 . ' emailAddress='.urlencode($email->address),
-		'here'
-	) ?>.
+ 	To reset your passphrase, click <?php echo Q_Html::a($link, 'here') ?>.
 </p>
 
 <p>
-	See you on <a href="<?php echo Q_Request::baseUrl() ?>"><?php echo $communityName ?></a>!
+	See you on <a href="<?php echo Q_Request::baseUrl() ?>"><?php echo Q_Html::text($communityName) ?></a>!
 </p>
 
 <script type="application/ld+json">
