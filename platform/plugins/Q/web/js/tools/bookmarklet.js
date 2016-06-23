06281388107acd58177c9f120b96a9f9a02e560d
Greg Magarshak <greg@qbix.com>
2016-06-23 03:40:10 +0100

    Updated Q/bookmarklet code generation

--- platform/plugins/Q/web/js/tools/bookmarklet.js
+++ platform/plugins/Q/web/js/tools/bookmarklet.js
@@ -89,186 +89,27 @@ Q.Tool.jQuery('Q/bookmarklet', function (o) {
 		}
 	});
 	
-		var $this = $(this);
-		
-		var browser = Q.Browser.detect();
-
-		$this.addClass('Q_clearfix');
-		switch (browser.OS) {
-			case 'android':
-				$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
-									'<div class="Q_bookmarklet_tool_step">' +
-										'<h3>Step 1: Select the text and copy it.</h3>' +
-										'<textarea class="Q_bookmarklet_tool_code">' +
-											o.code +
-										'</textarea>' +
-										'<ul>' +
-											'<li>Tap inside.</li>' +
-											'<li>Tap and hold for a bit, then release.</li>' +
-											'<li>Tap <b>Select All</b>.</li>' +
-											'<li>Tap <b>Copy</b>.</li>' + 
-											'<li>Tap <b>Done</b>.</li>' + 
-										'</ul>' +
-									'</div>' +
-									'<div class="Q_bookmarklet_tool_step">' +
-										'<h3>Step 2: Bookmark this page.</h3>' +
-										'<ol>' +
-											'<li>Tap the bookmark icon next to the address bar, then tap &#9733; Add.</li>' +
-											'<li>Tap the Location. Then, tap and hold to bring up the contextual menu, and select <b>Paste</b>.</li>' +
-											'<li> Save the changes by tapping <b>Done</b> and <b>OK</b>.</li>' +
-										'</ol>' +
-									'</div>' +
-									'<div class="Q_bookmarklet_tool_step">' +
-										'<h3>Step 4: Installation complete.</h3>' +
-										'<p>Installation should be complete!</p>' +
-										'<p>Tap the bookmark icon next to the address bar, then tap "'+o.title+'" to use it on any page.</p>' +
-									'</div>' +
-								'</div>');
-				break;
-				
-			case 'ios':
-				var url = Q.info.browser.mainVersion >= 8
-					? 'plugins/Q/img/bookmarklet/ios8_action.png'
-					: 'plugins/Q/img/bookmarklet/ios_action.png';
-				var icon = '<img src="'+Q.url(url)+'" class="Q_bookmarklet_ios_action_icon" />';
-				if (browser.device === 'iPad') {
-					$this.addClass('Q_bookmarklet_tool_iPad');
-					$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 1: Bookmark this page.</h3>' +
-											'<p>Tap the '+icon+' icon, then tap Add Bookmark, then tap Save.</p>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 2: Select the text and copy it.</h3>' +
-											'<textarea class="Q_bookmarklet_tool_code">' +
-												o.code +
-											'</textarea>' +
-											'<ul>' +
-												'<li>Tap inside.</li>' +
-												'<li>Tap and hold for a bit, then release.</li>' +
-												'<li>Tap <b>Select All</b>.</li>' +
-												'<li>Tap <b>Copy</b>.</li>' + 
-												'<li>Tap <b>Done</b>.</li>' + 
-											'</ul>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 3: Edit the bookmark.</h3>' +
-											'<ol>' +
-												'<li>Tap the Bookmarks button in the toolbar.</li>' +
-												'<li>Tap <b>Edit</b>. Select the "'+o.title+'" bookmark to edit.</li>' +
-												'<li>Tap its URL, tap the <b>x</b> to clear it, tap-and-hold for the magnifying glass, then tap <b>Paste</b>.</li>' +
-												'<li> Save the changes by tapping <b>Done</b>.</li>' +
-											'</ol>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 4: Installation complete.</h3>' +
-											'<p>Installation should be complete!</p>' +
-											'<p>Select the "'+o.title+'" bookmark from your Bookmarks list to use it on any page.</p>' +
-										'</div>' +
-									'</div>');
-				} else {
-					$this.addClass('Q_bookmarklet_tool_iPhone');
-					$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 1: Bookmark this page.</h3>' +
-											'<p>Tap the '+icon+' icon below, then tap Add Bookmark, then tap Save.</p>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 2: Select the text and copy it.</h3>' +
-											'<textarea class="Q_bookmarklet_tool_code">' +
-												o.code +
-											'</textarea>' +
-											'<ul>' +
-												'<li>Tap inside.</li>' +
-												'<li>Tap and hold for a bit, then release.</li>' +
-												'<li>Tap <b>Select All</b>.</li>' +
-												'<li>Tap <b>Copy</b>.</li>' + 
-												'<li>Tap <b>Done</b>.</li>' + 
-											'</ul>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 3: Edit the bookmark.</h3>' +
-											'<ol>' +
-												'<li>Tap the Bookmarks button in the toolbar.</li>' +
-												'<li>Tap <b>Edit</b>. Select the "'+o.title+'" bookmark to edit.</li>' +
-												'<li>Tap its URL, tap the <b>x</b> to clear it, tap-and-hold for the magnifying glass, then tap <b>Paste</b>.</li>' +
-												'<li> Save the changes by tapping <b>Done</b>.</li>' +
-											'</ol>' +
-										'</div>' +
-										'<div class="Q_bookmarklet_tool_step">' +
-											'<h3>Step 4: Installation complete.</h3>' +
-											'<p>Installation should be complete!</p>' +
-											'<p>Select the "'+o.title+'" bookmark from your Bookmarks list to use it on any page.</p>' +
-										'</div>' +
-									'</div>');
+	var code = null;
+	if (o.scripts && o.scripts.length) {
+		var scripts = [];
+		for (var i=0; i<o.scripts.length; ++i) {
+			var orig = o.scripts[i];
+			var url = Q.url(orig);
+			scripts.push(url);
+			if (o.skip && o.skip[orig]) {
+				o.skip[url] = o.skip[orig];
+				if (url !== orig) {
+					delete o.skip[orig];
 				}
-				break;
-			default:
-				$this.append('<div class="Q_bookmarklet_tool_instructions">' + 
-								 '<div class="Q_bookmarklet_tool_drag_button">' +
-									 '<div class="Q_bookmarklet_tool_sample_button_tip">Drag me to your<br />Bookmarks Bar</div><br />' +
-									 '<div class="Q_bookmarklet_tool_sample_button Q_bookmarklet_tool_button_' + browser.name + '_' + browser.OS + '">' +
-											 '<div class="Q_bookmarklet_tool_button_left"></div>' +
-											 '<div class="Q_bookmarklet_tool_button_middle">' +
-												 '<a href="#">' +
-													 (bookmarkletSettings[browser.name][browser.OS]['icon'] ?
-														'<img src="' +
-														(o.icon ? o.icon : Q.info.proxyBaseUrl + '/plugins/Q/img/bookmarklet/' + bookmarkletSettings[browser.name][browser.OS]['icon']) +
-														'" alt="" />'
-														: '') +
-													 o.title +
-												 '</a>' +
-											 '</div>' +
-											 '<div class="Q_bookmarklet_tool_button_right"></div>' +
-									 '</div>' +
-								 '</div>' +
-								 '<div class="Q_bookmarklet_tool_instruction_text">' +
-									 bookmarkletSettings[browser.name][browser.OS]['instructions'] +
-								 '</div>' +
-							 '</div>');
-	$this.append('<div class="Q_bookmarklet_tool_bookmarks_bar_sample">' +
-								 '<div class="Q_bookmarklet_tool_bar_screenshot Q_bookmarklet_tool_bar_screenshot_' + browser.name + '_' + browser.OS + '"">' +
-									 '<div class="Q_bookmarklet_tool_sample_button Q_bookmarklet_tool_button_' + browser.name + '_' + browser.OS + '">' +
-										 '<div class="Q_bookmarklet_tool_button_left"></div>' +
-										 '<div class="Q_bookmarklet_tool_button_middle">' +
-											 '<a href="#">' +
-												 (bookmarkletSettings[browser.name][browser.OS]['icon'] ?
-													'<img src="' +
-													(o.icon ? o.icon : Q.info.proxyBaseUrl + '/plugins/Q/img/bookmarklet/' + bookmarkletSettings[browser.name][browser.OS]['icon']) +
-													'" alt="" />'
-													: '') +
-												 o.title +
-											 '</a>' +
-										 '</div>' +
-										 '<div class="Q_bookmarklet_tool_button_right"></div>' +
-									 '</div>' +
-								 '</div>' +
-								 '<div class="Q_bookmarklet_tool_bookmarks_bar_description">' +
-									 'After you drag the button to the Bookmarks Bar, it will look like this.' +
-								 '</div>' +
-							 '</div>');
-			var $a = $this.find('.Q_bookmarklet_tool_button_middle a');
-			var code = null;
-			if (o.scripts && o.scripts.length) {
-				var scripts = [];
-				for (var i=0; i<o.scripts.length; ++i) {
-					var orig = o.scripts[i];
-					var url = Q.url(orig);
-					scripts.push(url);
-					if (o.skip && o.skip[orig]) {
-						o.skip[url] = o.skip[orig];
-						if (url !== orig) {
-							delete o.skip[orig];
-						}
-					}
-				}
-				var json = JSON.stringify({
-					scripts: scripts,
-					skip: o.skip,
-					code: o.code
-				});
-				var baseUrlJson = JSON.stringify(Q.info.baseUrl);
-				code =
+			}
+		}
+		var json = JSON.stringify({
+			scripts: scripts,
+			skip: o.skip,
+			code: o.code
+		});
+		var baseUrlJson = JSON.stringify(Q.info.baseUrl);
+		code =
   '(function () {'
 + ' var o = ' + json + ';'
 + '	var i=-1, loaded = {};'
@@ -320,22 +161,182 @@ Q.Tool.jQuery('Q/bookmarklet', function (o) {
 + '	}'
 + '	loadNextScript();'
 + '})();';
-			} else {
-				code = o.code;
-			}
-			// NOTE: code should be under 2000 total characters
-			// see http://stackoverflow.com/a/417184/467460
-			code = 'javascript:'+encodeURIComponent(code.replaceAll({
-				'\n': ' ',
-				'    ': ' ',
-				'  ': ' '
-			}));
-			$a.attr('href', code);
-			$a.eq(0).on('click.Q_bookmarklet', function() {
-				alert(o.clickPrompt);
-				return false;
-			});
+	} else {
+		code = o.code;
+	}
+	// NOTE: code should be under 2000 total characters
+	// see http://stackoverflow.com/a/417184/467460
+	code = 'javascript:'+encodeURIComponent(code.replaceAll({
+		'\n': ' ',
+		'    ': ' ',
+		'  ': ' '
+	}));
+
+	var $this = $(this);
+	
+	var browser = Q.Browser.detect();
+
+	$this.addClass('Q_clearfix');
+	switch (browser.OS) {
+	case 'android':
+		$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
+							'<div class="Q_bookmarklet_tool_step">' +
+								'<h3>Step 1: Select the text and copy it.</h3>' +
+								'<textarea class="Q_bookmarklet_tool_code">' +
+									code +
+								'</textarea>' +
+								'<ul>' +
+									'<li>Tap inside.</li>' +
+									'<li>Tap and hold for a bit, then release.</li>' +
+									'<li>Tap <b>Select All</b>.</li>' +
+									'<li>Tap <b>Copy</b>.</li>' + 
+									'<li>Tap <b>Done</b>.</li>' + 
+								'</ul>' +
+							'</div>' +
+							'<div class="Q_bookmarklet_tool_step">' +
+								'<h3>Step 2: Bookmark this page.</h3>' +
+								'<ol>' +
+									'<li>Tap the bookmark icon next to the address bar, then tap &#9733; Add.</li>' +
+									'<li>Tap the Location. Then, tap and hold to bring up the contextual menu, and select <b>Paste</b>.</li>' +
+									'<li> Save the changes by tapping <b>Done</b> and <b>OK</b>.</li>' +
+								'</ol>' +
+							'</div>' +
+							'<div class="Q_bookmarklet_tool_step">' +
+								'<h3>Step 4: Installation complete.</h3>' +
+								'<p>Installation should be complete!</p>' +
+								'<p>Tap the bookmark icon next to the address bar, then tap "'+o.title+'" to use it on any page.</p>' +
+							'</div>' +
+						'</div>');
+		break;
+		
+	case 'ios':
+		var url = Q.info.browser.mainVersion >= 8
+			? 'plugins/Q/img/bookmarklet/ios8_action.png'
+			: 'plugins/Q/img/bookmarklet/ios_action.png';
+		var icon = '<img src="'+Q.url(url)+'" class="Q_bookmarklet_ios_action_icon" />';
+		if (browser.device === 'iPad') {
+			$this.addClass('Q_bookmarklet_tool_iPad');
+			$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 1: Bookmark this page.</h3>' +
+									'<p>Tap the '+icon+' icon, then tap Add Bookmark, then tap Save.</p>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 2: Select the text and copy it.</h3>' +
+									'<textarea class="Q_bookmarklet_tool_code">' +
+										code +
+									'</textarea>' +
+									'<ul>' +
+										'<li>Tap inside.</li>' +
+										'<li>Tap and hold for a bit, then release.</li>' +
+										'<li>Tap <b>Select All</b>.</li>' +
+										'<li>Tap <b>Copy</b>.</li>' + 
+										'<li>Tap <b>Done</b>.</li>' + 
+									'</ul>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 3: Edit the bookmark.</h3>' +
+									'<ol>' +
+										'<li>Tap the Bookmarks button in the toolbar.</li>' +
+										'<li>Tap <b>Edit</b>. Select the "'+o.title+'" bookmark to edit.</li>' +
+										'<li>Tap its URL, tap the <b>x</b> to clear it, tap-and-hold for the magnifying glass, then tap <b>Paste</b>.</li>' +
+										'<li> Save the changes by tapping <b>Done</b>.</li>' +
+									'</ol>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 4: Installation complete.</h3>' +
+									'<p>Installation should be complete!</p>' +
+									'<p>Select the "'+o.title+'" bookmark from your Bookmarks list to use it on any page.</p>' +
+								'</div>' +
+							'</div>');
+		} else {
+			$this.addClass('Q_bookmarklet_tool_iPhone');
+			$this.append( '<div class="Q_bookmarklet_tool_instructions">' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 1: Bookmark this page.</h3>' +
+									'<p>Tap the '+icon+' icon below, then tap Add Bookmark, then tap Save.</p>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 2: Select the text and copy it.</h3>' +
+									'<textarea class="Q_bookmarklet_tool_code">' +
+										code +
+									'</textarea>' +
+									'<ul>' +
+										'<li>Tap inside.</li>' +
+										'<li>Tap and hold for a bit, then release.</li>' +
+										'<li>Tap <b>Select All</b>.</li>' +
+										'<li>Tap <b>Copy</b>.</li>' + 
+										'<li>Tap <b>Done</b>.</li>' + 
+									'</ul>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 3: Edit the bookmark.</h3>' +
+									'<ol>' +
+										'<li>Tap the Bookmarks button in the toolbar.</li>' +
+										'<li>Tap <b>Edit</b>. Select the "'+o.title+'" bookmark to edit.</li>' +
+										'<li>Tap its URL, tap the <b>x</b> to clear it, tap-and-hold for the magnifying glass, then tap <b>Paste</b>.</li>' +
+										'<li> Save the changes by tapping <b>Done</b>.</li>' +
+									'</ol>' +
+								'</div>' +
+								'<div class="Q_bookmarklet_tool_step">' +
+									'<h3>Step 4: Installation complete.</h3>' +
+									'<p>Installation should be complete!</p>' +
+									'<p>Select the "'+o.title+'" bookmark from your Bookmarks list to use it on any page.</p>' +
+								'</div>' +
+							'</div>');
 		}
+		break;
+	default:
+		$this.append('<div class="Q_bookmarklet_tool_instructions">' + 
+						 '<div class="Q_bookmarklet_tool_drag_button">' +
+							 '<div class="Q_bookmarklet_tool_sample_button_tip">Drag me to your<br />Bookmarks Bar</div><br />' +
+							 '<div class="Q_bookmarklet_tool_sample_button Q_bookmarklet_tool_button_' + browser.name + '_' + browser.OS + '">' +
+									 '<div class="Q_bookmarklet_tool_button_left"></div>' +
+									 '<div class="Q_bookmarklet_tool_button_middle">' +
+										 '<a href="#">' +
+											 (bookmarkletSettings[browser.name][browser.OS]['icon'] ?
+												'<img src="' +
+												(o.icon ? o.icon : Q.info.proxyBaseUrl + '/plugins/Q/img/bookmarklet/' + bookmarkletSettings[browser.name][browser.OS]['icon']) +
+												'" alt="" />'
+												: '') +
+											 o.title +
+										 '</a>' +
+									 '</div>' +
+									 '<div class="Q_bookmarklet_tool_button_right"></div>' +
+							 '</div>' +
+						 '</div>' +
+						 '<div class="Q_bookmarklet_tool_instruction_text">' +
+							 bookmarkletSettings[browser.name][browser.OS]['instructions'] +
+						 '</div>' +
+					 '</div>');
+$this.append('<div class="Q_bookmarklet_tool_bookmarks_bar_sample">' +
+						 '<div class="Q_bookmarklet_tool_bar_screenshot Q_bookmarklet_tool_bar_screenshot_' + browser.name + '_' + browser.OS + '"">' +
+							 '<div class="Q_bookmarklet_tool_sample_button Q_bookmarklet_tool_button_' + browser.name + '_' + browser.OS + '">' +
+								 '<div class="Q_bookmarklet_tool_button_left"></div>' +
+								 '<div class="Q_bookmarklet_tool_button_middle">' +
+									 '<a href="#">' +
+										 (bookmarkletSettings[browser.name][browser.OS]['icon'] ?
+											'<img src="' +
+											(o.icon ? o.icon : Q.info.proxyBaseUrl + '/plugins/Q/img/bookmarklet/' + bookmarkletSettings[browser.name][browser.OS]['icon']) +
+											'" alt="" />'
+											: '') +
+										 o.title +
+									 '</a>' +
+								 '</div>' +
+								 '<div class="Q_bookmarklet_tool_button_right"></div>' +
+							 '</div>' +
+						 '</div>' +
+						 '<div class="Q_bookmarklet_tool_bookmarks_bar_description">' +
+							 'After you drag the button to the Bookmarks Bar, it will look like this.' +
+						 '</div>' +
+					 '</div>');
+		var $a = $this.find('.Q_bookmarklet_tool_button_middle a');
+		$a.attr('href', code);
+		$a.eq(0).on('click.Q_bookmarklet', function() {
+			alert(o.clickPrompt);
+			return false;
+		});
+	}
 },
 
 {
