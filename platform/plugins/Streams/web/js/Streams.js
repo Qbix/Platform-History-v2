349d43bc66c8396df76c77d2576df70081c16251
Greg Magarshak <greg@qbix.com>
2016-09-21 12:30:05 -0400

    Started doing refreshUnlessSocket after relating

--- platform/plugins/Streams/web/js/Streams.js
+++ platform/plugins/Streams/web/js/Streams.js
@@ -627,6 +627,10 @@ Streams.create = function (fields, callback, related, options) {
 			callback && callback.call(stream, null, stream, extra, data.slots);
 			// process various messages posted to Streams/participating
 			_refreshUnlessSocket(Users.loggedInUserId(), 'Streams/participating');
+			if (related) {
+				// process possible relatedTo messages posted
+				_refreshUnlessSocket(related.publisherId, related.streamName);
+			}
 			return;
 		}, true);
 	}, { 
@@ -2299,140 +2303,9 @@ Streams.relate = function _Streams_relate (publisherId, streamName, relationType
 		streamName: streamName
 	});
 	Q.req('Streams/related', [slotName], function (err, data) {
-		var messageFrom = Q.getObject('slots.result.messageFrom', data);
-		var messageTo = Q.getObject('slots.result.messageTo', data);
-		// wait for messages from cached streams -- from, to or both!
 		callback && callback.call(this, err, Q.getObject('slots.result', data) || null);
-	}, { method: 'post', fields: fields, baseUrl: baseUrl });
-	_retain = undefined;
-};
-
-/**
- * Removes relations from streams to one another
- * @static
- * @method unrelate
- * @param {String} publisherId the publisher id of the stream to relate to
- * @param {String} streamName the name of the stream to relate to
- * @param {String} relationType the type of the relation, such as "parent" or "photo"
- * @param {String} fromPublisherId the publisher id of the stream to relate from
- * @param {String} fromStreamName the name of the stream to relate from
- * @param {Function} [callback] callback to call with the results
- *  First parameter is the error, the second will be relations data
- */
-Streams.unrelate = function _Stream_prototype_unrelate (publisherId, streamName, relationType, fromPublisherId, fromStreamName, callback) {
-	if (!Q.plugins.Users.loggedInUser) {
-		throw new Error("Streams.unrelate: Not logged in.");
-	}
-	var slotName = "result";
-	var fields = {
-		"toPublisherId": publisherId,
-		"toStreamName": streamName,
-		"type": relationType,
-		"fromPublisherId": fromPublisherId,
-		"fromStreamName": fromStreamName,
-		"Q.clientId": Q.clientId()
-	};
-	// TODO: When we refactor Streams to support multiple hosts,
-	// the client will have to post this request to both hosts if they are different
-	// or servers will have tell each other on their own
-	var baseUrl = Q.baseUrl({
-		publisherId: publisherId,
-		streamName: streamName
-	});
-	Q.req('Streams/related', [slotName], function (err, data) {
-		callback && callback.call(this, err, Q.getObject('slots.result', data) || null);
-	}, { method: 'delete', fields: fields, baseUrl: baseUrl });
-	_retain = undefined;
-};
-
-/**
- * Later we will probably make Streams.Relation objects which will provide easier access to this functionality.
- * For now, use this to update weights of relations, etc.
- * 
- * @method updateRelation
- * @param {String} toPublisherId
- * @param {String} toStreamName
- * @param {String} relationType
- * @param {String} fromPublisherId
- * @param {String} fromStreamName
- * @param {Number} weight
- * @param {Boolean} adjustWeights
- * @param {Function} callback
- */
-Streams.updateRelation = function(
-	toPublisherId,
-	toStreamName,
-	relationType,
-	fromPublisherId,
-	fromStreamName,
-	weight,
-	adjustWeights,
-	callback
-) {
-	if (!Q.plugins.Users.loggedInUser) {
-		throw new Error("Streams.relate: Not logged in.");
-	}
-	// We will send a request to wherever (toPublisherId, toStreamName) is hosted
-	var slotName = "result";
-	var fields = {
-		"toPublisherId": toPublisherId,
-		"toStreamName": toStreamName,
-		"type": relationType,
-		"fromPublisherId": fromPublisherId,
-		"fromStreamName": fromStreamName,
-		"weight": weight,
-		"adjustWeights": adjustWeights,
-		"Q.clientId": Q.clientId()
-	};
-	var baseUrl = Q.baseUrl({
-		publisherId: toPublisherId,
-		streamName: toStreamName
-	});
-	Q.req('Streams/related', [slotName], function (err, data) {
-		var message = Q.getObject('slots.result.message', data);
-		callback && callback.call(this, err, Q.getObject('slots.result', data) || null);
-	}, { method: 'put', fields: fields, baseUrl: baseUrl });
-	_retain = undefined;
-};
-
-/**
- * @class Streams.Message
- * Relates streams to one another
- * @method relate
- * @param {String} publisherId the publisher id of the stream to relate to
- * @param {String} streamName the name of the stream to relate to
- * @param {String} relationType the type of the relation, such as "parent" or "photo"
- * @param {String} fromPublisherId the publisher id of the stream to relate from
- * @param {String} fromStreamName the name of the stream to relate from
- * @param {Fucntion} callback [callback] to call with the results
- *  First parameter is the error, the second will be relations data
- */
-Streams.relate = function _Streams_relate (publisherId, streamName, relationType, fromPublisherId, fromStreamName, callback) {
-	if (!Q.plugins.Users.loggedInUser) {
-		throw new Error("Streams.relate: Not logged in.");
-	}
-	var slotName = "result";
-	var fields = {
-		"toPublisherId": publisherId,
-		"toStreamName": streamName,
-		"type": relationType,
-		"fromPublisherId": fromPublisherId,
-		"fromStreamName": fromStreamName,
-		"Q.clientId": Q.clientId()
-	};
-	// TODO: When we refactor Streams to support multiple hosts,
-	// the client will have to post this request to both hosts if they are different
-	// or servers will have tell each other on their own
-	var baseUrl = Q.baseUrl({
-		publisherId: publisherId,
-		streamName: streamName
-	});
-	Q.req('Streams/related', [slotName], function (err, data) {
-		var msg = Q.firstErrorMessage(err, data);
-		var messageFrom = Q.getObject('slots.result.messageFrom', data);
-		var messageTo = Q.getObject('slots.result.messageTo', data);
-		// wait for messages from cached streams -- from, to or both!
-		callback && callback.call(this, msg, Q.getObject('slots.result', data) || null);
+		_refreshUnlessSocket(publisherId, streamName);
+		_refreshUnlessSocket(fromPublisherId, fromStreamName);
 	}, { method: 'post', fields: fields, baseUrl: baseUrl });
 	_retain = undefined;
 };
