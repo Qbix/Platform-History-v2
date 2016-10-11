909bcc176f7a249ec20866fed2109d8ab14df66c
Greg Magarshak <greg@qbix.com>
2016-10-11 14:55:32 -0400

    Added support for marking sessions as terminated

---
+++ platform/classes/Q/Exception/SessionTerminated.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @module Q
+ */
+class Q_Exception_SessionTerminated extends Q_Exception
+{
+	/**
+	 * @class Q_Exception_SessionTerminated
+	 * @constructor
+	 * @extends Q_Exception
+	 * @param {string} $id the id of the session
+	 */
+};
+
+Q_Exception::add('Q_Exception_SessionTerminated', 'This session has been terminated. Try logging in again.');
