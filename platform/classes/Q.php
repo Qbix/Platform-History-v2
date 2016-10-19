939bfed79c43c73c3fbaed8c9e654bbb072ce68b
Greg Magarshak <greg@qbix.com>
2016-10-19 15:13:37 -0400

    Defined json_last_error_msg for PHP < 5.5

--- platform/classes/Q.php
+++ platform/classes/Q.php
@@ -1763,6 +1763,22 @@ class Q
 	public static $toolWasRendered = array();
 }
 
+if (!function_exists('json_last_error_msg')) {
+    function json_last_error_msg() {
+        static $ERRORS = array(
+            JSON_ERROR_NONE => 'No error',
+            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+            JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+            JSON_ERROR_SYNTAX => 'Syntax error',
+            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
+
+        $error = json_last_error();
+        return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
+    }
+}
+
 /// { aggregate classes for production
 /// Q/*.php
 /// Q/Exception/MissingFile.php
