d688421cb3c94a8e2b5d7db772610a27bfae2c9c
Greg Magarshak <greg@qbix.com>
2016-11-30 21:52:48 -0500

    One more fix to access checks for managing contacts

--- platform/plugins/Streams/handlers/Streams/before/Users_canManageContacts.php
+++ platform/plugins/Streams/handlers/Streams/before/Users_canManageContacts.php
@@ -5,6 +5,7 @@ function Streams_before_Users_canManageContacts($params, &$result)
 	$asUserId = $params['asUserId'];
 	$userId = $params['userId'];
 	$label = $params['label'];
+	$readOnly = $params['readOnly'];
 	$throwIfNotAuthorized = $params['throwIfNotAuthorized'];
 	if ($asUserId === $userId) {
 		if ($readOnly or substr($label, 0, 6) === 'Users/') {
