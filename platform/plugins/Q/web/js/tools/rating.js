70308f9aa1eb488a161248b022135b6b30ac45e3
Greg Magarshak <greg@qbix.com>
2017-01-08 22:53:15 -0500

    Implemented Q/paging tool

--- platform/plugins/Q/web/js/tools/rating.js
+++ platform/plugins/Q/web/js/tools/rating.js
@@ -28,9 +28,10 @@ Q.text.Q.rating = {
  * @param {Number} [options.max=5] The maximum of the range of values
  * @param {Number} [options.min=0] The minimum of the range of values
  * @param {Object} [options.stars] Information about stars
- * @param {Number} [options.stars.count=5] The number of stars corresponding to the max
- * @param {Number} [options.stars.full] Override the img src for the full star
- * @param {Number} [options.stars.empty] Override the img src for the empty star
+ * @param {Number} [options.stars.count=5] The maximum number of stars
+ * @param {String} [options.stars.full] Override the img src for the full star
+ * @param {String} [options.stars.empty] Override the img src for the empty star
+ * @param {Object} [options.stars.titles=Q.text.Q.rating.titles] A hash of {fraction: title} pairs
  * @return {Q.Tool}
  */
 Q.Tool.define('Q/rating', function (options) {
@@ -46,8 +47,8 @@ Q.Tool.define('Q/rating', function (options) {
 	stars: {
 		count: 5,
 		titles: Q.text.Q.rating.titles,
-		full: Q.url('plugins/Q/img/rating/star-yellow.png'),
-		empty: Q.url('plugins/Q/img/rating/star-gray.png')
+		full: 'plugins/Q/img/rating/star-yellow.png',
+		empty: 'plugins/Q/img/rating/star-gray.png'
 	}
 }, 
 
@@ -64,7 +65,6 @@ Q.Tool.define('Q/rating', function (options) {
 			var fraction = i/count;
 			var rounded = Math.round(fraction*10)/10;
 			var value = state.min + fraction * (state.max - state.min);
-			var id = tool.prefix + value;
 			var $star, $full, $empty, $occlusion;
 			if (!tool.filled) {
 				$star = $('<div class="Q_rating_star" />');
@@ -72,7 +72,7 @@ Q.Tool.define('Q/rating', function (options) {
 				.attr({
 					'alt': value,
 					'title': value + ': ' + state.stars.titles[rounded],
-					'src': state.stars.full
+					'src': Q.url(state.stars.full)
 				}).appendTo($star);
 				$occlusion = $('<div class="Q_rating_star_occlusion">').appendTo($star);
 				$empty = $('<img class="Q_rating_star Q_rating_star_empty" />')
