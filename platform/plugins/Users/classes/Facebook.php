f17d2873ff9835faad9a8e898d6c023814c36f12
Greg Magarshak <greg@qbix.com>
2017-02-10 17:21:51 -0500

    Replaced PHP Facebook SDK and removed node.js one

--- platform/plugins/Users/classes/Facebook.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/*
- * This file gets included when someone wants to use the Facebook class
- */
-
-set_include_path(get_include_path() . PS . dirname(__FILE__) . DS . 'Facebook');
-include('facebook.php');
