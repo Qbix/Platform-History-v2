2551e32715af7846ae321cae258183bd191737f0
Greg Magarshak <greg@qbix.com>
2017-02-17 01:21:37 -0500

    Regenerated models to work with PHP 5.3

--- platform/plugins/Streams/classes/Base/Streams/Subscription.php
+++ platform/plugins/Streams/classes/Base/Streams/Subscription.php
@@ -382,8 +382,9 @@ return array (
 		if ($value instanceof DateTime) {
 			$value = $value->getTimestamp();
 		}
+		$newDateTime = new DateTime();
 		$datetime = is_numeric($value)
-			? (new DateTime())->setTimestamp($value)
+			? $newDateTime->setTimestamp($value)
 			: new DateTime($value);
 		$value = $datetime->format("Y-m-d h:i:s");
 		return array('insertedTime', $value);			
@@ -428,8 +429,9 @@ return array (
 		if ($value instanceof DateTime) {
 			$value = $value->getTimestamp();
 		}
+		$newDateTime = new DateTime();
 		$datetime = is_numeric($value)
-			? (new DateTime())->setTimestamp($value)
+			? $newDateTime->setTimestamp($value)
 			: new DateTime($value);
 		$value = $datetime->format("Y-m-d h:i:s");
 		return array('updatedTime', $value);			
@@ -474,8 +476,9 @@ return array (
 		if ($value instanceof DateTime) {
 			$value = $value->getTimestamp();
 		}
+		$newDateTime = new DateTime();
 		$datetime = is_numeric($value)
-			? (new DateTime())->setTimestamp($value)
+			? $newDateTime->setTimestamp($value)
 			: new DateTime($value);
 		$value = $datetime->format("Y-m-d h:i:s");
 		return array('untilTime', $value);			
--- platform/plugins/Users/classes/Base/Users/Device.php
+++ platform/plugins/Users/classes/Base/Users/Device.php
@@ -514,8 +514,9 @@ return array (
 		if ($value instanceof DateTime) {
 			$value = $value->getTimestamp();
 		}
+		$newDateTime = new DateTime();
 		$datetime = is_numeric($value)
-			? (new DateTime())->setTimestamp($value)
+			? $newDateTime->setTimestamp($value)
 			: new DateTime($value);
 		$value = $datetime->format("Y-m-d h:i:s");
 		return array('insertedTime', $value);			
@@ -560,8 +561,9 @@ return array (
 		if ($value instanceof DateTime) {
 			$value = $value->getTimestamp();
 		}
+		$newDateTime = new DateTime();
 		$datetime = is_numeric($value)
-			? (new DateTime())->setTimestamp($value)
+			? $newDateTime->setTimestamp($value)
 			: new DateTime($value);
 		$value = $datetime->format("Y-m-d h:i:s");
 		return array('updatedTime', $value);			
