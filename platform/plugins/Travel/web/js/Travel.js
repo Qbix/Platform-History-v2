7a883a4c4fd0872e33ff59c6ba18f65d10c3b7d9
Greg Magarshak <greg@qbix.com>
2017-02-20 22:29:41 -0500

    Small tweaks

---
+++ platform/plugins/Travel/web/js/Travel.js
@@ -0,0 +1,23 @@
+/**
+ * Travel plugin's front end code
+ *
+ * @module Travel
+ * @class Travel
+ */
+
+(function(Q, $, w) {
+
+var Travel = Q.Travel = Q.plugins.Travel = {
+	
+};
+
+Q.text.Travel = {
+
+
+};
+
+Q.Tool.define({
+	"Travel/car": "plugins/Travel/js/tools/car.js",
+});
+
+})(Q, jQuery, window);
