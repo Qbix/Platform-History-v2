0af0904111744bb961aa0e63a6ec2a7ebb4019b0
Greg Magarshak <greg@qbix.com>
2017-03-04 20:56:43 -0500

    Added units to Places::distanceLabel() function

--- platform/plugins/Places/handlers/Places/location/tool.php
+++ platform/plugins/Places/handlers/Places/location/tool.php
@@ -11,6 +11,7 @@
  * @param {array} [$options] used to pass options
  * @param {array} [$options.meters] array of { meters: title } pairs, by default is generated from Places/nearby/meters config
  * @param {array} [$options.defaultMeters] override the key in the meters array to select by default. Defaults to "Places/nearby/defaultMeters" config
+ * @param {string} [$options.units] second parameter to pass to Places::distanceLabel()
  * @param {array} [$options.map] options for the map
  * @param {integer} [$options.map.delay=300] how many milliseconds to delay showing the map, e.g. because the container is animating
  * @param {string} [$options.map.prompt="img/map.png"] The src of the map graphical prompt when no location has been selected yet
@@ -22,8 +23,9 @@ function Places_location_tool($options)
 {
 	if (empty($options['meters'])) {
 		$options['meters'] = array();
+		$units = Q::ifset($options, 'units', null);
 		foreach (Q_Config::expect('Places', 'nearby', 'meters') as $m) {
-			$options['meters'][$m] = Places::distanceLabel($m);
+			$options['meters'][$m] = Places::distanceLabel($m, $units);
 		}
 	}
 	if (empty($options['map']['prompt'])) {
