690579c10fae2903585dad9805f86e068b6b3af5
Greg Magarshak <greg@qbix.com>
2017-03-05 18:28:27 -0500

    A few small improvements to mysql query execution in Node.js

--- platform/plugins/Streams/classes/Streams/Message.js
+++ platform/plugins/Streams/classes/Streams/Message.js
@@ -204,8 +204,6 @@ Streams_Message.post = function (fields, callback)
 		f.publisherId, f.streamName,
 		f.publisherId, f.streamName
 	];
-	console.log(query);
-	console.log(values);
 	Streams.Stream.db()
 	.rawQuery(query, values)
 	.execute(function (params) {
