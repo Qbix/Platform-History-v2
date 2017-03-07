a9008cb5862fcfaf64d52ed6974e95978352a049
Greg Magarshak <greg@qbix.com>
2017-03-07 11:37:17 -0500

    Fixed encoding and mb_strtolower in several files

--- platform/plugins/Users/classes/ApnsPHP/Log/Embedded.php
+++ platform/plugins/Users/classes/ApnsPHP/Log/Embedded.php
@@ -21,10 +21,8 @@
  * A simple logger.
  *
  * This simple logger implements the Log Interface and is the default logger for
- * all ApnsPHP_Abstract based class.
- *
- * This simple logger outputs The Message to standard output prefixed with date,
- * service name (ApplePushNotificationService) and Process ID (PID).
+ * all ApnsPHP_Abstract based classes. It uses Q::log() to output the message
+ * prefixed with date, service name (ApplePushNotificationService) and Process ID (PID).
  *
  * @ingroup ApnsPHP_Log
  */
@@ -37,8 +35,10 @@ class ApnsPHP_Log_Embedded implements ApnsPHP_Log_Interface
 	 */
 	public function log($sMessage)
 	{
-		printf("%s ApnsPHP[%d]: %s\n",
-			date('r'), getmypid(), trim($sMessage)
-		);
+		if ($key = Q_Config::get(Q::app(), "native", "ios", "logging", false)) {
+			Q::log(sprintf("%s ApnsPHP[%d]: %s\n",
+				date('r'), getmypid(), trim($sMessage)
+			), $key);
+		}
 	}
 }
