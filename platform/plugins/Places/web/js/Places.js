31243079672a3f544f5c7ea9dcf5ef1de80f60f9
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2017-03-10 14:16:30 +0300

    Update Places.location.geocode method and remove getGooglePostion method.

--- platform/plugins/Places/web/js/Places.js
+++ platform/plugins/Places/web/js/Places.js
@@ -93,91 +93,7 @@ var Places = Q.Places = Q.plugins.Places = {
 		default:
 			return meters % 100 == 0 ? (meters/1000)+' '+units : Math.ceil(meters)+" meters";
 		}
-	},
-
-	/**
-	 * Calculate google position object from different arguments
-	 * @method getGooglePosition
-	 * @param {object|string} loc (Places/location stream, google placeId, a "latitude,longitude" pair, address, {userId: ...}, {latitude: ..., longitude})
-	 * @param {function} callback
-	 */
-	getGooglePosition: function (loc, callback) {
-		if (!loc) return;
-
-		// loc already is a geocoder.geocode object
-		if(loc.lat && loc.lng){
-			callback(loc);
-			return;
-		}
-
-		var tool = this;
-		var param = {};
-
-		if (typeof loc === 'string') {
-			var parts = loc.split(',');
-			if (parts.length == 2 && !isNaN(parts[0]) && !isNaN(parts[1])) { // loc is lng,ltd pair
-				param.location = {
-					lat: parseFloat(parts[0]),
-					lng: parseFloat(parts[1])
-				};
-			} else if (loc.indexOf(' ') >= 0) { // loc is address
-				param.address = loc;
-			}else{ // loc is google place_id
-				param.placeId = loc;
-			}
-		} else if (Q.typeOf(loc) === 'Q.Streams.Stream') {// loc is Places/location stream
-			if (loc.fields.type !== 'Places/location') {
-				throw new Q.Error();
-			}
-			param.location = {
-				lat: parseFloat(loc.getAttribute('latitude')),
-				lng: parseFloat(loc.getAttribute('longitude'))
-			};
-		} else if(Q.typeOf(loc) === 'object'){
-			if(loc.placeId){ // loc is object {placeId: ...}
-				param.placeId = loc.placeId;
-			}else if(loc.latitude && loc.longitude){ // loc is object {latitude: ..., longitude: ...}
-				param.location = {
-					lat: parseFloat(loc.latitude),
-					lng: parseFloat(loc.longitude)
-				};
-			}else if(loc.userId){ // loc is user id
-				Q.Streams.get(loc.userId, "Places/user/location", function (err) {
-					if (err) {
-						return;
-					}
-
-					tool.getGooglePosition(this, callback);
-				});
-
-				return;
-			}else{
-				throw new Q.Error("Travel/map: unknown loc param: " + loc);
-			}
-		}else{
-			throw new Q.Error("Travel/map: unknown loc param: " + loc);
-		}
-
-		// localy calculate if known lat and lng, to avoid OVER_QUERY_LIMIT
-		if(param.location){
-			callback(new google.maps.LatLng(param.location.lat, param.location.lng));
-			return;
-		}
-
-		// calculate location only for placeId and address
-		var geocoder = new google.maps.Geocoder;
-		geocoder.geocode(param, function (results, status) {
-			if (status !== 'OK') {
-				throw new Q.Error("Travel/map: Can't geocode " + status);
-			}
-			if (!results[0]) {
-				throw new Q.Error("Travel/map: No place matched " + results);
-			}
-
-			callback(results[0].geometry.location);
-		});
 	}
-
 };
 
 /**
@@ -212,15 +128,8 @@ Places.Location = {
 	 * @static
 	 * @param {Object} loc Provide a Places/location stream, or an object with either a "placeId" property, a pair of "latitude","longitude" properties, an "address" property for reverse geocoding, or a pair of "userId" and optional "streamName" (which otherwise defaults to "Places/user/location")
 	 * @param {Function} callback gets (array of results of the geolocation, and status code)
-	 * @param {Object} [options]
-	 * @param {Object} [options.provider='google']
 	 */
-	geocode: function (loc, callback, options) {
-		var o = options || {};
-		o.provider = o.provider || 'google';
-		if (o.provider !== 'google') {
-			return;
-		}
+	geocode: function (loc, callback) {
 		Places.loadGoogleMaps(function () {
 			var param = {};
 			var p = "Places.Location.geocode: ";
@@ -236,18 +145,22 @@ Places.Location = {
 				if (!loc.latitude) {
 					throw new Q.Error(p + "missing latitude");
 				}
-				if (!loc.latitude) {
+				if (!loc.longitude) {
 					throw new Q.Error(p + "missing longitude");
 				}
-				param.location = {
-					lat: loc.latitude,
-					lng: loc.longitude
-				};
+
+				// localy calculate if known lat and lng, to avoid requests to google api (danger of OVER_QUERY_LIMIT !!!)
+				Q.handle(callback, Places.Location, [new google.maps.LatLng(parseFloat(loc.latitude), parseFloat(loc.longitude)), 'OK']);
+				return;
 			} else if (loc.address) {
 				param.address = loc.address;
+			} else if (typeof loc.lat == 'function' && typeof loc.lng == 'function') { // loc - already google object
+				Q.handle(callback, Places.Location, [loc, 'OK']);
+				return;
 			} else {
 				throw new Q.Error(p + "wrong location format");
 			}
+
 			var geocoder = new google.maps.Geocoder;
 			geocoder.geocode(param, function (results, status) {
 				if (status !== 'OK') {
@@ -256,7 +169,8 @@ Places.Location = {
 				if (!results[0]) {
 					throw new Q.Error(p + "no place matched " + loc);
 				}
-				Q.handle(callback, Places.Location, [results[0], status, results]);
+
+				Q.handle(callback, Places.Location, [results[0].geometry.location, status, results]);
 			});
 		});
 	}
