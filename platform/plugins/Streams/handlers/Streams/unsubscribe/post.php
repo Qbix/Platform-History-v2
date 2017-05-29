d719474cb835ea103b4d260123e95ec8822823e4
Greg Magarshak <greg@qbix.com>
2017-05-29 13:31:24 -0400

    Added subscribe and unsubscribe handlers in Streams plugin

---
+++ platform/plugins/Streams/handlers/Streams/unsubscribe/post.php
@@ -0,0 +1,12 @@
+<?php
+
+function Streams_unsubscribe_post()
+{
+	$user = Users::loggedInUser(true);
+	$publisherId = Streams::requestedPublisherId();
+	$streamName = Streams::requestedName(true);
+	$stream = Streams::fetchOne($user->id, $publisherId, $streamName, true);
+	if ($participant = $stream->unsubscribe($participant)) {
+		Q_Response::setSlot('participant', $participant->exportArray());
+	}
+}
