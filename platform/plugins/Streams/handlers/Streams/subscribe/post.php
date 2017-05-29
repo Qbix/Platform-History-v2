f49ff4e66973ab1b570398b3991be36d2007b329
Greg Magarshak <greg@qbix.com>
2017-05-29 13:31:24 -0400

    Added subscribe and unsubscribe handlers in Streams plugin

---
+++ platform/plugins/Streams/handlers/Streams/subscribe/post.php
@@ -0,0 +1,14 @@
+<?php
+
+function Streams_subscribe_post()
+{
+	$user = Users::loggedInUser(true);
+	$publisherId = Streams::requestedPublisherId();
+	$streamName = Streams::requestedName(true);
+	$stream = Streams::fetchOne($user->id, $publisherId, $streamName, true);
+	// SECURITY: Do not allow client to set options here
+	// because then they can set participant extra.
+	if ($participant = $stream->subscribe()) {
+		Q_Response::setSlot('participant', $participant->exportArray());
+	}
+}
