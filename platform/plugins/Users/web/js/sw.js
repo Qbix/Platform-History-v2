f6fd605a48525de1e6277a54326c35b30a96cb38
Evgeny Kokhanovsky <evgeny.kokhanovsky@gmail.com>
2017-07-07 22:00:00 +0300

    Send app config to SW and initialize Firebase

--- platform/plugins/Users/web/js/sw.js
+++ platform/plugins/Users/web/js/sw.js
@@ -1,11 +1,26 @@
-importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
 
+var messaging;
 
+/*
 self.addEventListener('install', function(event) {
 	console.log('Service Worker installing.');
+	console.log("this is mess:", mess);
 });
 
 self.addEventListener('activate', function(event) {
 	console.log('Service Worker activating.');
+});
+*/
+
+self.addEventListener('message', function(event) {
+
+	var data = JSON.parse(event.data);
+	// try to get SW config and check if firebase is already initialized
+	if (data.config && !firebase.apps.length) {
+		firebase.initializeApp(data.config);
+		messaging = firebase.messaging();
+	}
+
 });
