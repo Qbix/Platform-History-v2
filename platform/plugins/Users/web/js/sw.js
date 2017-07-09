f0cd70fa39eb05000b1990184325587da5ba0353
Evgeny Kokhanovsky <evgeny.kokhanovsky@gmail.com>
2017-07-09 03:26:57 +0300

    Saving and restoring indexedDB firebase config for the service worker. Doing web pushes in a foreground, background and after browser restart.

--- platform/plugins/Users/web/js/sw.js
+++ platform/plugins/Users/web/js/sw.js
@@ -1,32 +1,85 @@
 importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.1.3/firebase-messaging.js');
 
-var messaging;
+var baseName = "qbix_db",
+	storeName = "qbix_store";
 
-/*
-self.addEventListener('install', function(event) {
-	console.log('Service Worker installing.');
-	console.log("this is mess:", mess);
-});
+function connectDB(f){
+	var request = indexedDB.open(baseName, 1);
+	request.onerror = function (err) {
+		console.warn(err);
+	};
+	request.onsuccess = function(){
+		f(request.result);
+	};
+	request.onupgradeneeded = function(e){
+		e.currentTarget.result.createObjectStore(storeName, { keyPath: "id" });
+		connectDB(f);
+	}
+}
 
-self.addEventListener('activate', function(event) {
-	console.log('Service Worker activating.');
+// save config
+function saveConfig(conf, callback) {
+	connectDB(function(db){
+		var request = db.transaction([storeName], "readwrite").objectStore(storeName).put({id: 'config', config: conf});
+		request.onerror = function (err) {
+			console.warn(err);
+		};
+		request.onsuccess = function(){
+			callback();
+			return request.result;
+		}
+	});
+}
+
+// get config
+function getConfig() {
+	return new Promise(function(resolve, reject){
+		connectDB(function(db){
+			var request = db.transaction([storeName], "readonly").objectStore(storeName).get('config');
+			request.onerror = function (err) {
+				console.warn(err);
+				reject(err);
+			};
+			request.onsuccess = function(){
+				console.log("Get config");
+				resolve(request.result ? request.result : -1);
+			}
+		});
+	});
+
+}
+
+function initializeFirebase(config) {
+	if (firebase.apps.length) {
+		return firebase.messaging();
+	}
+	firebase.initializeApp(config);
+	messaging = firebase.messaging();
+	return messaging;
+}
+
+self.addEventListener('push', function(event) {
+	var getConf = getConfig().then(function(val) {
+		var config = val.config;
+		if (config) {
+			var payload = JSON.parse(event.data.text());
+			return self.registration.showNotification(payload.notification.title, {
+				title: payload.notification.title,
+				body: payload.notification.body
+			})
+		}
+		return false;
+	});
+	event.waitUntil(getConf);
 });
-*/
 
 self.addEventListener('message', function(event) {
-
 	var data = JSON.parse(event.data);
-	// try to get SW config and check if firebase is already initialized
-	if (data.config && !firebase.apps.length) {
-		firebase.initializeApp(data.config);
-		messaging = firebase.messaging();
-
-		messaging.setBackgroundMessageHandler(function(payload){
-			return self.registration.showNotification();
+	if (data.config) {
+		initializeFirebase(data.config);
+		saveConfig(data.config, function() {
+			console.log('Saved config');
 		});
-
 	}
-
 });
-
