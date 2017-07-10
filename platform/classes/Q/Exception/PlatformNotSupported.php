aefea7b55c9bb021da3bdda0664394f54180a1ec
Greg Magarshak <greg@qbix.com>
2017-07-09 22:15:44 -0400

    Added methods to generate polylines from routes

---
+++ platform/classes/Q/Exception/PlatformNotSupported.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @module Q
+ */
+class Q_Exception_PlatformNotSupported extends Q_Exception
+{
+	/**
+	 * @class Q_Exception_PlatformNotSupported
+	 * @constructor
+	 * @extends Q_Exception
+	 * @param {string} $platform
+	 */
+};
+
+Q_Exception::add('Q_Exception_PlatformNotSupported', 'the {{platform}} platform is not supported');
