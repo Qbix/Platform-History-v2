fba621131099cf973f4086dfe61100646d5efa7c
Greg Magarshak <greg@qbix.com>
2017-08-06 19:42:21 -0400

    Fixed some small things related to tasks

--- platform/plugins/Streams/scripts/Streams/0.9.11-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/0.9.11-Streams.mysql.php
@@ -4,7 +4,7 @@ function Streams_0_9_11_Streams()
 {
 	$publisherId = Q::app();
 	Streams::create($publisherId, $publisherId, 'Streams/tasks', array(
-		'name' => 'Streams/import/app',
+		'name' => 'Streams/tasks/app',
 		'readLevel' => 0,
 		'writeLevel' => 0,
 		'adminLevel' => 0
