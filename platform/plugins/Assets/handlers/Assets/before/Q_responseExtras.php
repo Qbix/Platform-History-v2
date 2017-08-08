751fdd73464ff3a441c06e4fd8044ee3158cd067
Evgeny Kokhanovsky <evgeny.kokhanovsky@gmail.com>
2017-08-09 02:10:00 +0300

    Android Pay for Mobile Chrome

--- platform/plugins/Assets/handlers/Assets/before/Q_responseExtras.php
+++ platform/plugins/Assets/handlers/Assets/before/Q_responseExtras.php
@@ -9,8 +9,11 @@ function Assets_before_Q_responseExtras() {
 		$amount = null;
 	}
 	Q_Response::setScriptData('Q.plugins.Assets.credits', compact('amount'));
-	
+
 	if ($publishableKey = Q_Config::get('Assets', 'payments', 'stripe', 'publishableKey', null)) {
+		Q_Response::addScript('https://js.stripe.com/v2/');
 		Q_Response::setScriptData('Q.plugins.Assets.Payments.stripe.publishableKey', $publishableKey);
+		Q_Response::setScriptData('Q.plugins.Assets.Payments.androidPay', Q_Config::get('Assets', 'payments', 'androidPay', null));
+		Q_Response::setScriptData('Q.plugins.Assets.Payments.stripe.version', Q_Config::get('Assets', 'payments', 'stripe', 'version', null));
 	}
 }
