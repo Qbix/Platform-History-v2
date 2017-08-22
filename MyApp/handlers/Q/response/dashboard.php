70b98729beb7225fb5983c3edaa0e8971a8e5675
Greg Magarshak <greg@qbix.com>
2017-08-22 00:14:40 -0400

    Made Home tab appear when logged in

--- MyApp/handlers/Q/response/dashboard.php
+++ MyApp/handlers/Q/response/dashboard.php
@@ -5,5 +5,13 @@ function Q_response_dashboard()
 	$app = Q_Config::expect('Q', 'app');
 	$slogan = "Powered by Qbix.";
 	$user = Users::loggedInUser();
-	return Q::view("$app/dashboard.php", compact('slogan', 'user'));
+	if (Users::loggedInUser(false, false)) {
+		$tabs = array('welcome' => 'Welcome');
+	} else {
+		$tabs = array('home' => 'Home');
+	}
+	$tabs = array_merge($tabs, array(
+		'about' => 'About'
+	));
+	return Q::view("$app/dashboard.php", compact('slogan', 'user', 'tabs'));
 }
