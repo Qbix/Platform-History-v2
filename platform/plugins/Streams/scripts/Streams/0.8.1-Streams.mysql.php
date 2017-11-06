a84a1cfe5153aa6a82c6e1b5f84433ef70b5e201
Greg Magarshak <greg@qbix.com>
2017-11-05 23:48:37 -0500

    Added subscription template for Streams/experience streams

--- platform/plugins/Streams/scripts/Streams/0.8.1-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/0.8.1-Streams.mysql.php
@@ -16,6 +16,22 @@ function Streams_0_8_1_Streams_mysql()
 	$stream->writeLevel = Streams::$WRITE_LEVEL['join'];
 	$stream->adminLevel = Streams::$ADMIN_LEVEL['tell'];
 	$stream->save();
+
+	// also save subscription template
+	$subscription = new Streams_Subscription();
+	$subscription->publisherId = '';
+	$subscription->streamName = 'Streams/experience/';
+	$subscription->filter = Q::json_encode(array(
+		"types" => array(
+			"^(?!(Users/)|(Streams/)).*/",
+			"Streams/announcement",
+			"Streams/chat/message"
+		),
+		"notifications" => 0
+	));
+	$subscription->untilTime = null;
+	$subscription->duration = 0;
+	$subscription->save();
 	
 	// main community experience stream, for community-wide announcements etc.
 	$user = Users_User::fetch($communityId);
