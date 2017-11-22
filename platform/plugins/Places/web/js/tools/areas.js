b584005271de5d8ce25dedaa8b5387558f46e5f7
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2017-11-22 17:53:44 +0300

    check whether Places/location stream for current venue exist on server. If exist - return one. If not - create and return.

--- platform/plugins/Places/web/js/tools/areas.js
+++ platform/plugins/Places/web/js/tools/areas.js
@@ -24,33 +24,7 @@ Q.Tool.define("Places/areas", function (options) {
 	var state = this.state;
 	var $te = $(tool.element);
 
-	// set communityId as publisherId if last empty
-	state.publisherId = state.publisherId || Q.Users.communityId;
-
-	if (!state.stream && (state.publisherId && state.streamName)) {
-		state.stream = {
-			publisherId: state.publisherId,
-			name: state.streamName,
-			stripped: true
-		};
-	}
-
-	// required publisherId and streamName OR location
-	if (!state.stream && !state.location) {
-		throw new Exception("Places/areas: required publisherId and streamName OR location");
-	}
-
 	Q.addStylesheet('Q/plugins/Places/css/areas.css');
-
-	if (state.stream && state.stream.stripped) { // stripped stream means that it have only publisherId and name
-		Q.Streams.get(state.stream.publisherId, state.stream.name, function () {
-			tool.refresh(this);
-		});
-	} else if(state.stream) { // we have pure stream
-		tool.refresh(state.stream);
-	} else if(state.location) { // we have just location object and need to check whether stream exist
-
-	}
 
 	// get location stream and run refresh method
 	tool.refresh();
@@ -73,25 +47,23 @@ Q.Tool.define("Places/areas", function (options) {
 		var state = tool.state;
 		var $te = $(tool.element);
 
-		// create Streams/participants tool
-		$te.tool('Streams/lookup', {
-			publisherId: Q.Users.communityId,
-			types: ["Places/area"],
-			filter: {
-				placeholder: "Any area inside this location?"
-			},
-			onChoose: function (streamName, element, obj) {
-				console.log(streamName);
-			}
-		}, tool.prefix).activate(function(){
-			// no need stream type selector as we use only one
-			$("select[name=streamType]", this.element).remove();
+		tool.getStream(function(stream){
+			// create Streams/participants tool
+			$te.tool('Streams/lookup', {
+				publisherId: stream.fields.publisherId,
+				types: ["Places/area"],
+				filter: {
+					placeholder: "Any area inside this location?"
+				},
+				onChoose: function (streamName, element, obj) {
+					console.log(streamName);
+				}
+			}, tool.prefix).activate(function(){
+				// no need stream type selector as we use only one
+				$("select[name=streamType]", this.element).remove();
+			});
 		});
 	},
-	/**
-	 * Render template with locations, so user can select one of them
-	 * @method selectLocation
-	 */
 	/**
 	 * Get location stream and launch callback with this stream as argument
 	 * @method getStream
@@ -100,14 +72,55 @@ Q.Tool.define("Places/areas", function (options) {
 	getStream: function(callback){
 		var state = this.state;
 
-		Q.Streams.get(state.publisherId, state.streamName, function (err) {
-			var fem = Q.firstErrorMessage(err);
-			if (fem) {
-				return console.warn("Places/areas: " + fem);
-			}
+		// set communityId as publisherId if last empty
+		state.publisherId = state.publisherId || Q.info.appId;
 
-			Q.handle(callback, this, [this]);
-		});
+		if (!state.stream && (state.publisherId && state.streamName)) {
+			state.stream = {
+				publisherId: state.publisherId,
+				name: state.streamName,
+				stripped: true
+			};
+		}
+
+		var location = state.location;
+
+		// required publisherId and streamName OR location
+		if (!state.stream && !location) {
+			throw new Exception("Places/areas: required publisherId and streamName OR location");
+		}
+
+		if (state.stream && state.stream.stripped) { // stripped stream means that it have only publisherId and name
+			Q.Streams.get(state.stream.publisherId, state.stream.name, function () {
+				Q.handle(callback, this, [this]);
+			});
+		} else if(state.stream) { // we have pure stream
+			Q.handle(callback, state.stream, [state.stream]);
+		} else if(location) { // we have just location object and need to check whether stream exist
+			// check if we already have location with lat, lng and use one
+			// if no - create Places/location stream
+			Q.req("Places/areas", 'data', function (err, response) {
+				var msg;
+				if (msg = Q.firstErrorMessage(err, response && response.errors)) {
+					console.warn("Places/areas: " + msg);
+					return false;
+				}
+
+				var stream = response.slots.data;
+				Q.Streams.get(stream.publisherId, stream.name, function () {
+					Q.handle(callback, this, [this]);
+				});
+			}, {
+				method: 'GET',
+				fields: {
+					location: {
+						latitude: location.latitude,
+						longitude: location.longitude,
+						venue: location.venue
+					}
+				}
+			});
+		}
 	}
 });
 
