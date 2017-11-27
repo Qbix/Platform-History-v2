35394f0082f3af03841c5c89f24234700bf303ff
Greg Magarshak <greg@qbix.com>
2017-11-27 12:43:42 -0500

    Made Q/response use empty slotName at the end instead of beginning of response

--- platform/handlers/Q/response.php
+++ platform/handlers/Q/response.php
@@ -107,7 +107,7 @@ function Q_response($params)
 				Q::event('Q/responseExtras', array(), 'after');
 				$to_encode['slots'] = Q_Response::slots(true);
 				// add stylesheets, stylesinline, scripts, scriptlines, scriptdata, templates
-				foreach (array_merge(array(''), $slotNames) as $slotName) {
+				foreach (array_merge($slotNames, array('')) as $slotName) {
 					$temp = Q_Response::stylesheetsArray($slotName);
 					if ($temp) $to_encode['stylesheets'][$slotName] = $temp;
 					$temp = Q_Response::stylesInline($slotName);
