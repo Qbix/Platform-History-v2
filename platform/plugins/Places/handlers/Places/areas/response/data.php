f423fd8a7da264da7a638ae6432fa54b44aa219e
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2017-11-28 20:00:47 +0300

    Use Places_location::stream to get/create Places/location stream and use appId as publisherId

--- platform/plugins/Places/handlers/Places/areas/response/data.php
+++ platform/plugins/Places/handlers/Places/areas/response/data.php
@@ -7,18 +7,9 @@ function Places_areas_response_data()
 	$location = $_REQUEST["location"];
 	Q_Valid::requireFields(array('latitude', 'longitude', 'venue'), $location, true);
 
-	$app = Q::app();
+	$appId = Q::app();
 
-	$stream = new Streams_Stream();
-	$stream->publisherId = $app;
-	$stream->type = "Places/location";
-	$stream->title = $location["venue"];
-	$stream->setAttribute("latitude", $location["latitude"]);
-	$stream->setAttribute("longitude", $location["longitude"]);
-
-	if(!$stream->retrieve()) {
-		$stream->save();
-	}
+	$stream = Places_Location::stream($appId, $location['placeId'], true);
 
 	return $stream;
 }
