49b0c3cd8e34490aea20dc250542fe39e095d863
Greg Magarshak <greg@qbix.com>
2017-11-30 19:58:25 -0500

    Updated MyApp template to add welcome.js under content slot

--- MyApp/handlers/MyApp/welcome/response/content.php
+++ MyApp/handlers/MyApp/welcome/response/content.php
@@ -5,7 +5,7 @@ function MyApp_welcome_response_content($params)
 	// Do controller stuff here. Prepare variables
 	$tabs = array("foo" => "bar");
 	$description = "this is a description";
-	Q_Response::addScript('js/pages/welcome.js', 'MyApp');
+	Q_Response::addScript('js/pages/welcome.js');
 	return Q::view('MyApp/content/welcome.php', compact('tabs', 'description'));
 }
 
