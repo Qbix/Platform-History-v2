a5efc13398c7284215b76972c97ee75f9711e151
Greg Magarshak <greg@qbix.com>
2017-12-05 01:22:11 -0500

    Added royalty-free terms template to MyApp template

---
+++ MyApp/views/MyApp/content/terms.php
@@ -0,0 +1,167 @@
+<div id="content">
+	
+<h1>Terms of Service</h1>
+
+<h2>Introduction</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">These terms and conditions govern your use of this website or app; by using them, you accept these terms and conditions in full. &nbsp; If you disagree with these terms and conditions or any part of these terms and conditions, you must not use this website or this app. </span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must be at least 13 years of age to use this website. &nbsp;By using this website or app and by agreeing to these terms and conditions you warrant and represent that you are at least 13 years of age.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">This website and web app uses cookies. &nbsp;By using it and agreeing to these terms and conditions, you consent to our <?php echo $communityName ?>&#39;s use of cookies in accordance with the terms of <?php echo $communityName ?>&#39;s privacy policy / cookies policy.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>License to use website or app</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">Unless otherwise stated, <?php echo $communityName ?> and/or its licensors own the intellectual property rights in the website/app and material on the website/app. &nbsp;Subject to the license below, all these intellectual property rights are reserved.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You may view, download for caching purposes only, and print pages or other content from the website or app for your own personal use, subject to the restrictions set out below and elsewhere in these terms and conditions. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not:</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<ul class="c7 lst-kix_ddtfrmtti1dt-0 start">
+ <li class="c0 c1"><span class="c2">republish material from this website/app (including republication on another website);</span></li>
+ <li class="c0 c1"><span class="c2">sell, rent or sub-license material from the website or app;</span></li>
+ <li class="c0 c1"><span class="c2">show any material from the website/app in public;</span></li>
+ <li class="c0 c1"><span class="c2">reproduce, duplicate, copy or otherwise exploit material on this website/app for a commercial purpose;</span></li>
+ <li class="c0 c1"><span class="c2">edit or otherwise modify any material on the website/app; or</span></li>
+ <li class="c0 c1"><span class="c2">redistribute material from this website/app except for content specifically and expressly made available for redistribution.</span></li>
+</ul>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Where content is specifically made available for redistribution, it may only be redistributed within your organisation.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Acceptable use</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">You must not use this website or app in any way that causes, or may cause, damage to the website or app, or impairment of its availability or accessibility; or in any way which is unlawful, illegal, fraudulent or harmful, or in connection with any unlawful, illegal, fraudulent or harmful purpose or activity.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not use this website or app to copy, store, host, transmit, send, use, publish or distribute any material which consists of (or is linked to) any spyware, computer virus, Trojan horse, worm, keystroke logger, rootkit or other malicious computer software.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not conduct any systematic or automated data collection activities (including without limitation scraping, data mining, data extraction and data harvesting) on or in relation to this website or app without express written consent from <?php echo $organizationName ?>.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not use this website or app to transmit or send unsolicited commercial communications.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not use this website or app for any purposes related to marketing without <?php echo $communityName ?>'s express written consent. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Restricted access</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">Access to certain areas of this website or app is restricted. &nbsp;<?php echo $communityName ?> reserves the right to restrict access to other areas of this website or app, or indeed this entire website or app, at <?php echo $communityName ?> discretion.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">If <?php echo $communityName ?> provides you with a user ID and password to enable you to access restricted areas of this website or app or other content or services, you must ensure that the user ID and password are kept confidential. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2"><?php echo $communityName ?> may disable your user ID and password in <?php echo $communityName ?> sole discretion without notice or explanation.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>User content</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">In these terms and conditions, &ldquo;your user content&rdquo; means material (including without limitation text, images, audio material, video material and audio-visual material) that you submit to this website or app, for whatever purpose.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You grant to <?php echo $communityName ?> a worldwide, irrevocable, non-exclusive, royalty-free license to use, reproduce, adapt, publish, translate and distribute your user content in any existing or future media. &nbsp;You also grant to <?php echo $communityName ?> the right to sub-license these rights, and the right to bring an action for infringement of these rights.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Your user content must not be illegal or unlawful, must not infringe any third party&#39;s legal rights, and must not be capable of giving rise to legal action whether against you or <?php echo $communityName ?> or a third party (in each case under any applicable law). &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You must not submit any user content to the website or app that is or has ever been the subject of any threatened or actual legal proceedings or other similar complaint.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2"><?php echo $communityName ?> reserves the right to edit or remove any material submitted to this website or app, or stored on <?php echo $communityName ?>'s servers, or hosted or published upon this website or app.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Notwithstanding <?php echo $communityName ?>'s rights under these terms and conditions in relation to user content, <?php echo $communityName ?> does not undertake to monitor the submission of such content to, or the publication of such content on, this website or app.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>No warranties</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">This website/app is provided &ldquo;as is&rdquo; without any representations or warranties, express or implied. &nbsp;<?php echo $communityName ?> makes no representations or warranties in relation to this website/app or the information and materials provided on this website/app. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Without prejudice to the generality of the foregoing paragraph, <?php echo $communityName ?> does not warrant that:</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<ul class="c7 lst-kix_vyha1skn9sk6-0 start">
+ <li class="c0 c1"><span class="c2">this website or app will be constantly available, or available at all; or</span></li>
+ <li class="c0 c1"><span class="c2">the information on this website or app is complete, true, accurate or non-misleading.</span></li>
+</ul>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Nothing on this website or app constitutes, or is meant to constitute, advice of any kind. &nbsp;If you require advice in relation to any legal, financial or medical matter you should consult an appropriate professional.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Limitations of liability</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2"><?php echo $communityName ?> will not be liable to you (whether under the law of contact, the law of torts or otherwise) in relation to the contents of, or use of, or otherwise in connection with, this website or app:</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<ul class="c7 lst-kix_92vwhsqogi1z-0 start">
+ <li class="c0 c1"><span class="c2">to the extent that the website or app is provided free-of-charge, for any direct loss;</span></li>
+ <li class="c0 c1"><span class="c2">for any indirect, special or consequential loss; or</span></li>
+ <li class="c0 c1"><span class="c2">for any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business relationships, loss of reputation or goodwill, or loss or corruption of information or data.</span></li>
+</ul>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">These limitations of liability apply even if <?php echo $communityName ?> has been expressly advised of the potential loss.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Exceptions</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">Nothing in this website or app disclaimer will exclude or limit any warranty implied by law that it would be unlawful to exclude or limit; and nothing in this website or app disclaimer will exclude or limit <?php echo $communityName ?>'s liability in respect of any:</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<ul class="c7 lst-kix_jksad2hrr972-0 start">
+ <li class="c0 c1"><span class="c2">death or personal injury caused by <?php echo $communityName ?>'s negligence;</span></li>
+ <li class="c0 c1"><span class="c2">fraud or fraudulent misrepresentation on the part of <?php echo $communityName ?>; or</span></li>
+ <li class="c0 c1"><span class="c2">matter which it would be illegal or unlawful for <?php echo $communityName ?> to exclude or limit, or to attempt or purport to exclude or limit, its liability. </span></li>
+</ul>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Reasonableness</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">By using this website or app, you agree that the exclusions and limitations of liability set out in this website or app disclaimer are reasonable. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">If you do not think they are reasonable, you must not use this website or app.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Other parties</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">You accept that, as a limited liability entity, <?php echo $communityName ?> has an interest in limiting the personal liability of its officers and employees. &nbsp;You agree that you will not bring any claim personally against <?php echo $communityName ?>'s officers or employees in respect of any losses you suffer in connection with the website or app.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">Without prejudice to the foregoing paragraph, you agree that the limitations of warranties and liability set out in this website or app disclaimer will protect <?php echo $communityName ?>'s officers, employees, agents, subsidiaries, successors, assigns and sub-contractors as well as <?php echo $communityName ?>. </span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Unenforceable provisions</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">If any provision of this website or app disclaimer is, or is found to be, unenforceable under applicable law, that will not affect the enforceability of the other provisions of this website or app disclaimer.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Indemnity</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">You hereby indemnify <?php echo $communityName ?> and undertake to keep <?php echo $communityName ?> indemnified against any losses, damages, costs, liabilities and expenses (including without limitation legal expenses and any amounts paid by <?php echo $communityName ?> to a third party in settlement of a claim or dispute on the advice of <?php echo $communityName ?>'s legal advisers) incurred or suffered by <?php echo $communityName ?> arising out of any breach by you of any provision of these terms and conditions, or arising out of any claim that you have breached any provision of these terms and conditions.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Breaches of these terms and conditions</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">Without prejudice to <?php echo $communityName ?>'s other rights under these terms and conditions, if you breach these terms and conditions in any way, <?php echo $communityName ?> may take such action as <?php echo $communityName ?> deems appropriate to deal with the breach, including suspending your access to the website or app, prohibiting you from accessing the website or app, blocking computers using your IP address from accessing the website or app, contacting your internet service provider to request that they block your access to the website or app and/or bringing court proceedings against you.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Variation</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2"><?php echo $communityName ?> may revise these terms and conditions from time-to-time. &nbsp;Revised terms and conditions will apply to the use of this website or app from the date of the publication of the revised terms and conditions on this website or app. &nbsp;Please check this page regularly to ensure you are familiar with the current version.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Assignment</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2"><?php echo $communityName ?> may transfer, sub-contract or otherwise deal with <?php echo $communityName ?>'s rights and/or obligations under these terms and conditions without notifying you or obtaining your consent.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0"><span class="c2">You may not transfer, sub-contract or otherwise deal with your rights and/or obligations under these terms and conditions. &nbsp;</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Severability</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">If a provision of these terms and conditions is determined by any court or other competent authority to be unlawful and/or unenforceable, the other provisions will continue in effect. &nbsp;If any unlawful and/or unenforceable provision would be lawful or enforceable if part of it were deleted, that part will be deemed to be deleted, and the rest of the provision will continue in effect. </span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Entire agreement</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">These terms and conditions constitute the entire agreement between you and <?php echo $communityName ?> in relation to your use of this website or app, and supersede all previous agreements in respect of your use of this website or app.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Law and jurisdiction</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span class="c2">These terms and conditions will be governed by and construed in accordance with <?php echo $governingLaw ?>, and any disputes relating to these terms and conditions will be subject to the exclusive jurisdiction of the courts of <?php echo $jurisdiction ?>.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<h2>Credit</h2>
+<p class="c0 c4"><span class="c3"></span></p>
+<p class="c0"><span>This document was created using a Contractology template available at</span><span><a class="c5" href="http://www.contractology.com&amp;sa=D&amp;ust=1512456757349000&amp;usg=AFQjCNHw7Bmr63gcPzsdbSH7y8AKtORcvw">&nbsp;</a></span><span class="c14"><a class="c5" href="q=http://www.contractology.com&amp;sa=D&amp;ust=1512456757349000&amp;usg=AFQjCNHw7Bmr63gcPzsdbSH7y8AKtORcvw">http://www.contractology.com</a></span><span class="c2">.</span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<p class="c0 c4"><span class="c2"></span></p>
+<a id="t.64cb0989cb3b65ddc0533ab68dfd3b8788e269bc"></a><a id="t.0"></a>
+<table class="c8">
+ <tbody>
+    <tr class="c9">
+       <td class="c15" colspan="1" rowspan="1">
+          <p class="c0"><span class="c2">You must retain the &quot;Credit&quot; section in this document. If you wish to use the document without the &quot;Credit&quot; section (e.g. to project a more professional image) then you can get a license to do so here:</span></p>
+          <p class="c0 c4"><span class="c3"></span></p>
+          <p class="c0"><span class="c6"><a class="c5" href="http://www.contractology.com/free-document-license-website-terms-and-conditions.html&amp;sa=D&amp;ust=1512456757351000&amp;usg=AFQjCNFhBzzG4KUWB5aJYd4GZE6El-Buhw">http://www.contractology.com/free-document-license-website-terms-and-conditions.html</a></span></p>
+          <p class="c0 c4"><span class="c11 c13"><a class="c5" href="http://www.contractology.com/free-document-license-website-terms-and-conditions.html&amp;sa=D&amp;ust=1512456757352000&amp;usg=AFQjCNG1i3vXzYhoSAl3XQ5QqQLxd9pNpg"></a></span></p>
+          <p class="c0"><span class="c2">It is an infringement of our copyright to use the document without the &quot;Credit&quot; section and without paying the license fee.</span></p>
+       </td>
+    </tr>
+ </tbody>
+</table>
+
+</div>
