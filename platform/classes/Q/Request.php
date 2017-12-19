6046dee05352e18a0a3c445a37f890af2561e175
Evgeny Kokhanovsky <evgeny@qbix.com>
2017-12-20 04:24:10 +0500

    Used old php array() syntax

--- platform/classes/Q/Request.php
+++ platform/classes/Q/Request.php
@@ -270,7 +270,7 @@ class Q_Request
 		if (strpos($ext, '/') !== false) {
 			$ext = '';
 		}
-		$extensions = Q_Config::get("Q", "filename", "extensions", []);
+		$extensions = Q_Config::get("Q", "filename", "extensions", array());
 		return in_array($ext, $extensions) ? Q_PLUGIN_WEB_DIR.DS.'img'.DS.'404'.DS."404.$ext" : null;
 	}
 	
