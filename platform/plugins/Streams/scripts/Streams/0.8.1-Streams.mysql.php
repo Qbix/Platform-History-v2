b507f56f9ce323122a8ce2a4042f62af8e18109c
Greg Magarshak <greg@qbix.com>
2018-01-28 23:25:36 -0500

    Fixed symlinking during install in Users and Streams plugins, too

--- platform/plugins/Streams/scripts/Streams/0.8.1-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/0.8.1-Streams.mysql.php
@@ -50,6 +50,13 @@ function Streams_0_8_1_Streams_mysql()
 		USERS_PLUGIN_FILES_DIR.DS.'Users'.DS.'icons'.DS.'labels'.DS.'Streams',
 		true
 	);
+	
+	// symlink the icons folder
+	Q_Utils::symlink(
+		STREAMS_PLUGIN_FILES_DIR.DS.'Streams'.DS.'icons',
+		STREAMS_PLUGIN_WEB_DIR.DS.'img'.DS.'icons',
+		true
+	);
 }
 
 Streams_0_8_1_Streams_mysql();
