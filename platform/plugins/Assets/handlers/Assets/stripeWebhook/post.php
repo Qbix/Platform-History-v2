e42a69ff13b562bf8b7199808dbcda201324cce2
Evgeny Kokhanovsky <evgeny@qbix.com>
2018-02-08 05:13:50 +0500

    Stripe webhook

--- platform/plugins/Assets/handlers/Assets/stripeWebhook/post.php
+++ platform/plugins/Assets/handlers/Assets/stripeWebhook/post.php
@@ -11,8 +11,30 @@
  *  @param {String} [$_REQUEST.currency="usd"] the currency to pay in. (authnet supports only "usd")
  *  @param {String} [$_REQUEST.token] the token obtained from the hosted forms
  */
-function Assets_stripeWebhook_post($params = array())
+function Assets_stripeWebhook_post()
 {
-	print 4523;
+	$secretKey = Q_Config::get('Assets', 'payments', 'stripe', 'secret', null);
+	if (!$secretKey) {
+		return;
+	}
+	\Stripe\Stripe::setApiKey($secretKey);
+	// Retrieve the request's body and parse it as JSON
+	$input = @file_get_contents("php://input");
+	$event_json = json_decode($input, true);
+	if ($event_json['type'] === 'charge.succeeded') {
+		$metadata =& $event_json['data']['object']['metadata'];
+		if (!empty($metadata) && $metadata['streamName']) {
+			$arr = explode('/', $metadata['streamName']);
+			array_pop($arr);
+			$streamName = implode('/', $arr);
+			switch($streamName) {
+				case 'Calendars/event';
+					break;
+				default:
+			}
+			Q::event('Calendars/event/webhook/Stripe/charge', $metadata);
+		}
+	};
+	http_response_code(200); // PHP 5.4 or greater
 	exit;
 }
