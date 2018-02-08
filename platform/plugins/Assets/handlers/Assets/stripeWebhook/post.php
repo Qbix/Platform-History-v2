7b9b08a878adfb7c22d70289d39e9994690f977f
Evgeny Kokhanovsky <evgeny@qbix.com>
2018-02-08 05:28:05 +0500

    Stripe webhook post handler

--- platform/plugins/Assets/handlers/Assets/stripeWebhook/post.php
+++ platform/plugins/Assets/handlers/Assets/stripeWebhook/post.php
@@ -1,15 +1,7 @@
 <?php
 
 /**
- * HTTP method for starting a payment
- * @param {array} $_REQUEST
- * @param {string} $_REQUEST.payments Required. Should be either "authnet" or "stripe"
- *  @param {String} [$_REQUEST.publisherId=Q.Users.communityId] The publisherId of the Assets/product or Assets/service stream
- *  @param {String} [$_REQUEST.streamName] The name of the Assets/product or Assets/service stream
- *  @param {Number} [$_REQUEST.description] A short name or description of the product or service being purchased.
- *  @param {Number} [$_REQUEST.amount] the amount to pay. 
- *  @param {String} [$_REQUEST.currency="usd"] the currency to pay in. (authnet supports only "usd")
- *  @param {String} [$_REQUEST.token] the token obtained from the hosted forms
+ * Stripe webhook https://stripe.com/docs/webhooks
  */
 function Assets_stripeWebhook_post()
 {
@@ -18,9 +10,10 @@ function Assets_stripeWebhook_post()
 		return;
 	}
 	\Stripe\Stripe::setApiKey($secretKey);
-	// Retrieve the request's body and parse it as JSON
 	$input = @file_get_contents("php://input");
 	$event_json = json_decode($input, true);
+
+	// charge.succeeded event
 	if ($event_json['type'] === 'charge.succeeded') {
 		$metadata =& $event_json['data']['object']['metadata'];
 		if (!empty($metadata) && $metadata['streamName']) {
@@ -32,9 +25,10 @@ function Assets_stripeWebhook_post()
 					break;
 				default:
 			}
-			Q::event('Calendars/event/webhook/Stripe/charge', $metadata);
+			Q::event('Calendars/event/webhook/Stripe/charge', $event_json);
 		}
 	};
+
 	http_response_code(200); // PHP 5.4 or greater
 	exit;
 }
