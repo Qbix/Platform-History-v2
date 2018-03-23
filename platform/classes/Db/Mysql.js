774e35ac5307272feb49d4f3505c61e7d2ba6bf6
Greg Magarshak <greg@qbix.com>
2018-03-23 12:51:29 -0400

    Changed offset to utcOffset in moment-timezone call

--- platform/classes/Db/Mysql.js
+++ platform/classes/Db/Mysql.js
@@ -144,7 +144,7 @@ function Db_Mysql(connName, dsn) {
 			});
 			var mt = require('moment-timezone');
 			var timezone = Q.Config.expect(['Q', 'defaultTimezone']);
-			var offset = mt.tz.zone(timezone).offset(Date.now());
+			var offset = mt.tz.zone(timezone).utcOffset(Date.now());
 		    var dt = new Date(
 				Math.abs(offset) * 60000 + new Date(2000, 0).getTime()
 		    ).toTimeString();
