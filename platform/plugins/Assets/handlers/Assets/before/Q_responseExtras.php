147e0c92baf964af46216964484353041dc05c7a
Evgeny Kokhanovsky <evgeny@qbix.com>
2018-04-04 17:44:57 +0500

    Fixed ios Adapter

--- platform/plugins/Assets/handlers/Assets/before/Q_responseExtras.php
+++ platform/plugins/Assets/handlers/Assets/before/Q_responseExtras.php
@@ -24,6 +24,10 @@ function Assets_before_Q_responseExtras() {
 	if (!empty($_GET['browsertab']) && $_GET['browsertab'] == 'yes') {
 		Q_Response::layoutView('Assets/browsertab.php');
 		Q_Response::addStylesheet('Q/plugins/Assets/css/browsertab.css');
+		$srcs = Q_Config::get("Assets", "browsertab", "css", array());
+		foreach ($srcs as $src) {
+			Q_Response::addStylesheet($src);
+		}
 		Q_Response::addScript('Q/plugins/Assets/js/browsertab.js');
 	}
 }
