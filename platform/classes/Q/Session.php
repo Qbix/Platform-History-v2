f4ff6e53c94824626fd92a87543a6fa5aa8fbead
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2018-06-15 16:07:02 +0300

    Applied patch "Qbix-Platform-1-Modern-Crypto.patch". After this patch applied, need to do "composer install" when install platform.

--- platform/classes/Q/Session.php
+++ platform/classes/Q/Session.php
@@ -924,7 +924,7 @@ class Q_Session
 	{
 		self::start();
 		if ($overwrite or !isset($_SESSION['Q']['nonce'])) {
-			$_SESSION['Q']['nonce'] = sha1(mt_rand().microtime());
+			$_SESSION['Q']['nonce'] = bin2hex(random_bytes(32));
 		}
 		if (!empty($_SERVER['HTTP_HOST'])) {
 			$durationName = self::durationName();
@@ -1051,7 +1051,11 @@ class Q_Session
 	 */
 	static function generateId()
 	{
-		$id = str_replace('-', '', Q_Utils::uuid());
+		if (is_callable('random_bytes')) {
+			$id = bin2hex(random_bytes(16));
+		} else {
+			$id = str_replace('-', '', Q_Utils::uuid());
+		}
 		$secret = Q_Config::get('Q', 'external', 'secret', null);
 		if (isset($secret)) {
 			$sig = Q_Utils::signature($id, "$secret");
@@ -1064,7 +1068,13 @@ class Q_Session
 			$id
 		);
 	}
-
+	/**
+	 * @param string $id
+	 *
+	 * @return array
+	 * @throws Q_Exception
+	 * @throws TypeError
+	*/
 	protected static function decodeId($id)
 	{
 		if (!$id) {
@@ -1100,7 +1110,7 @@ class Q_Session
 		$b = substr($result, 32, 32);
 		$secret = Q_Config::get('Q', 'external', 'secret', null);
 		$c = isset($secret)
-			? ($b === substr(Q_Utils::signature($a, $secret), 0, 32))
+			? Q_Utils::hashEquals($b, substr(Q_Utils::signature($a, $secret), 0, 32))
 			: true;
 		return array($c, $a, $b);
 	}
