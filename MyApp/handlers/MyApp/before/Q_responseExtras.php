8428e71cf29cc9c2b4d63016cb95db3cb1b4e46f
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2018-09-19 15:40:37 +0300

    Added to Q_responseExtras.php code to run an event for loading action-specific extras (if there are any).

--- MyApp/handlers/MyApp/before/Q_responseExtras.php
+++ MyApp/handlers/MyApp/before/Q_responseExtras.php
@@ -9,4 +9,13 @@ function MyApp_before_Q_responseExtras()
 	}
 	header('Vary: User-Agent');
 	Q_Response::addStylesheet('https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700', 'MyApp');
+
+	// running an event for loading action-specific extras (if there are any)
+	$uri = Q_Dispatcher::uri();
+	$module = $uri->module;
+	$action = $uri->action;
+	$event = "$module/$action/response/responseExtras";
+	if (Q::canHandle($event)) {
+		Q::event($event);
+	}
 }
