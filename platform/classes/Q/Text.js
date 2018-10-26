857865ba121090c260c9cca1c69094a3e02f1b69
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2018-10-26 18:24:47 +0300

    Make Q.Text.get Synchronously in Node. Implemented Q.Text.params in Node.

--- platform/classes/Q/Text.js
+++ platform/classes/Q/Text.js
@@ -3,6 +3,7 @@
  */
 
 var Q = require('../Q');
+var fs = require('fs');
 
 /**
  * Holds application config
@@ -15,29 +16,74 @@ var Text = {
 };
 
 /**
- * Loads data from text file
+ * Synchronously loads data from text file and parse to JSON
  * @method get
  * @param {string} filename The filename of the file to load.
  * @param {string} [language] Preferred language
- * @param {function} [callback=null] Function to call back, with params (err, data)
+ * @return {object} JSON object
  */
-Text.get = function (filename, language, callback) {
-	var preferredLanguage = Text.language;
+Text.get = function (filename, language) {
+	var preferredLanguage = language || Text.language;
 
-	if (typeof language === 'function') {
-		callback = language;
-	} else if (typeof language === 'string') {
-		preferredLanguage = language;
+	var path = Q.app.DIR + '/text/' + filename + '/' + preferredLanguage + '.json';
+	var data = fs.readFileSync(path, 'utf-8');
+
+	try {
+		data = data.replace(/\s*(?!<")\/\*[^\*]+\*\/(?!")\s*/gi, '');
+		data = JSON.parse(data);
+	} catch (e) {
+		return console.error(e.message);
 	}
 
-	(new Q.Tree()).load(Q.app.DIR + '/text/' + filename + '/' + preferredLanguage + '.json', function(err, data) {
-		if (err) {
-			if (err.code !== "ENOENT") {
-				return Q.handle(callback, null, [err]);
-			} else data = {};
+	return data;
+};
+
+/**
+ * Get parameters merged from all the text sources corresponding to a view template
+ * @method params
+ * @param {string} viewPath The parts of the view name, to use with Q/text config
+ * @param {object} [options] Object of options which will pass to Q.Text.get
+ * @return {array} The merged parameters that come from the text
+ */
+Text.params = function (viewPath, options) {
+	if(typeof viewPath === 'string') {
+		viewPath = viewPath.split('/');
+	}
+
+	options = options || {};
+
+	var count = viewPath.length;
+	var _try = [];
+	for (var i=0, j=0; i<=count; ++i) {
+		_try[j] = viewPath.slice(0);
+		if (i > 0) {
+			_try[j].splice(-i, i, '*');
 		}
-		Q.handle(callback, null, [null, data]);
-	});
+		++j;
+	}
+
+	count = _try.length;
+	var tree = new Q.Tree();
+	var p, text, options2;
+	for (j=0; j < count; ++j) {
+		p = ['Q', 'text'].concat(_try[j].slice(0));
+		if (text = Q.Config.get(p, null)) {
+			if (Array.isArray(text)) {
+				options2 = options;
+			} else if (typeof text === 'object') {
+				options2 = Q.extend(options, text);
+				if (!options2.sources) {
+					continue;
+				}
+				text = options2.sources;
+			} else {
+				continue;
+			}
+
+			tree.merge(Q.Text.get(text, options2.language));
+		}
+	}
+	return tree.getAll();
 };
 
 module.exports = Text;
