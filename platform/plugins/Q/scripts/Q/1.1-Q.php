c35d8315636f4d63ebad25abfbffbf7ccff6c918
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2018-11-14 16:20:37 +0300

    Added column 'type' (with default value "person") to table users_user. Regenerate models for Users plugin. Change type for all communities to 'community'. Add plugin users with type=plugin.

--- platform/plugins/Q/scripts/Q/1.1-Q.php
+++ platform/plugins/Q/scripts/Q/1.1-Q.php
@@ -1,51 +1,24 @@
 <?php
-$FROM_APP = defined('RUNNING_FROM_APP'); //Are we running from app or framework?
+// add 'type' column to users_user table (if not exist)
+$db = Db::connect('Users');
+$tableName = Users_User::table();
+$cols = $db->rawQuery("SHOW COLUMNS FROM ".$tableName." like 'type'")->execute()->fetchAll(PDO::FETCH_ASSOC);
+if (!$cols) {
+	$db->rawQuery("ALTER TABLE ".$tableName." ADD COLUMN type varchar(50) NOT NULL DEFAULT 'person' AFTER username")->execute();
 
-#Arguments
-$argv = $_SERVER['argv'];
-$count = count($argv);
-
-#Usage strings
-$usage = "Usage: php {$argv[0]} " . ($FROM_APP ? '' : '<app_root> ');
-
-if(!$FROM_APP) {
-	$usage.=PHP_EOL.PHP_EOL.'<app_root> must be a path to the application root directory';
+	// generate Models for Users plugin
+	$db->generateModels(Q_DIR.DS.'plugins'.DS.'Users'.DS.'classes');
 }
 
-$help = <<<EOT
-This script create users for each plugin mentioned in config/app.json under Q/plugins
-
-$usage
-EOT;
-
-#Is it a call for help?
-if (isset($argv[1]) and in_array($argv[1], array('--help', '/?', '-h', '-?', '/h')))
-	die($help);
-
-#Check primary arguments count: 1 if running /app/scripts/Q/q11.php, 2 if running /framework/scripts/q11.php
-if ($count < ($FROM_APP ? 1 : 2))
-	die($usage);
-
-#Read primary arguments
-$LOCAL_DIR = $FROM_APP ? APP_DIR : $argv[1];
-
-#Check paths
-if (!file_exists($Q_filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Q.inc.php')) #Q Platform
-	die("[ERROR] $Q_filename not found" . PHP_EOL);
-
-if (!is_dir($LOCAL_DIR)) #App dir
-	die("[ERROR] $LOCAL_DIR doesn't exist or is not a directory" . PHP_EOL);
-
-#Define APP_DIR
-if (!defined('APP_DIR'))
-	define('APP_DIR', $LOCAL_DIR);
-
-#Include Q
-try {
-	include($Q_filename);
-} catch (Exception $e) {
-	die('[ERROR] ' . $e->getMessage() . PHP_EOL . $e->getTraceAsString() . PHP_EOL);
+// set type=community for all communities users
+$communities = $db->rawQuery("select id from ".$tableName." where type='person' and signedUpWith='none'")->fetchDbRows();
+foreach($communities as $community) {
+	if (ctype_upper($community->id{0})) {
+		// need to use rawQuery because models doesn't updated yet
+		$db->rawQuery("update ".$tableName." set type='community' where id='".$community->id."'")->execute();
+	}
 }
+
 
 $plugins = Q_Config::expect('Q', 'plugins');
 
@@ -64,5 +37,9 @@ foreach($plugins as $plugin) {
 	$user->icon = "{{baseUrl}}/img/icons/".$user->id;
 	$user->signedUpWith = 'none';
 	$user->save();
+
+	// need to use rawQuery because models doesn't updated yet
+	$db->rawQuery("update ".$tableName." set type='plugin' where id='".$user->id."'")->execute();
+
 	echo "\t" . 'user created successfully' . PHP_EOL;
 }
