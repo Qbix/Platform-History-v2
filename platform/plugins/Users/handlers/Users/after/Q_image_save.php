8346e07ce6ec89a652253c9403609cd2e652cf80
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2018-11-29 14:53:55 +0300

    Fetch id from $subpath and check whether logged user can modify icon of another user. And if yes - change icon.

--- platform/plugins/Users/handlers/Users/after/Q_image_save.php
+++ platform/plugins/Users/handlers/Users/after/Q_image_save.php
@@ -9,6 +9,7 @@ function Users_after_Q_image_save($params, &$return)
 	 * @var Users_User $user
 	 */
 	$user = Users::loggedInUser(true);
+
 	$fullpath = $path.($subpath ? DS.$subpath : '');
 	Q_Utils::normalizePath($fullpath);
 
@@ -16,13 +17,38 @@ function Users_after_Q_image_save($params, &$return)
 	$prefix = "Q/uploads/Users/$splitId/icon";
 	Q_Utils::normalizePath($prefix);
 
-	if (substr($fullpath, 0, strlen($prefix)) === $prefix) {
+	if (substr($fullpath, 0, strlen($prefix)) === $prefix) { // modification of logged user icon
 		if ($user->icon != $subpath) {
 			$user->icon = Q_Html::themedUrl("$path/$subpath");
 			$user->save(); // triggers any registered hooks
 			Users::$cache['iconUrlWasChanged'] = true;
 		} else {
 			Users::$cache['iconUrlWasChanged'] = false;
+		}
+	} else { // modification of another user
+		// trying to fetch userId from subpath
+		$anotherUserId = preg_replace('/\/icon.*/', '', $subpath);
+		$anotherUserId = preg_replace('/\//', '', $anotherUserId);
+
+		$anotherUser = Users_User::fetch($anotherUserId, false);
+
+		if (!$anotherUser) {
+			return;
+		}
+
+		$permitted = array_keys(Users::byRoles(array("Communities/admins", "Users/owners")));
+
+		// check whether logged user have permissiosn to change icon of another user
+		if (in_array($anotherUserId, $permitted)) {
+			if ($anotherUser->icon != $subpath) {
+				$anotherUser->icon = Q_Html::themedUrl("$path/$subpath");
+				$anotherUser->save(); // triggers any registered hooks
+				Users::$cache['iconUrlWasChanged'] = true;
+			} else {
+				Users::$cache['iconUrlWasChanged'] = false;
+			}
+		} else {
+			throw new Users_Exception_NotAuthorized();
 		}
 	}
 }
