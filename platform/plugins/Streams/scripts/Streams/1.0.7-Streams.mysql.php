500056df002e4603753e8ecc8c14129ac92de95d
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-01-21 11:15:11 +0300

    Fixed bug in 1.0.7-Streams.mysql.php installation script.

--- platform/plugins/Streams/scripts/Streams/1.0.7-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/1.0.7-Streams.mysql.php
@@ -6,12 +6,13 @@ function Streams_0_1_7_Streams()
 	$dir = APP_WEB_DIR.DS.'Q'.DS.'views'.DS.'Streams';
 	$to = $dir.DS.'templates';
 	if (!file_exists($to)) {
-		if (!file_exists($dir)) {
-			mkdir($dir, 0777, true);
-		}
-		Q_Utils::symlink($from, $to);
+		mkdir($to, 0777, true);
 	}
-	
+	if (!file_exists($dir)) {
+		mkdir($dir, 0777, true);
+	}
+	Q_Utils::symlink($from, $to);
+
 	// symlink the icons folder
 	/*Q_Utils::symlink(
 		STREAMS_PLUGIN_FILES_DIR.DS.'Streams'.DS.'icons',
