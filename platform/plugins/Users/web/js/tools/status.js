bb755be16487525b122d97e0f759f73f75dd8b75
Greg Magarshak <greg@qbix.com>
2019-01-29 03:17:53 -0500

    Removing passenger location on logout

--- platform/plugins/Users/web/js/tools/status.js
+++ platform/plugins/Users/web/js/tools/status.js
@@ -12,13 +12,14 @@ var Users = Q.Users;
  * @class Users status
  * @constructor
  * @param {Object} [options] this object contains function parameters
+ *   @param {Q.Event} [options.onInvoke] When the tool is clicked
  *	 @param {String} [options.avatar] Options for the user avatar
  *	 @param {String} [options.avatar.icon=80] The default size of the avatar icon
  *	 @param {String} [options.avatar.contents=!Q.info.isMobile] Whether to show the name
  *	 @param {String} [options.avatar.short=true] Whether the name shown should be short
  */
 Q.Tool.define("Users/status", function(options) {
-	this.refresh(this.element.innerHTML);
+	this.refresh(!!this.element.innerHTML);
 	Q.Users.onLogin
 	.and(Q.Users.onLogout)
 	.set(this.refresh.bind(this), this);
@@ -28,19 +29,26 @@ Q.Tool.define("Users/status", function(options) {
 		icon: 80,
 		contents: !Q.info.isMobile,
 		short: true
-	}
+	},
+	onInvoke: new Q.Event()
 },
 {
 	refresh: function (skipRefill) {
 		var tool = this;
 		var state = tool.state;
 		if (Q.Users.loggedInUser) {
-			if (!skipRefill) {
-				var element = Q.Tool.setUpElement('div', 'Users/avatar', state.avatar);
-				element.innerHTML = '';
-				tool.element.appendChild(element);
-				Q.activate(element);
+			if (skipRefill === false) {
+				var avatar = $('<div />').tool('Users/avatar', state.avatar);
+				$(tool.element).empty().append(
+					$('<div class="Users_whenLoggedIn Users_status_avatar" />')
+					.append(avatar)
+				).activate();
 			}
+			tool.$('.Users_status_avatar')
+			.plugin('Q/clickable')
+			.on(Q.Pointer.click, tool, function () {
+				Q.handle(state.onInvoke);
+			});
 			_wireup();
 		} else {
 			Q.Text.get('Users/content', function (err, text) {
