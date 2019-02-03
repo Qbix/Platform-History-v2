6a0ab777fa85715a325591ffbdd281c80ee86a7f
Greg Magarshak <greg@qbix.com>
2019-02-02 19:45:20 -0500

    FIxed Streams symlink in installer

--- platform/plugins/Streams/scripts/Streams/1.0.7-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/1.0.7-Streams.mysql.php
@@ -6,12 +6,11 @@ function Streams_0_1_7_Streams()
 	$dir = APP_WEB_DIR.DS.'Q'.DS.'views'.DS.'Streams';
 	$to = $dir.DS.'templates';
 	if (!file_exists($to)) {
-		mkdir($to, 0777, true);
+		if (!file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		Q_Utils::symlink($from, $to);
 	}
-	if (!file_exists($dir)) {
-		mkdir($dir, 0777, true);
-	}
-	Q_Utils::symlink($from, $to);
 
 	// symlink the icons folder
 	/*Q_Utils::symlink(
