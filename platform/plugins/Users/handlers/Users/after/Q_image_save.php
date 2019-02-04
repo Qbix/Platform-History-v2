74a6b925ac43ef6847057a50428ec75123f96f1b
Gregory Magarshak <greg@qbix.com>
2019-02-04 16:06:02 -0500

    Update Q_image_save.php

--- platform/plugins/Users/handlers/Users/after/Q_image_save.php
+++ platform/plugins/Users/handlers/Users/after/Q_image_save.php
@@ -44,7 +44,7 @@ function Users_after_Q_image_save($params, &$return)
 				$user->save();
 			}
 		}
-	} elseif (Q::startsWith($fullpath, implode(DS, array('Q', 'uploads', 'Users')))) {
+	} else if (Q::startsWith($fullpath, implode(DS, array('Q', 'uploads', 'Users')))) {
 		// modification of another user
 		// trying to fetch userId from subpath
 		$anotherUserId = preg_replace('/\/icon.*/', '', $subpath);
