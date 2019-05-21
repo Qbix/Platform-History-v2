2684894d3b9e916895f50981214fa369e4ef8ce6
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-05-21 13:23:33 +0300

    Fixed session.js First time I tested, there required function handleOpenUrl to process urls with scheme yang2020:// But after last update this function not exist and need to redirect document to url.

--- platform/plugins/Users/web/js/pages/session.js
+++ platform/plugins/Users/web/js/pages/session.js
@@ -1,14 +1,6 @@
 Q.page("Users/session", function () {
-	var url = Q.getObject("Q.Cordova.handoff.url");
-
-	console.log("Trying to redirect to:", url);
-
-	var timeId = setInterval(function () {
-		if (typeof handleOpenURL === 'function') {
-			clearInterval(timeId);
-			handleOpenURL(url);
-		}
-	}, 500);
+	
+	document.location.href = Q.getObject("Q.Cordova.handoff.url");
 
 	return function () {
 		// code to execute before page starts unloading
