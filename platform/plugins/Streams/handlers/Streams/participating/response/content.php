7558c6e19974c3154728b9fd74b618f7a6c31e58
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-05-23 15:50:59 +0300

    Updated Streams/participating page. Added option $skipOwnStreams Order streams by subscription. Use table in view. Added Streams/participants tool.

--- platform/plugins/Streams/handlers/Streams/participating/response/content.php
+++ platform/plugins/Streams/handlers/Streams/participating/response/content.php
@@ -3,14 +3,16 @@
 function Streams_participating_response_content()
 {
 	$loggedUserId = Users::loggedInUser(true)->id;
-	//$participants = Streams::participating(null, array('streamsOnly' => true));
 	$participants = Db::connect('Streams')->rawQuery("select srt.*, sp.state, sp.subscribed from 
 	streams_related_to srt, streams_participant sp 
 	where srt.toPublisherId='".$loggedUserId."' and srt.toStreamname='Streams/participating'
-	and sp.publisherId=srt.fromPublisherId and sp.streamName=srt.fromStreamName and sp.userId='".$loggedUserId."'")->fetchDbRows();
+	and sp.publisherId=srt.fromPublisherId and sp.streamName=srt.fromStreamName and sp.userId='".$loggedUserId."' 
+	order by sp.subscribed,sp.streamType")->fetchDbRows();
+
+	$skipOwnStreams = Q_Config::get('Streams', 'participating', 'skipOwnStreams', true);
 
 	Q_Response::addStylesheet("{{Streams}}/css/pages/participants.css");
 	Q_Response::addScript("{{Streams}}/js/pages/participants.js");
 
-	return Q::view("Streams/content/participating.php", compact('participants', 'loggedUserId'));
+	return Q::view("Streams/content/participating.php", compact('participants', 'loggedUserId', 'skipOwnStreams'));
 }
--- platform/plugins/Streams/views/Streams/content/participating.php
+++ platform/plugins/Streams/views/Streams/content/participating.php
@@ -1,12 +1,30 @@
 <div id="content" class="Streams_participating">
+    <table class="Streams_participating_stream">
+        <tr>
+            <th><?php echo $participating['Title'] ?></th>
+            <th><?php echo $participating['Subscribed'] ?></th>
+            <th><?php echo $participating['Participants'] ?></th>
+        </tr>
     <?php foreach($participants as $participant) {
 		$stream = Streams::fetchOne($loggedUserId, $participant->fromPublisherId, $participant->fromStreamName);
 		$checked = $participant->subscribed == 'yes' ? 'checked' : '';
+		$iconUrl = $stream->iconUrl('40.png');
+
+		if ($skipOwnStreams && $stream->publisherId == $loggedUserId) {
+		    continue;
+        }
     ?>
-		<div class="Streams_participating_stream">
-            <img class="Streams_participating_stream_icon" src="<?php echo $stream->iconUrl('40.png') ?>" />
-            <h2><?php echo $stream->title ?></h2>
-            <input type="checkbox" class="Streams_participating_stream_checkmark" data-publisherId="<?php echo $stream->publisherId ?>" name="<?php echo $stream->name ?>" <?php echo $checked?>>
-        </div>
+        <tr></tr>
+		<tr>
+            <td data-type="title">
+                <img class="Streams_participating_stream_icon" src="<?php echo $iconUrl ?>" onerror="this.style.width=0;this.style.visibility='hidden';" />
+                <h2 title="<?php echo $stream->title ?>"><?php echo $stream->title ?></h2>
+            </td>
+            <td data-type="checkmark">
+                <input type="checkbox" class="Streams_participating_stream_checkmark" data-publisherId="<?php echo $stream->publisherId ?>" name="<?php echo $stream->name ?>" <?php echo $checked?>>
+            </td>
+            <td data-type="participants" data-publisherId="<?php echo $stream->publisherId ?>" data-streamName="<?php echo $stream->name ?>"></td>
+        </tr>
 	<?php } ?>
+    </table>
 </div>
