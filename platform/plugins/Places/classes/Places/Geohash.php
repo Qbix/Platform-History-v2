6009bb2f6a93004a6e3303fe5aefccaee0582876
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-06-03 18:50:06 +0300

    Fixed Palces_Geohash class.

--- platform/plugins/Places/classes/Places/Geohash.php
+++ platform/plugins/Places/classes/Places/Geohash.php
@@ -196,11 +196,13 @@ class Places_Geohash
 	    while ($k < $limit && $j < $b) {
     		$result[] = $below[$j];
     		++$j;
+    		++$k;
 	    }
 
+	    return $result;
     }
 
-    private function closer($center, $a, $b) {
+    private static function closer($center, $a, $b) {
     	$cn = self::alpha2num($center);
     	$an = self::alpha2num($a);
     	$bn = self::alpha2num($b);
@@ -213,7 +215,7 @@ class Places_Geohash
 	 * @return string The converted number.
 	 * @author Theriault
 	 */
-	private function alpha2num($a) {
+	private static function alpha2num($a) {
 		$r = 0;
 		$l = strlen($a);
 		for ($i = 0; $i < $l; $i++) {
