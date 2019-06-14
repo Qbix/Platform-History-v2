b27c4a935b561fd648de27e198a6bbf21e99b742
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-06-14 18:57:22 +0300

    Modified tool Assets/payment. Set button text to just "Pay". Added shipping options.

--- platform/plugins/Assets/web/js/tools/payment.js
+++ platform/plugins/Assets/web/js/tools/payment.js
@@ -17,7 +17,9 @@
  *  @param {string} [options.image] The url pointing to a square image of your brand or product. The recommended minimum size is 128x128px.
  *  @param {string} [options.description=null] A short name or description of the product or service being purchased.
  *  @param {boolean} [options.allowRememberMe=true] Specify whether to include the option to "Remember Me" for future purchases (true or false).
- *  @param {boolean} [options.bitcoin=false] Specify whether to accept Bitcoin (true or false). 
+ *  @param {boolean} [options.billingAddress=false] Specify whether to include the option to set billing address.
+ *  @param {boolean} [options.shippingAddress=false] Specify whether to include the option to set shipping address.
+ *  @param {boolean} [options.bitcoin=false] Specify whether to accept Bitcoin (true or false).
  *  @param {boolean} [options.alipay=false] Specify whether to accept Alipay ('auto', true, or false). 
  *  @param {boolean} [options.alipayReusable=false] Specify if you need reusable access to the customer's Alipay account (true or false).
  */
@@ -65,6 +67,8 @@ Q.Tool.define("Assets/payment", function (options) {
 	payButton: null,
 	description: null,
 	allowRememberMe: true,
+	shippingAddress: false,
+	billingAddress: false,
 	bitcoin: false,
 	alipay: false,
 	alipayReusable: false,
@@ -94,9 +98,9 @@ Q.Tool.define("Assets/payment", function (options) {
 
 		Q.Template.render(
 			templateName,
-			Q.extend({}, data, {
-				payButton: "Pay with " + payments
-			}),
+			{
+				text: data.text.payment
+			},
 			function (err, html) {
 				if (err) return;
 
@@ -207,12 +211,12 @@ Q.Template.set('Assets/payment/Gpay',
 	'<button class="Q_button Assets_gpay">&nbsp;</button>'
 );
 Q.Template.set('Assets/payment/Stripe',
-	'<button class="Q_button Assets_pay">{{payButton}}</button>'
+	'<button class="Q_button Assets_pay">{{text.Pay}}</button>'
 );
 Q.Template.set('Assets/payment/Authnet',
 	'<form method="post" target="Assets_authnet" action="{{action}}">' +
 	'<input type="hidden" name="Token" value="{{token}}">' +
-	'<button class="Q_button Assets_pay">{{payButton}}</button>' +
+	'<button class="Q_button Assets_pay">{{text.Pay}}</button>' +
 	'</form>'
 );
 
