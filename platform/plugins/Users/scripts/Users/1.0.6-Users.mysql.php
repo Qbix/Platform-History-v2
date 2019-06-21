dbbbf32626256be2643b67e36c45a8c3cb2860fb
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-06-21 18:51:58 +0300

    During installation of Users plugin add directories local/Users/certs/[appName]/token and local/Users/certs/[appName]/sandbox These certs required for iOS notifications.

--- platform/plugins/Users/scripts/Users/1.0.6-Users.mysql.php
+++ platform/plugins/Users/scripts/Users/1.0.6-Users.mysql.php
@@ -2,6 +2,7 @@
 	
 function Users_1_0_6_Users()
 {
+	// create templates dir
 	$from = USERS_PLUGIN_VIEWS_DIR.DS.'Users'.DS.'templates';
 	$dir = APP_WEB_DIR.DS.'Q'.DS.'views'.DS.'Users';
 	$to = $dir.DS.'templates';
@@ -10,6 +11,19 @@ function Users_1_0_6_Users()
 			mkdir($dir, 0777, true);
 		}
 		Q_Utils::symlink($from, $to);
+	}
+
+	// create ios token/cert.p8 and sandbox/bundle.pem
+	$from = USERS_PLUGIN_FILES_DIR.DS.'Users'.DS.'ios';
+	$dir = APP_DIR.DS.'local'.DS.'Users'.DS.'certs'.DS.Q::app();
+	if (!file_exists($dir)) {
+		mkdir($dir, 0755, true);
+	}
+	if (!file_exists($dir.DS.'sandbox')) {
+		Q_Utils::symlink($from.DS.'sandbox', $dir.DS.'sandbox');
+	}
+	if (!file_exists($dir.DS.'token')) {
+		Q_Utils::symlink($from.DS.'token', $dir.DS.'token');
 	}
 }
 
