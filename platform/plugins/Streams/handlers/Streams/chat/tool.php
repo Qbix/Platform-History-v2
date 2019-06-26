7de4d25529c7376548cb7efa99f06b70afda35b9
Greg Magarshak <greg@qbix.com>
2019-06-26 19:29:53 -0400

    Fixed a typo in Streams/chat/tool refactor

--- platform/plugins/Streams/handlers/Streams/chat/tool.php
+++ platform/plugins/Streams/handlers/Streams/chat/tool.php
@@ -11,6 +11,7 @@
  * @param {array} $options Options for the tool
  * @param {string} $options.publisherId Publisher id of the stream to get messsages from.
  * @param {string} $options.streamName Required. Name of the stream to get messsages from.
+ * @param {Streams_Stream} [$options.stream] You can pass this instead of publisherId and streamName
  * @param {string} [$options.loadMore] May have one these values: 'scroll', 'click' or 'pull' which indicates what kind of algorithm will be used for loading new messages. 'scroll' means that new messages will be loaded when scrollbar of the chat cointainer reaches the top (for desktop) or whole document scrollbar reaches the top (for android). 'click' will show label with 'Click to see earlier messages' and when user clicks it, new messages will be loaded. Finally, 'pull' implements 'pull-to-refresh' behavior used in many modern applications today when new messages loaded by rubber-scrolling the container by more amount than it actually begins. Defaults to 'scroll' for desktop and Android devices and 'pull' for iOS devices.
 */
 function Streams_chat_tool($options)
@@ -31,7 +32,7 @@ function Streams_chat_tool($options)
 	if (isset($stream)) {
 		$options['publisherId'] = $stream->publisherId;
 		$options['streamName'] = $stream->name;
-		unset($options);
+		unset($options['stream']);
 	} else {
 		if (!isset($publisherId)) {
 			$publisherId = Streams::requestedPublisherId(true);
