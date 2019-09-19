bf89b2cb80fbb180d8a142edbddcd94c3361f709
Greg Magarshak <greg@qbix.com>
2019-09-19 12:56:27 -0400

    Made Q/columns tool remove tools before replacing html of titleSlot, controlsSlot, columnSlot

--- platform/plugins/Q/web/js/tools/columns.js
+++ platform/plugins/Q/web/js/tools/columns.js
@@ -427,13 +427,16 @@ Q.Tool.define("Q/columns", function(options) {
 				params.handler = function _handler(response) {
 					var elementsToActivate = {};
 					if ('title' in response.slots) {
+						Q.Tool.remove(titleSlot);
 						$(titleSlot).html(response.slots.title);
 						elementsToActivate['title'] = titleSlot;
 					}
 					if ('controls' in response.slots) {
+						Q.Tool.remove(controlsSlot);
 						$(controlsSlot).html(response.slots.controls);
 						elementsToActivate['controls'] = controlsSlot;
 					}
+					Q.Tool.remove(columnSlot);
 					columnSlot.innerHTML = response.slots.column;
 					elementsToActivate['column'] = columnSlot;
 					return elementsToActivate;
