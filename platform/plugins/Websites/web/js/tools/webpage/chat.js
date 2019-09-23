71354f1cdf0b8c5d27209f10cf4743b50aef646f
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-09-23 18:23:56 +0300

    Last fixes in Websites/webpage/preview tool.

--- platform/plugins/Websites/web/js/tools/webpage/chat.js
+++ platform/plugins/Websites/web/js/tools/webpage/chat.js
@@ -4,9 +4,13 @@
 	 * @class Websites/webpage/chat
 	 * @constructor
 	 * @param {Object} [options] this is an object that contains parameters for this function
+	 * @param {string} [options.appendTo=bubble] Where to append preview tool in chat message:
+	 * 	bubble - inside bubble element of message,
+	 * 	message - inside message element itself
 	 */
 	Q.Tool.define("Websites/webpage/chat", ["Streams/chat"], function (options) {
 		var tool = this;
+		var state = this.state;
 		tool.chatTool = Q.Tool.from(this.element, "Streams/chat");
 
 		// preload throbber
@@ -14,13 +18,14 @@
 
 		Q.addStylesheet('{{Websites}}/css/tools/webpage/chat.css');
 
-		$(tool.chatTool.element).on('input', '.Streams_chat_composer :input', Q.debounce(function () {
+		$(tool.chatTool.element).on('input', '.Streams_chat_composer input', Q.debounce(function () {
 			tool.process(this);
 		}, 500));
 
 		// on before message post
 		tool.chatTool.state.beforePost.set(function (fields) {
 			var $preview = tool.getActivePreview();
+			state.websitesPreview = {};
 			if (!$preview) {
 				return;
 			}
@@ -34,21 +39,33 @@
 				}
 			});
 
-			Q.Tool.remove(previewTool.element, true, true);
-			tool.state.websitesPreview = {};
+			tool.removePreview(previewTool.element);
 		}, tool);
 
 		// on new message render
 		tool.chatTool.state.onMessageRender.set(function (fields, html) {
-			var instructions = Q.getObject('Websites/webpages', JSON.parse(fields.instructions || null));
-
-			if (!instructions) {
-				return;
-			}
-
 			var $html = $(html);
 
-			$(Q.Tool.setUpElementHTML('div', 'Websites/webpage/preview', instructions)).appendTo($(".Streams_chat_bubble", $html));
+			// parse all links in message
+			var $chatMessageContent = $(".Streams_chat_message_content", $html);
+			var chatMessageContent = $chatMessageContent.html();
+			Q.each(chatMessageContent.matchTypes('url'), function (i, url) {
+				var href = url;
+				if (href.indexOf('//') === -1) {
+					href = '//' + href;
+				}
+
+				chatMessageContent = chatMessageContent.replace(url, "<a href='" + href + "' target='_blank'>" + url + "</a>");
+			});
+			$chatMessageContent.html(chatMessageContent);
+
+			var instructions = Q.getObject('Websites/webpages', JSON.parse(fields.instructions || null));
+
+			if (instructions) {
+				var elementToAppend = state.appendTo === 'bubble' ? $(".Streams_chat_bubble", $html) : $html;
+				instructions.editable = false;
+				$(Q.Tool.setUpElementHTML('div', 'Websites/webpage/preview', instructions)).appendTo(elementToAppend);
+			}
 
 			fields.html = $html[0].outerHTML;
 		}, tool);
@@ -62,11 +79,12 @@
 					window.open(previewTool.state.url, '_blank');
 				});
 			}
-		}, true);
+		}, tool);
 	},
 
 	{
-		websitesPreview: {}
+		websitesPreview: {},
+		appendTo: 'bubble'
 	},
 
 	{
@@ -79,12 +97,13 @@
 		process: function (input) {
 			var tool = this;
 			var $input = $(input);
+			var inputVal = $input.val();
 			var $form = $input.closest("form");
 			var websitesPreview = this.state.websitesPreview;
-			var urls = $input.val().matchTypes('url');
+			var urls = inputVal.matchTypes('url');
 
 			Q.each(urls, function (i, url) {
-				if (tool.getActivePreview() || url in websitesPreview) {
+				if (tool.getActivePreview() || url in websitesPreview || inputVal.endsWith(url)) {
 					return;
 				}
 
@@ -95,24 +114,28 @@
 						return;
 					}
 
-					websitesPreview[url] = $(html).insertBefore($form);
+					websitesPreview[url] = $(html).prependTo($form);
 
-					$('a.Q_close', websitesPreview[url]).on(Q.Pointer.fastclick, function () {
-						websitesPreview[url].remove();
+					var _close = function () {
+						tool.removePreview(websitesPreview[url]);
 						websitesPreview[url] = null;
-					});
+						tool.process(input);
+					};
+
+					$('a.Q_close', websitesPreview[url]).on(Q.Pointer.fastclick, _close);
 
 					Q.req('Websites/scrape', ['result', 'publisherId', 'streamName'], function (err, response) {
 						var msg = Q.firstErrorMessage(err, response && response.errors);
-						if (msg || !websitesPreview[url]) {
-							return;
+						if (msg) {
+							return _close();
 						}
 
 						var siteData = response.slots.result;
 
-						websitesPreview[url].tool("Websites/webpage/preview", {
+						$(".Websites_webpage_preview_tool", websitesPreview[url]).tool("Websites/webpage/preview", {
 							publisherId: response.slots.publisherId,
-							streamName: response.slots.streamName
+							streamName: response.slots.streamName,
+							editable: false
 						}, Date.now()).activate(function () {
 							var previewTool = this;
 							var $te = $(previewTool.element);
@@ -122,11 +145,6 @@
 							$te.off(Q.Pointer.fastclick).on(Q.Pointer.fastclick, function () {
 								window.open(siteData.url, '_blank');
 							});
-
-							$('<a class="Q_close"></a>').on(Q.Pointer.fastclick, function () {
-								websitesPreview[url] = null;
-								Q.Tool.remove(previewTool.element, true, true);
-							}).appendTo(previewTool.element);
 						});
 					}, {
 						method: 'post',
@@ -146,15 +164,33 @@
 
 			for (var url in websitesPreview) {
 				if (websitesPreview[url]) {
-					return websitesPreview[url];
+					return $(".Websites_webpage_preview_tool", websitesPreview[url]);
 				}
 			}
 
 			return null;
+		},
+		/**
+		 *	Correctly remove preview element
+		 *
+		 * @method removePreview
+		 * @param {HTMLElement|jQuery} element Element need to remove
+		 */
+		removePreview: function (element) {
+			if (element instanceof HTMLElement) {
+				element = $(element);
+			}
+
+			var $parent = element.closest(".Websites_webpage_chat");
+			Q.Tool.remove(element[0], true, true);
+			$parent.remove();
 		}
 	});
 
 	Q.Template.set('Websites/webpage/chat',
-		'<div class="Websites_webpage_preview_tool"><img src="{{src}}"><a class="Q_close"></a></div>'
+		'<div class="Websites_webpage_chat">' +
+		'	<div class="Websites_webpage_preview_tool"><img src="{{src}}"></div>' +
+		'	<a class="Q_close"></a>' +
+		'</div>'
 	);
 })(Q, Q.$, window);
