29a10d0b06e92e713157f5d20f4ed73c2c9c3d4c
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-10-09 16:16:06 +0300

    Added metas to MyApp welcome page.

--- MyApp/handlers/MyApp/welcome/response/content.php
+++ MyApp/handlers/MyApp/welcome/response/content.php
@@ -6,6 +6,29 @@ function MyApp_welcome_response_content($params)
 	$tabs = array("foo" => "bar");
 	$description = "this is a description";
 	Q_Response::addScript('js/pages/welcome.js');
+
+	// set meta tags
+	$communityId = Users::communityId();
+	$communityUser = Users_User::fetch($communityId);
+	$text = Q_Text::get($communityId.'/content');
+	$title = Q::text($text['welcome']['Title'], array($communityId));
+	$description = Q::text($text['welcome']['Description'], array($communityId));
+	$communityIcon = Q_Uri::interpolateUrl($communityUser->icon.'/400.png');
+	$url = Q_Uri::url($communityId.'/welcome');
+	Q_Response::setMeta(array(
+		array('attrName' => 'name', 'attrValue' => 'title', 'content' => $title),
+		array('attrName' => 'property', 'attrValue' => 'og:title', 'content' => $title),
+		array('attrName' => 'property', 'attrValue' => 'twitter:title', 'content' => $title),
+		array('attrName' => 'name', 'attrValue' => 'description', 'content' => $description),
+		array('attrName' => 'property', 'attrValue' => 'og:description', 'content' => $description),
+		array('attrName' => 'property', 'attrValue' => 'twitter:description', 'content' => $description),
+		array('attrName' => 'name', 'attrValue' => 'image', 'content' => $communityIcon),
+		array('attrName' => 'property', 'attrValue' => 'og:image', 'content' => $communityIcon),
+		array('attrName' => 'property', 'attrValue' => 'twitter:image', 'content' => $communityIcon),
+		array('attrName' => 'property', 'attrValue' => 'og:url', 'content' => $url),
+		array('attrName' => 'property', 'attrValue' => 'twitter:url', 'content' => $url)
+	));
+
 	return Q::view('MyApp/content/welcome.php', compact('tabs', 'description'));
 }
 
