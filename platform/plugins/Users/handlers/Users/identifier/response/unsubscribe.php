18ef5843e1ebc6ce42d4e1c052c3329592a67ab2
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-12-21 15:26:54 +0300

    If modified Users_User emailAddress or mobileNumber, modify also Streams/user/emailAddress or Streams/user/mobileNumber streams.

---
+++ platform/plugins/Users/handlers/Users/identifier/response/unsubscribe.php
@@ -0,0 +1,22 @@
+<?php
+
+function Users_identifier_response_unsubscribe($params)
+{
+	Q_Valid::nonce(true);
+
+	$user = Users::loggedInUser(true);
+
+	$r = array_merge($_REQUEST, $params);
+	$fields = Q::take($r, array('identifier', 'type'));
+	$identifier = $fields['identifier'];
+	$type = $fields['type'];
+
+	$dbStreams = Db::connect('Streams');
+	if ($type == 'email') {
+		$dbStreams->rawQuery("update users_email set state='unsubscribed' where userId='".$user->id."' and address='".$identifier."'")->execute();
+	} elseif ($type == 'mobile') {
+		$dbStreams->rawQuery("update users_mobile set state='unsubscribed' where userId='".$user->id."' and number='".$identifier."'")->execute();
+	}
+
+	return true;
+}
