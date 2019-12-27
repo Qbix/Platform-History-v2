1228a4101c069ce5bc04ed273ffd9cd73317462b
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2019-12-27 15:20:48 +0300

    Define Q.info.baseUrl on the first step Q.info definition. This will allow to use Q.url() correctly outside of Q.page().

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -10546,6 +10546,7 @@ Q.info = {
 	isStandalone: detected.isStandalone,
 	isCordova: _isCordova,
 	platform: detected.OS,
+	baseUrl: location.protocol + '//' + location.host,
 	browser: detected,
 	isIE: function (minVersion, maxVersion) {
 		return Q.info.browser.name === 'explorer'
