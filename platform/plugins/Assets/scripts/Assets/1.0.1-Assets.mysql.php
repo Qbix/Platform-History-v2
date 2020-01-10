6aa4ac583594f8a44cc6e799c91832af6af0da29
Greg Magarshak <greg@qbix.com>
2020-01-10 01:43:42 -0500

    Fixed Assets installer script

--- platform/plugins/Assets/scripts/Assets/1.0.1-Assets.mysql.php
+++ platform/plugins/Assets/scripts/Assets/1.0.1-Assets.mysql.php
@@ -6,8 +6,8 @@ function Assets_1_0_1()
 	$from = ASSETS_PLUGIN_WEB_DIR.DS.'.well-known'.DS.$filename;
 	$dir = APP_WEB_DIR.DS.'.well-known';
 	$to = $dir.DS.$filename;
-	if (!file_exists($to)) {
-		mkdir($to, 0755);
+	if (!file_exists($dir)) {
+		mkdir($dir, 0755);
 		Q_Utils::copy($from, $to);
 	}
 }
