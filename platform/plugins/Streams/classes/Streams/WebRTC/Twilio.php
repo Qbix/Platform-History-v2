9ea8e41dcf15379d4ca6fdb3408e5de371bccd9b
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-02-10 18:09:51 +0300

    Fixed bug with using Users_Quota. Also created method Streams_WebRTC::getOrCreateStream and use it in Node and Twilio versions.

--- platform/plugins/Streams/classes/Streams/WebRTC/Twilio.php
+++ platform/plugins/Streams/classes/Streams/WebRTC/Twilio.php
@@ -28,20 +28,10 @@ class Streams_WebRTC_Twilio extends Streams_WebRTC implements Streams_WebRTC_Int
         if (empty($publisherId)) {
             throw new Q_Exception_RequiredField(array('field' => 'publisherId'));
         }
-        if (!empty($roomId)) {
-            $streamName = "Streams/webrtc/$roomId";
-            $stream = Streams::fetchOne($publisherId, $publisherId, $streamName);
-            if (!$stream) {
-                $stream = Streams::create($publisherId, $publisherId, 'Streams/webrtc', array(
-                    'name' => $streamName
-                ));
-            }
-        } else {
-            $stream = Streams::create($publisherId, $publisherId, 'Streams/webrtc');
-            $roomId = substr($stream->name, strlen('Streams/webrtc/'));
-        }
 
-        $created = false;
+		$stream = Streams_WebRTC::getOrCreateStream($publisherId, $roomId);
+		$roomId = substr($stream->name, strlen('Streams/webrtc/'));
+
         try {
             $twilioRoom = $this->getTwilioRoom($roomId, $publisherId);
         } catch (Exception $e) {
@@ -50,7 +40,6 @@ class Streams_WebRTC_Twilio extends Streams_WebRTC implements Streams_WebRTC_Int
             $stream->setAttribute('twilioRoomSid', $twilioRoom->sid);
             $stream->setAttribute('twilioRoomName', $twilioRoom->uniqueName);
             $stream->changed();
-            $created = true;
         }
 
         try {
@@ -67,7 +56,6 @@ class Streams_WebRTC_Twilio extends Streams_WebRTC implements Streams_WebRTC_Int
 
 		return array(
             'stream' => $stream,
-	        'created' => $created,
             'roomId' => $stream->name,
             'accessToken' => $accessToken,
             'debug' => $debug,
