Qbix/Platform-History-v2

commit 3b39a847ffde117c0f6c7a17f191aaf95a70fb2b

Implemented Q/lazyload for images and tools

platform/plugins/Q/web/js/Q.js
@@ -3959,7 +3959,6 @@ Q.Tool.remove = function _Q_Tool_remove(elem, removeCached, removeElementAfterLa
 			if (Q.typeOf(Q.getObject(["Q", "tools", tn[i], "remove"], toolElement)) !== "function") {
 				continue;
 			}
-
 			toolElement.Q.tools[tn[i]].remove(removeCached, removeElementAfterLastTool);
 		}
 	});
@@ -4487,11 +4486,15 @@ Tp.remove = function _Q_Tool_prototype_remove(removeCached, removeElementAfterLa
 	var nn = Q.normalize(this.name);
 	delete this.element.Q.tools[nn];
 	delete Q.Tool.active[this.id][nn];
-	if (Q.isEmpty(Q.Tool.active[this.id])) {
+	var tools = Q.Tool.active[this.id];
+	if (Q.isEmpty()) {
 		if (removeElementAfterLastTool) {
 			Q.removeElement(this.element);
 		}
+		this.element.Q.tool = null;
 		delete Q.Tool.active[this.id];
+	} else if (Q.normalize(this.element.Q.tool) === nn) {
+		this.element.Q.tool = Q.byId(this.id);
 	}
 
 	// remove all the tool's events automatically
@@ -12952,7 +12955,8 @@ Q.onJQuery.add(function ($) {
 		"Q/resize": "{{Q}}/js/tools/resize.js",
 		"Q/layouts": "{{Q}}/js/tools/layouts.js",
 		"Q/infinitescroll": "{{Q}}/js/tools/infinitescroll.js",
-		"Q/parallax": "{{Q}}/js/tools/parallax.js"
+		"Q/parallax": "{{Q}}/js/tools/parallax.js",
+		"Q/lazyload": "{{Q}}/js/tools/lazyload.js"
 	});
 	
 	Q.Tool.jQuery({

platform/classes/Q.php
@@ -750,6 +750,9 @@ class Q
 	 * @param {boolean} [$extra.replace]
 	 *    Pass true to tell Q.js to replace any previously rendered tool tool
 	 *    even if it was marked to be retained.
+	 * @param {boolean} [$extra.lazyload]
+	 *    Pass true to allow the tool to be lazy-loaded by a Q/lazyload tool if it is
+	 *    activated on one of its containers.
 	 * @param {boolean} [$extra.merge=false]
 	 *    If true, the element for this tool is merged with the element of the tool
 	 *    already being rendered when this function is called (if any),

platform/classes/Q/Response.php
@@ -791,6 +791,7 @@ class Q_Response
 	}
 	
 	/**
+	 * Indicate whether to retain a tool
 	 * @method toolRetain
 	 * @static
 	 * @param {boolean} [$toRetain=true] Defaults to true. Whether to cache the tool being rendered.
@@ -802,9 +803,10 @@ class Q_Response
 	}
 	
 	/**
+	 * Indicate whether to replace a tool
 	 * @method toolReplace
 	 * @static
-	 * @param {boolean} [$toReplaceWith=true] Defaults to true. Whether to cache the tool being rendered.
+	 * @param {boolean} [$toReplaceWith=true] Defaults to true. Whether to replace the tool being rendered.
 	 */
 	static function toolReplace($toRetain = true)
 	{

platform/handlers/Q/after/Q/tool/render.php
@@ -53,6 +53,9 @@ function Q_after_Q_tool_render($params, &$result)
 	$data_replace = !empty($extra['replace']) || Q_Response::shouldReplaceWithTool($id_prefix)
 		? " data-Q-replace=''"
 		: '';
+	$data_retain = !empty($extra['lazyload'])
+		? " data-Q-lazyload=''"
+		: '';
 	$names = ($count === 1) ? ' '.key($info) : 's '.implode(" ", $names);
 	$ajax = Q_Request::isAjax();
 	$result = "<$tag id='{$id_prefix}tool' "

platform/plugins/Q/web/js/tools/lazyload.js
@@ -1,0 +1,210 @@
+(function (Q, $) {
+/**
+ * @module Q-tools
+ */
+	
+/**
+ * Implements lazy-loading for various types of elements.
+ * By default, has implementations for "img" and "Q_tool" selectors.
+ * The "Q_tool" handler only works with tools whose elements have a "data-q-lazyload" attribute .
+ * Note that the elements must have a nonzero width and height to be lazy-loaded when
+ * they appear in the viewport.
+ * @class Q lazyload
+ * @constructor
+ * @param {Object} [options] Override various options for this tool
+ * @param {Array} [options.handle] Set this to 
+ * @param {Object} [options.handlers] You can modify the defaults by passing an object
+ *    that looks like {handlerName: { selector: String, entering: Function, exiting: Function, preparing: Function}}
+ *    Function "entering" receives (element, intersectionObserverEntry)
+ *    Function "exiting" receives (element, intersectionObserverEntry)
+ *    Function "preparing" receives (element) and is used the first time to prepare the element
+ *    Both functions must return true if the element was modified.
+ * @param {Object} [observerOptions] Override any options to pass to IntersectionObserver
+ * @return {Q.Tool}
+ */
+Q.Tool.define('Q/lazyload', function (options) {
+
+	var tool = this;
+	var state = this.state;
+
+	var Elp = Element.prototype;
+
+	// Observe whatever is on the page already
+	tool.observer = _createObserver(tool, tool.element);
+	tool.findAndObserve(tool.element);
+
+	// Override innerHTML
+
+	var originalSet = Object.getOwnPropertyDescriptor(Elp, 'innerHTML').set;
+	var originalGet = Object.getOwnPropertyDescriptor(Elp, 'innerHTML').get;
+
+	Object.defineProperty(Elp, 'innerHTML', {
+		set: function (html) {
+			var element = document.createElement('div');
+			originalSet.call(element, html);
+			var found = false;
+			Q.each(state.handlers, function (name, info) {
+				var elements = element.querySelectorAll
+					? Array.from(element.querySelectorAll(info.selector))
+					: [];
+				Q.each(elements, function (i, element) {
+					if (info.preparing.call(tool, element) === true) {
+						found = true;
+					}
+				});
+			});
+			if (found) {
+				html = originalGet.call(element);
+			}
+			originalSet.call(this, html);
+			if (found) {
+				tool.findAndObserve(this);
+			}
+			return html;
+		},
+		get: originalGet
+	});
+
+	Q.each(['insertBefore', 'appendChild'], function (i, fn) {
+		var orig = Elp[fn];
+		Elp[fn] = function (element) {
+			if (!element) {
+				return;
+			}
+			var found = false;
+			Q.each(state.handlers, function (name, info) {
+				var elements = element.querySelectorAll
+					? Array.from(element.querySelectorAll(info.selector))
+					: [];
+				if (element.matches && element.matches(info.selector)) {
+					elements.push(element);
+				}
+				Q.each(elements, function (i, element) {
+					if (info.preparing.call(tool, element) === true) {
+						found = true;
+						tool.observer.observe(element);
+					}
+				});
+			});
+			return orig.apply(this, arguments);
+		};
+	});
+
+},
+
+{
+	root: null,
+	handlers: {
+		img: {
+			selector: 'img',
+			entering: function (img, entry) {
+				var src = img.getAttribute('data-defer-src');
+				if (src) {
+					img.setAttribute('src', src);
+					img.removeAttribute('data-defer-src');
+					return true;
+				}
+			},
+			exiting: function (img) {
+				// no need to do anyting
+				return true;
+			},
+			preparing: function (img, entry) {
+				var src = img.getAttribute('src');
+				if (src) {
+					img.setAttribute('data-defer-src', src);
+					img.removeAttribute('src');
+				}
+				return true;
+			}
+		},
+		tool: {
+			selector: '.Q_tool',
+			entering: function (element, entry) {
+				if (element.hasAttribute('data-Q-lazyload')
+				&& (!element.Q || !element.Q.tool)) {
+					Q.activate(element)
+					return true;
+				}
+			},
+			exiting: function (element, entry) {
+				if (element.hasAttribute('data-Q-lazyload')
+				&& element.Q.tool) {
+					Q.Tool.remove(element);
+					if (this.state.handlers.tool.exitingRemoveHTML) {
+						element.innerHTML = '';
+					}
+				}
+				return true;
+			},
+			preparing: function (element) {
+				return true;
+			},
+			exitingRemoveHTML: true
+		}
+	},
+	observerOptions: {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0
+	}
+}, 
+
+{
+	refresh: function () {
+		var tool = this;
+		var state = tool.state;
+		var index = state.index;
+		var total = state.total;
+		if (!Q.isInteger(state.total) || state.total < 0) {
+			throw new Q.Error("Q/paging: total is not valid: " + state.total);
+		}
+		$(tool.element).empty();
+		for (var i=0; i<=state.total-1; ++i) {
+			var $dot = $('<img />').attr({
+				'src': Q.url((i === index) ? state.pages.current : state.pages.other),
+				'class': 'Q_paging_dot '
+					+ (i === index) ? 'Q_paging_dot_current' : 'Q_paging_dot_other'
+			}).appendTo(tool.element);
+		}
+	},
+	findAndObserve: function (container) {
+		var tool = this;
+		var found = false;
+		Q.each(tool.state.handlers, function (name, info) {
+			var elements = container.querySelectorAll
+				? Array.from(container.querySelectorAll(info.selector))
+				: [];
+			if (container.matches && container.matches(info.selector)) {
+				elements.push(container);
+			}
+			Q.each(elements, function (i, element) {
+				if (info.preparing.call(tool, element) === true) {
+					found = true;
+					tool.observer.observe(element);
+				}
+			});
+		});
+	}
+});
+
+function _createObserver(tool, container) {
+	return new IntersectionObserver(function (entries, observer) {
+		Q.each(entries, function (i, entry) {
+			Q.each(tool.state.handlers, function (name, info) {
+				if (entry.target.matches && entry.target.matches(info.selector)) {
+					if (entry.isIntersecting) {
+						info.entering.call(tool, entry.target, entry);
+					} else  {
+						var rect = entry.target.getBoundingClientRect();
+						if (rect.width > 0 && rect.height > 0) {
+							info.exiting.call(tool, entry.target, entry);
+						} // otherwise it might get a false positive
+					}
+				}
+			});
+		});
+	}, tool.state.observerOptions);
+}
+
+})(Q, jQuery);

commit fe2ca5534a4f31198f15b65c57531dde887c3ab2

Fixed careless typo introduced in commit 3b39a847ffde117c0f6c7a17f191aaf95a70fb2b

platform/plugins/Q/web/js/Q.js
@@ -3963,21 +3963,34 @@ Q.Tool.beforeRemove = Q.Event.factory(_beforeRemoveToolHandlers, ["", _toolEvent
  *  should be removed.
  * @param {boolean} [removeElementAfterLastTool=false]
  *  If true, removes the element if the last tool on it was removed
+ * @param {String|Function} [filter]
+ *  This is a string that would match the tool name exactly (after normalization) to remove it,
+ *  or a function that will take a tool name and return a boolean, false means don't remove tool.
  */
-Q.Tool.remove = function _Q_Tool_remove(elem, removeCached, removeElementAfterLastTool) {
+Q.Tool.remove = function _Q_Tool_remove(elem, removeCached, removeElementAfterLastTool, filter) {
 	if (typeof elem === 'string') {
 		var tool = Q.Tool.byId(elem);
 		if (!tool) return false;
 		elem = tool.element;
 	}
-	Q.find(elem, true, null,
-	function _Q_Tool_remove_found(toolElement) {
+	if (typeof filter === 'string') {
+		filter = Q.normalize(filter);
+	}
+	Q.find(elem, true, function _Q_Tool_remove_found(toolElement) {
 		var tn = toolElement.Q.toolNames;
 		if (!tn) { // this edge case happens very rarely, usually if a slot element
 			return; // being replaced is inside another slot element being replaced
 		}
 		for (var i=tn.length-1; i>=0; --i) {
-			// check if "remove" method exist
+			if (typeof filter === 'string') {
+				if (tn[i] !== 'filter') {
+					continue;
+				}
+			} else if (typeof filter === 'function') {
+				if (!filter(tn[i])) {
+					continue;
+				}
+			}
 			if (Q.typeOf(Q.getObject(["Q", "tools", tn[i], "remove"], toolElement)) !== "function") {
 				continue;
 			}
@@ -4509,7 +4522,7 @@ Tp.remove = function _Q_Tool_prototype_remove(removeCached, removeElementAfterLa
 	delete this.element.Q.tools[nn];
 	delete Q.Tool.active[this.id][nn];
 	var tools = Q.Tool.active[this.id];
-	if (Q.isEmpty()) {
+	if (Q.isEmpty(Q.Tool.active[this.id]))) {
 		if (removeElementAfterLastTool) {
 			Q.removeElement(this.element);
 		}

platform/plugins/Q/web/js/fn/actions.js
@@ -52,6 +52,7 @@ function _Q_actions(options) {
 	remove: function () {
 		var $this = $(this);
 		var state = $this.state('Q/actions');
+		Q.Tool.remove(this, false, false, 'Q/actions');
 		state.container.remove();
 		$this.off('mouseenter.Q_actions mouseleave.Q_actions');
 	},