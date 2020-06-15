cef638c8367153f75cecd56bdcf903478b04ce03
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-06-15 18:33:41 +0300

    Added method Assets.pay which will send request to Assets/credits/post handler. This handler check if credits enough for payment and make Assets_Credits::send to stream publisher. If credits not enough, send back request to ask user buy missing credits.

--- platform/plugins/Assets/web/js/Assets.js
+++ platform/plugins/Assets/web/js/Assets.js
@@ -26,6 +26,41 @@
 					return false;
 				}
 				Streams.get(Users.loggedInUser.id, "Assets/user/credits", callback);
+			},
+			/**
+			 * Make payment for some source. Pay with credits if enough, or buy missing credits and pay.
+			 * @method pay
+			 *  @param {object} options
+			 *  @param {number} options.amount
+			 *  @param {string} options.currency Currency ISO 4217 code (USD, EUR etc)
+			 *  @param {Streams_Stream} [options.stream] Stream object for which to pay. If also can be object {publisherId: ..., streamName: ...}
+			 *  @param {Streams_Stream} [options.userId] User id where need to pass credits.
+			 *  @param {string} [options.reason] Particular payment reason.
+			 *  @param {function} [options.callback] Callback to run when payment done.
+			 */
+			pay: function (options) {
+				if (Streams.isStream(options.stream)) {
+					options.stream = {
+						publisherId: options.stream.fields.publisherId,
+						streamName: options.stream.fields.name
+					};
+				}
+
+				Q.req("Assets/credist", ['status', 'details'], function (err, response) {
+					var msg = Q.firstErrorMessage(err, response && response.errors);
+					if (msg) {
+						return Q.alert(msg);
+					}
+
+					if (!response.slots.status) {
+						Q.Dialogs.push();
+					}
+
+					Q.handle(options.callback, null, response.slots);
+				}, {
+					method: 'post',
+					fields: options
+				});
 			}
 		},
 
