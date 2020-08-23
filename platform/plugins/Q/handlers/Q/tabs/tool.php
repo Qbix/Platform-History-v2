912350e526777dfc7f374079e9fe0160dacce613
Greg Magarshak <greg@qbix.com>
2020-08-23 02:09:20 -0400

    Fixed tool option for Q/tabs after recent refactor

--- platform/plugins/Q/handlers/Q/tabs/tool.php
+++ platform/plugins/Q/handlers/Q/tabs/tool.php
@@ -121,7 +121,7 @@ function Q_tabs_tool($options)
 		$result .= Q_Html::tag('li', $attributesMerged, $a);
 	}
 	Q_Response::setToolOptions(compact(
-		'selectors', 'slot', 'urls', 'memorize', 'defaultTabName', 'touchlabels',
+		'selectors', 'slot', 'urls', 'retain', 'defaultTabName', 'touchlabels',
 		'vertical', 'compact', 'overflow',
 		'field', 'loader', 'beforeSwitch', 'beforeScripts', 'onActivate'
 	));
