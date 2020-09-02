9720768f606f303ad4902ceaba6761977be6b369
Greg Magarshak <greg@qbix.com>
2020-09-01 20:43:34 -0400

    Renamed canHandle to moduleCanHandle

--- MyApp/handlers/MyApp/canHandle.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-function MyApp_canHandle($params)
-{
-	$module = "MyApp";
-	$action = $params['action'];
-	return Q::canHandle("$module/$action/response/content")
-		or Q::canHandle("$module/$action/response");
-}
