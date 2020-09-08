2b610ea081a6048e4c5550cf03101c5897884fa0
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-09-08 16:17:18 +0300

    Modified Q.url method.
    Added 'blob:' to exception.

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -6894,7 +6894,7 @@ Q.load = function _Q_load(plugins, callback, options) {
  */
 Q.url = function _Q_url(what, fields, options) {
 	var what2 = what || '';
-	if (what2.substr(0, 5) === 'data:') {
+	if (what2.startsWith('data:') || what2.startsWith('blob:')) {
 		return what2; // this is a special type of URL
 	}
 	var parts = what2.split('?');
