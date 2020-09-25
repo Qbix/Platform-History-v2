dfde9e6484dbf06bfca8642e1a3be0ae19aeea51
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-09-25 14:06:52 +0300

    Modified Websites/pdf handler to allow create/edit streams on backend
    (cherry picked from commit 3068d6059937c134926f90ecc8a02d0b19c45cc7)

--- platform/plugins/Websites/handlers/Websites/pdf/post.php
+++ platform/plugins/Websites/handlers/Websites/pdf/post.php
@@ -6,13 +6,20 @@ function Websites_pdf_post($params)
 	Q_Valid::nonce(true);
 
     $params = array_merge($_REQUEST, $params);
-
-	$fields = Q::take($params, array('url'));
+	$fields = Q::take($params, array('url', 'publisherId', 'streamName', 'clipStart', 'clipEnd'));
 
 	$url = $fields['url'];
+	$publisherId = $fields['publisherId'];
+	$streamName = $fields['streamName'];
+	$clipStart = $fields['clipStart'];
+	$clipEnd = $fields['clipEnd'];
 
-	// stream required if publisherId and streamName slots requested
-	$streamRequired = Q_Request::slotName("publisherId") && Q_Request::slotName("streamName");
+	if ($publisherId && $streamName) {
+		$stream = Streams::fetchOne(null, $publisherId, $streamName);
+	} else {
+		// stream required if publisherId and streamName slots requested
+		$streamCreate = Q_Request::slotName("publisherId") && Q_Request::slotName("streamName");
+	}
 
 	if (parse_url($url, PHP_URL_SCHEME) === null) {
 		$url = 'http://'.$url;
@@ -20,17 +27,14 @@ function Websites_pdf_post($params)
 
 	$result = Websites_PDF::scrape($url);
 
-	// check if stream already exists
-	$stream = Websites_Webpage::fetchStream($url);
-
-	if ($streamRequired && !$stream) {
-		$stream = Websites_PDF::createStream(compact("url"));
+	if ($stream) {
+		$stream = Websites_PDF::editStream(compact("stream", "url", "clipStart", "clipEnd"));
+	} elseif ($streamCreate) {
+		$stream = Websites_PDF::createStream(compact("url", "clipStart", "clipEnd"));
 	}
 
 	Q_Response::setSlot('publisherId', Q::ifset($stream, "publisherId", null));
 	Q_Response::setSlot('streamName', Q::ifset($stream, "name", null));
-
-	$result["alreadyExist"] = (bool)$stream;
 
 	Q_Response::setSlot('result', $result);
 }
