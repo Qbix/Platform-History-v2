4cc58e1aa2778e62cb3648399b4df28cb2412763
Greg Magarshak <greg@qbix.com>
2020-11-07 22:11:37 -0500

    Fixed default contextual behavior and content in MyApp

--- MyApp/views/MyApp/dashboard.php
+++ MyApp/views/MyApp/dashboard.php
@@ -6,6 +6,13 @@
 	<div id="dashboard_user">
 		<?php echo Q::tool('Users/status') ?>
 	</div>
+
+	<div id="dashboard_user_contextual">
+		<ul class="Q_listing">
+			<li data-action="setIdentifier"><?php echo $dashboard['SetEmailAddress'] ?></li>
+			<li data-action="logout"><?php echo $dashboard['LogOut'] ?></li>
+		</ul>
+	</div>
 	
 	<?php echo Q::tool('Q/tabs', array(
 		'vertical' => !$isMobile and Q_Config::get('Q', 'response', 'layout', 'sidebar', false),
