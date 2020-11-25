269f547252dc26aab68da1d3706ba1219e395bdc
Greg Magarshak <greg@qbix.com>
2020-11-25 10:54:03 -0500

    Removed old shim for Object.keys from Q.js

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -156,48 +156,6 @@ Object.getPrototypeOf = function (obj) {
 	return undefined;
 };
 
-if (!Object.keys)
-/**
-* Returns an array containing the object's keys, in a cross-browser way
-* @method keys
-* @return {Array}
-*/
-Object.keys = (function () {
-	var hasOwnProperty = Object.prototype.hasOwnProperty,
-		hasDontEnumBug = !{toString:null}.propertyIsEnumerable("toString"),
-		DontEnums = [
-			'toString',
-			'toLocaleString',
-			'valueOf',
-			'hasOwnProperty',
-			'isPrototypeOf',
-			'propertyIsEnumerable',
-			'constructor'
-		],
-		DontEnumsLength = DontEnums.length;
-  
-	return function (o) {
-		if (typeof o != "object" && typeof o != "function" || o === null)
-			throw new TypeError("Object.keys called on a non-object");
-	 
-		var result = [];
-		for (var name in o) {
-			if (hasOwnProperty.call(o, name)) {
-				result.push(name);
-			}
-		}
-	 
-		if (hasDontEnumBug) {
-			for (var i = 0; i < DontEnumsLength; i++) {
-				if (hasOwnProperty.call(o, DontEnums[i]))
-					result.push(DontEnums[i]);
-			}   
-		}
-	 
-		return result;
-	};
-})();
-
 /**
  * @class String
  * @description Q extended methods for Strings
