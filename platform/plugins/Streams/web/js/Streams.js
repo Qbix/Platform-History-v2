6b4e6544c612b9ab18c531502447eea0a76b3bd3
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-11-27 16:46:54 +0300

    Fixed Streams.metrics. If publisherId or streamName empty, don't send requests to server.

--- platform/plugins/Streams/web/js/Streams.js
+++ platform/plugins/Streams/web/js/Streams.js
@@ -4736,11 +4736,11 @@ Streams.Metrics = function (params) {
 	this.streamName = Q.getObject("streamName", params) || null;
 
 	if (!this.publisherId) {
-		return console.warn("Streams.Metrics: publisherId undefined");
+		console.warn("Streams.Metrics: publisherId undefined");
 	}
 
 	if (!this.streamName) {
-		return console.warn("Streams.Metrics: streamName undefined");
+		console.warn("Streams.Metrics: streamName undefined");
 	}
 
 	// set useFaces option
@@ -4837,6 +4837,10 @@ Streams.Metrics = function (params) {
 	 * Start timer interval
 	 */
 	this.timerId = setInterval(function () {
+		if (!this.publisherId || !this.streamName) {
+			return;
+		}
+
 		if (Q.isEmpty(that.predefined)) {
 			return;
 		}
