c241f06fd8b2118ada6d95ef603eb31284e0836e
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2020-12-22 20:56:03 +0300

    Make Streams/webrtc preview tool non editable by default.

--- platform/plugins/Streams/web/js/tools/webrtc/preview.js
+++ platform/plugins/Streams/web/js/tools/webrtc/preview.js
@@ -1,5 +1,4 @@
 (function (Q, $, window, undefined) {
-
     /**
      * Streams/webrtc/preview tool.
      * Renders a tool to preview Streams/webrtc
@@ -8,70 +7,65 @@
      * @param {Object} [options] options to pass besides the ones to Streams/preview tool
      */
     Q.Tool.define("Streams/webrtc/preview", ["Streams/preview"], function _Streams_webrtc_preview (options, preview) {
-            var tool = this;
-            var state = this.state;
-            tool.preview = preview;
+        var tool = this;
+        var state = this.state;
+        tool.preview = preview;
 			
-			preview.state.editable = false;
+        //preview.state.creatable.preprocess = tool.composer.bind(this);
 
-            //preview.state.creatable.preprocess = tool.composer.bind(this);
+        //Q.addStylesheet('{{Streams}}/css/tools/webrtcPreview.css', { slotName: 'Streams' });
 
-            //Q.addStylesheet('{{Streams}}/css/tools/webrtcPreview.css', { slotName: 'Streams' });
+        preview.state.editable = this.state.editable;
 
-            Q.Text.get('Streams/content', function (err, text) {
-                var msg = Q.firstErrorMessage(err);
-                if (msg) {
-                    return console.warn(msg);
-                }
-
-                tool.text = text;
-                preview.state.onRefresh.add(tool.refresh.bind(tool));
-            });
-        },
-
-        {
-
-        },
-
-{
-        refresh: function (stream) {
-            var tool = this;
-            var state = this.state;
-            var $toolElement = $(tool.element);
-
-            if (!Q.Streams.isStream(stream)) {
-                return;
+        Q.Text.get('Streams/content', function (err, text) {
+            var msg = Q.firstErrorMessage(err);
+            if (msg) {
+                return console.warn(msg);
             }
 
-            tool.stream = stream;
+            tool.text = text;
+            preview.state.onRefresh.add(tool.refresh.bind(tool));
+        });
+    },
 
-            // retain with stream
-            Q.Streams.retainWith(tool).get(stream.fields.publisherId, stream.fields.name);
+    {
+        editable: false
+    },
 
-            setTimeout(function () {
-                $toolElement.tool("Streams/preview", {
-                    publisherId: state.publisherId,
-                    streamName: state.streamName
-                }).tool("Streams/default/preview").activate();
-            }, 0);
+{
+    refresh: function (stream) {
+        var tool = this;
+        var state = this.state;
+        var $toolElement = $(tool.element);
 
-            $toolElement.on(Q.Pointer.fastclick, function () {
-                var WebConference = Q.Streams.WebRTC();
-                WebConference.start({
-                    element: document.body,
-                    roomId: stream.fields.name.split('/').pop(),
-                    roomPublisherId: stream.fields.publisherId,
-                    mode: 'node',
-                    startWith: {video: false, audio: true},
-                    onWebRTCRoomCreated: function() {
-                        console.log('onWebRTCRoomCreated', this);
-                    },
-                    onWebrtcControlsCreated: function() {
-                        console.log('onWebrtcControlsCreated', this);
-                    }
-                });
-            });
+        if (!Q.Streams.isStream(stream)) {
+            return;
         }
-    });
+
+        tool.stream = stream;
+
+        // retain with stream
+        Q.Streams.retainWith(tool).get(stream.fields.publisherId, stream.fields.name);
+
+        $toolElement.tool("Streams/default/preview").activate();
+
+        $toolElement.on(Q.Pointer.fastclick, function () {
+            var WebConference = Q.Streams.WebRTC();
+            WebConference.start({
+                element: document.body,
+                roomId: stream.fields.name.split('/').pop(),
+                roomPublisherId: stream.fields.publisherId,
+                mode: 'node',
+                startWith: {video: false, audio: true},
+                onWebRTCRoomCreated: function() {
+                    console.log('onWebRTCRoomCreated', this);
+                },
+                onWebrtcControlsCreated: function() {
+                    console.log('onWebrtcControlsCreated', this);
+                }
+            });
+        });
+    }
+});
 
 })(Q, Q.$, window);
