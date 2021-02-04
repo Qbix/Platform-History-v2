4a22ce84859baae34334fca84a850795c846557d
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2021-02-04 12:57:29 +0300

    Fixed Streams_Invite::accept method. Put stream->join back to condition, so it call only if option "subscribe" false.

--- platform/plugins/Streams/classes/Streams/Invite.php
+++ platform/plugins/Streams/classes/Streams/Invite.php
@@ -233,7 +233,7 @@ class Streams_Invite extends Base_Streams_Invite
 			}
 		}
 		
-		if (!empty($options['subscribe'])) {
+		if (Q::ifset($options, "subscribe", false)) {
 			$participant = new Streams_Participant();
 			$participant->publisherId = $stream->publisherId;
 			$participant->streamName = $stream->name;
@@ -254,12 +254,12 @@ class Streams_Invite extends Base_Streams_Invite
 					// this exception, they could have written this code block themselves.
 				}
 			}
+		} else {
+			$stream->join($userId, $this->publisherId, $this->streamName, array(
+				'extra' => array('Streams/invitingUserId' => $this->invitingUserId),
+				'noVisit' => true
+			));
 		}
-
-		$stream->join($userId, $this->publisherId, $this->streamName, array(
-			'extra' => array('Streams/invitingUserId' => $this->invitingUserId),
-			'noVisit' => true
-		));
 
 		// if labels exist add contact
 		$extra = Q::json_decode($this->extra ?: '{}', true);
