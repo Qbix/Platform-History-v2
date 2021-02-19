518bd88651ce1074ade01435b09581bae66b0c70
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2021-02-19 19:32:53 +0300

    Improved Streams_stream_delete handler. Use $params argument.

--- platform/plugins/Streams/handlers/Streams/stream/delete.php
+++ platform/plugins/Streams/handlers/Streams/stream/delete.php
@@ -11,18 +11,18 @@
  * @param {string} $_REQUEST.publisherId The id of the stream publisher
  * @param {string} $_REQUEST.streamName The name of the stream the user will be invited to
  */
-function Streams_stream_delete() {
-	$user = Users::loggedInUser(true);
-	$publisherId = Streams::requestedPublisherId(true);
-	$streamName = Streams::requestedName(true);
+function Streams_stream_delete($params) {
+	$asUserId = Q::ifset($params, "asUserId", null) ?: Users::loggedInUser(true)->id;
+	$publisherId = Q::ifset($params, "publisherId", null) ?: Streams::requestedPublisherId(true);
+	$streamName = Q::ifset($params, "streamName", null) ?: Streams::requestedName(true);
 	
-	$stream = Streams::fetchOne(null, $publisherId, $streamName, true);
+	$stream = Streams::fetchOne($asUserId, $publisherId, $streamName, true);
 	$close = Streams_Stream::getConfigField($stream->type, 'close', true);
 	if (!$close) {
         throw new Q_Exception("This app doesn't let clients directly close streams of type ".$stream->type, 'type');
 	}
 	
-	Streams::$cache['result'] = Streams::close($user->id, $publisherId, $streamName);
+	Streams::$cache['result'] = Streams::close($asUserId, $publisherId, $streamName);
 	
 	// NOTE: we did not delete the stream. That will have to be done in a cron job like this:
 	// // Clean up access
