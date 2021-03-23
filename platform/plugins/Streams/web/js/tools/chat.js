72462ad7b600b2ca6ee81a6a8830dfeb7ebad79c
Andrey Tepaykin <andrey.tepaykin@gmail.com>
2021-03-23 17:22:09 +0300

    Fixed bug in chat tool. Instructions contacin double quotes, so need to use single quotes to escape.

--- platform/plugins/Streams/web/js/tools/chat.js
+++ platform/plugins/Streams/web/js/tools/chat.js
@@ -1256,7 +1256,7 @@ Q.Template.set('Streams/chat/message/bubble',
 	'<div class="Streams_chat_item {{classes}}" '+
 			'data-byUserId="{{byUserId}}" '+
 			'data-ordinal="{{ordinal}}" ' +
-			'data-instructions="{{instructions}}">'+
+			"data-instructions='{{instructions}}'>"+
 		'<div class="Streams_chat_avatar_icon" data-byUserId="{{byUserId}}"></div>'+
 		'<div class="Streams_chat_bubble">'+
 			'<div class="Streams_chat_tick"></div>'+
