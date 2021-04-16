e847cd49afd1444d5a11cbc54aa9f7f031bbfe7d
Andrey Tepaykin <andrey@qbix.com>
2021-04-16 17:18:49 +0300

    Fixed bug.

--- platform/classes/Q/Handlebars.js
+++ platform/classes/Q/Handlebars.js
@@ -145,7 +145,7 @@ handlebars.registerHelper('replace', function(find, replace, options) {
 });
 
 /* helper to compare two arguemnts for equal: {{#ifEquals arg1 arg2}} */
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
 	return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
 
