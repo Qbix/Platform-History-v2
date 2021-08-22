9f27bc28b7ee4192da63ca2b66b564d6d2910cbc
Greg Magarshak <greg@qbix.com>
2021-08-22 05:58:59 -0400

    Renamed saveStat to saveActivity

--- platform/plugins/Users/handlers/Users/after/Q_response_content.php
+++ platform/plugins/Users/handlers/Users/after/Q_response_content.php
@@ -4,6 +4,6 @@ function Users_after_Q_response_content()
 {
 	$url = Q_Request::url();
 
-	Users_Vote::setStat("app", "visited");
-	Users_Vote::setStat("page", base64_encode($url));
+	Users_Vote::saveActivity("app", "visited");
+	Users_Vote::saveActivity("page", base64_encode($url));
 }
