fec510cfd168a72da688a14ad7924fbc0c5c7911
Andrey Tepaykin <andrey@qbix.com>
2021-10-28 16:41:41 +0300

    Updated MyApp/scripts/Q/models.php script. Added ability to define exact plugins the models need to update.

--- MyApp/scripts/Q/models.php
+++ MyApp/scripts/Q/models.php
@@ -21,6 +21,8 @@ Options:
 
 --all Generate also plugins models
 
+--plugin=<plugin name> Generate models for this plugin only. Possible to define multiple plugins: --plugin=<plugin_1> --plugin=<plugin_2> ... 
+
 EOT;
 
 $help = <<<EOT
@@ -34,21 +36,48 @@ EOT;
 if (isset($argv[1]) and in_array($argv[1], array('--help', '/?', '-h', '-?', '/h')))
 	die($help);
 
+$longopts = array('plugin:', 'all');
+$options = getopt('', $longopts);
+$plugins = array();
+if (isset($options['plugin'])) {
+	$plugins = is_array($options['plugin']) ? $options['plugin'] : array($options['plugin']);
+}
+
+// if plugins explicit defined
+if (!empty($plugins)) {
+	foreach ($plugins as $plugin) {
+		createModels($plugin);
+	}
+	exit;
+}
+
 $connections = array_keys(Q_Config::get('Db', 'connections', array()));
 $plugins = Q::plugins();
 
-if (!isset($argv[1]) or $argv[1] != '--all')
+if (!isset($argv[1]) or $argv[1] != '--all') {
 	$connections = array_diff($connections, $plugins);
+}
 
 foreach($connections as $c) {
-	if ($c === '*') continue;
+	if ($c === '*') {
+		continue;
+	}
+
 	$isPlugin = in_array($c, $plugins);
-	echo "\nMaking models for ".($isPlugin ? "plugin" : "connection")." $c\n";
-	$path = ($isPlugin ? Q_DIR.DS.'plugins'.DS.$c : APP_DIR).DS.'classes';
-	if ($filenames = Db::connect($c)->generateModels($path)) {
+
+	if (!empty($plugins) && !in_array($c, $plugins)) {
+		continue;
+	}
+
+	createModels($c, $isPlugin);
+}
+echo "\nSuccess\n";
+
+function createModels($plugin, $isPlugin=true) {
+	echo "\nMaking models for ".($isPlugin ? "plugin" : "connection")." $plugin\n";
+	$path = ($isPlugin ? Q_DIR.DS.'plugins'.DS.$plugin : APP_DIR).DS.'classes';
+	if ($filenames = Db::connect($plugin)->generateModels($path)) {
 		echo "Files saved:\n\t".implode("\n\t", $filenames);
 	}
 	echo "\n";
 }
-echo "\nSuccess\n";
-
