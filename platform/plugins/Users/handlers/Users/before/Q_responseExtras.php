4a3140a0d889edd97253e2d1b64677d583525df8
Andrey Tepaykin <andrey@qbix.com>
2021-12-16 12:36:33 +0300

    Fixed path to config params according to last changes in config structure.

--- platform/plugins/Users/handlers/Users/before/Q_responseExtras.php
+++ platform/plugins/Users/handlers/Users/before/Q_responseExtras.php
@@ -74,13 +74,13 @@ function Users_before_Q_responseExtras()
 	Q_Response::setScriptData("Q.plugins.Users.labels", Users_Label::getLabels());
 
 	// load ethers js libs if web3 config defined
-	if (Q_Config::get("Users", "apps", "web3", Users::communityId(), null)) {
+	if ($config = Q_Config::get("Users", "apps", "web3", null)) {
 		Q_Response::addScript('{{Users}}/js/web3/ethers-5.2.umd.min.js', 'Users');
 		Q_Response::addScript('{{Users}}/js/web3/evm-chains.min.js', 'Users');
-		if (Q_Config::get("Users", "web3", "providers", "Fortmatic", false)) {
+		if (Q::ifset($config, Users::communityId(), "providers", "fortmatic", null)) {
 			Q_Response::addScript('{{Users}}/js/web3/fortmatic.js', 'Users');
 		}
-		if (Q_Config::get("Users", "web3", "providers", "Portis", false)) {
+		if (Q::ifset($config, Users::communityId(), "providers", "portis", null)) {
 			Q_Response::addScript('{{Users}}/js/web3/portis.js', 'Users');
 		}
 		Q_Response::addScript('{{Users}}/js/web3/walletconnect.min.js', 'Users');
