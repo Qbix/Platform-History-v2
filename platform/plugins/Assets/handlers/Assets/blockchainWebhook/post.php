9f97f350965cd1265cdd084618986292994d9522
Pavan Yogi <pavanyogi121@gmail.com>
2022-01-10 21:56:23 +0530

    Fix post request to accept request body using php://input

--- platform/plugins/Assets/handlers/Assets/blockchainWebhook/post.php
+++ platform/plugins/Assets/handlers/Assets/blockchainWebhook/post.php
@@ -3,7 +3,12 @@
 function Assets_blockchainWebhook_post()
 {
 
-    Q::log(json_encode($_REQUEST), 'blockchain');
+    if(count($_REQUEST) === 0) {
+        $blockData = file_get_contents('php://input');
+    } else {
+        $blockData = json_encode($_REQUEST);
+    }
+    Q::log($blockData, 'blockchain');
     http_response_code(200); // PHP 5.4 or greater
     exit;
 
