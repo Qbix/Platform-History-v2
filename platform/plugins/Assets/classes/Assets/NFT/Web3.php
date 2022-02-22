e984683de6c4820ebc755bdc67a2d6074be666e2
Andrey Tepaykin <andrey@qbix.com>
2022-02-22 18:14:50 +0300

    Created class Assets_NFT_Web3 with all methods required for NFT (moved from Users_Web3).

---
+++ platform/plugins/Assets/classes/Assets/NFT/Web3.php
@@ -0,0 +1,316 @@
+<?php
+
+/**
+ * @module Assets
+ */
+/**
+ * Methods for manipulating "Assets/NFT" streams
+ * @class Assets_NFT
+ */
+class Assets_NFT_Web3 extends Users_Web3
+{
+	static $categoryStreamName = "Assets/user/NFTs";
+
+    /**
+	 * Check if NFT category exists, and create if not
+	 * @method category
+	 * @param {string} [$publisherId=null] If null - logged user id used.
+	 */
+	static function category($publisherId=null)
+	{
+		if ($publisherId === null) {
+			$publisherId = Users::loggedInUser(true)->id;
+		}
+		if (empty($publisherId)) {
+			throw new Q_Exception_WrongValue(array(
+				'field' => 'publisherId',
+				'range' => 'nonempty'
+			));
+		}
+
+		$stream = Streams::fetchOne($publisherId, $publisherId, self::$categoryStreamName);
+		if (!$stream) {
+			Streams::create(null, $publisherId, 'Streams/category', array('name' => self::$categoryStreamName));
+		}
+
+		if ($stream->getAttribute('Assets/NFT/minted/total', null) === null) {
+			$stream->setAttribute('Assets/NFT/minted/total', 0);
+			$stream->changed();
+		}
+
+		return $stream;
+	}
+
+	/**
+	 * Get or create new NFT empty stream for composer
+	 * This is for user creating new NFT streams in the interface
+	 * @method getNFTStream
+	 * @param {string} [$userId=null] If null loggedin user id used
+	 * @return {Streams_Stream}
+	 */
+	static function getNFTStream ($userId = null) {
+		$userId = $userId ?: Users::loggedInUser(true)->id;
+		$category = self::category($userId);
+
+		$streams = Streams::related($userId, $userId, $category->name, true, array(
+			"type" => "new",
+			"streamsOnly" => true,
+			"ignoreCache" => true
+		));
+
+		if (empty($streams)) {
+			$stream = Streams::create($userId, $userId, "Assets/NFT", array(), array(
+				"publisherId" => $userId,
+				"streamName" => $category->name,
+				"type" => "new"
+			));
+			$stream->join(compact("userId"));
+			return $stream;
+		} else {
+			return reset($streams);
+		}
+	}
+
+	/**
+	 * Updated NFT stream with new data
+	 * @method updateNFT
+	 * @param {Streams_Stream} $stream NFT stream
+	 * @param {array} $fields Array of data to update stream
+	 * @return {Streams_Stream}
+	 */
+	static function updateNFT ($stream, $fields) {
+		$communityId = Users::communityId();
+		$userId = Users::loggedInUser(true)->id;
+
+		$fieldsUpdated = false;
+		foreach (array("title", "content") as $field) {
+			if (!Q::ifset($fields, $field)) {
+				continue;
+			}
+
+			$stream->{$field} = $fields[$field];
+			$fieldsUpdated = true;
+		}
+
+		// update attributes
+		if (Q::ifset($fields, "attributes")) {
+			if ($stream->attributes) {
+				$attributes = (array)Q::json_decode($stream->attributes);
+			} else {
+				$attributes = array();
+			}
+			$stream->attributes = Q::json_encode(array_merge($attributes, $fields["attributes"]));
+		}
+
+		if ($fieldsUpdated) {
+			$stream->save();
+		}
+
+		$interestsRelationType = "NFT/interest";
+		// remove relations
+		$relateds = Streams_RelatedTo::select()->where(array(
+			"type" => $interestsRelationType,
+			"fromPublisherId" => $stream->publisherId,
+			"fromStreamName" => $stream->name
+		))->fetchDbRows();
+		foreach ($relateds as $related) {
+			Streams::unrelate($userId, $related->toPublisherId, $related->toStreamName, $interestsRelationType, $stream->publisherId, $stream->name);
+		}
+
+		if (!empty(Q::ifset($fields, "interests", null))) {
+			foreach ($fields["interests"] as $key => $interest) {
+				$interestStream = Streams::getInterest(trim($interest));
+				$fields["interests"][$key] = $interestStream->name;
+			}
+
+			// relate to interests
+			Streams::relate($userId, $communityId, $fields["interests"], $interestsRelationType, $stream->publisherId, $stream->name);
+		}
+
+		// change stream relation
+		Streams::unrelate($userId, $stream->publisherId, self::$categoryStreamName, "new", $stream->publisherId, $stream->name);
+		Streams::relate($userId, $stream->publisherId, self::$categoryStreamName, "NFT", $stream->publisherId, $stream->name, array("weight" => time()));
+
+		//$onMarketPlace = Q::ifset($fields, "attributes", "onMarketPlace", null);
+		//if ($onMarketPlace == "true") {
+		// relate to main category
+		Streams::relate($userId, $communityId, "Assets/NFTs", "NFT", $stream->publisherId, $stream->name, array("weight" => time()));
+		//} elseif ($onMarketPlace == "false") {
+		// unrelate from main category
+		//	Streams::unrelate($userId, $communityId, "Assets/NFTs", "NFT", $stream->publisherId, $stream->name);
+		//}
+
+		return $stream;
+	}
+
+	/**
+	 * Get tokens by author
+	 * @method tokensByAuthor
+	 * @static
+	 * @param {String} $address Author wallet address
+	 * @param {String} $chainId
+	 * @return array
+	 */
+	static function tokensByAuthor ($address, $chainId) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		return self::execute($network["contract"], __FUNCTION__, $address);
+	}
+
+	/**
+	 * Get comission info by token
+	 * @method commissionInfo
+	 * @static
+	 * @param {String} $tokenId
+	 * @param {String} $chainId
+	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
+	 * @param {String} [$contractAddress=null] Custom contract address
+	 * @return array
+	 */
+	static function commissionInfo ($tokenId, $chainId, $updateCache=false) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		$data = self::execute($network["contract"],"getCommission", $tokenId, null, $updateCache);
+		$data["value"] = gmp_intval(Q::ifset($data, "r", "value", null));
+
+		return $data;
+	}
+
+	/**
+	 * Get tokens by owner
+	 * @method tokensByOwner
+	 * @static
+	 * @param {String} $address Owner wallet address
+	 * @param {String} $chainId
+	 * @return array
+	 */
+	static function tokensByOwner ($address, $chainId) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		return self::execute($network["contract"],__FUNCTION__, $address);
+	}
+
+	/**
+	 * Get author of token
+	 * @method authorOf
+	 * @static
+	 * @param {String} $tokenId
+	 * @param {String} $chainId
+	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
+	 * @param {String} [$contractAddress=null] Custom contract address
+	 * @return array
+	 */
+	static function authorOf ($tokenId, $chainId, $updateCache=false) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		return self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
+	}
+
+	/**
+	 * Get owner of token
+	 * @method ownerOf
+	 * @static
+	 * @param {String} $tokenId
+	 * @param {String} $chainId network symbol
+	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
+	 * @return array
+	 */
+	static function ownerOf ($tokenId, $chainId, $updateCache=false) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		return self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
+	}
+
+	/**
+	 * Get sale info by token
+	 * @method saleInfo
+	 * @static
+	 * @param {String} $tokenId
+	 * @param {String} $chainId
+	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
+	 * @return array
+	 */
+	static function saleInfo ($tokenId, $chainId, $updateCache=false) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		$data = self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
+		$data[1] = gmp_intval(Q::ifset($data, 1, "value", null));
+		return $data;
+	}
+
+	/**
+	 * Get currency by chainId and currency token
+	 * @method getCurrencyByChain
+	 * @static
+	 * @param {String} $chainId
+	 * @param {String} $currencyToken currency token
+	 * @return array
+	 */
+	static function getCurrencyByChain ($chainId, $currencyToken) {
+		$currencies = Q_Config::expect("Assets", "NFT", "currencies");
+		foreach ($currencies as $currency) {
+			if ($currency[$chainId] == $currencyToken) {
+				return $currency;
+			}
+		}
+	}
+
+	/**
+	 * Get available blockchain networks info (contact address, currency, rpcUrl, blockExplorerUrl)
+	 * @method getChains
+	 * @static
+	 * @return array
+	 */
+	static function getChains () {
+		$chains = Q_Config::get("Users", "apps", "web3", array());
+		$currencies = Q_Config::get("Assets", "NFT", "currencies", array());
+		$chainsClient = array();
+		foreach ($chains as $i => $chain) {
+			// if contract or rpcUrls undefined, skip this chain
+			$contract = Q::ifset($chain, "contracts", "NFT", "address", null);
+			$rpcUrl = Q::ifset($chain, "rpcUrl", null);
+			$infuraId = Q::ifset($chain, "providers", "walletconnect", "infura", "projectId", null);
+			$blockExplorerUrl = Q::ifset($chain, "blockExplorerUrl", null);
+			$chainId = Q::ifset($chain, "appId", null);
+
+			if (!$contract || !$rpcUrl) {
+				unset($chain[$i]);
+				continue;
+			}
+
+			$rpcUrl = Q::interpolate($rpcUrl, compact("infuraId"));
+			$temp = compact("chainId", "contract", "rpcUrl", "blockExplorerUrl");
+
+			foreach ($currencies as $currency) {
+				if ($currency[$chainId] == "0x0000000000000000000000000000000000000000") {
+					$temp["currency"] = $currency;
+					$temp["currency"]["token"] = $currency[$chainId];
+					break;
+				}
+			}
+
+			$temp["default"] = $i == Users::communityId();
+
+			$chainsClient[$chainId] = $temp;
+		}
+
+		return $chainsClient;
+	}
+
+	/**
+	 * Get sale info by token
+	 * @method saleInfo
+	 * @static
+	 * @param {String} $tokenId
+	 * @param {String} $chainId
+	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
+	 * @return array
+	 */
+	static function getSaleInfo ($tokenId, $chainId, $updateCache=false) {
+		self::construct($chainId);
+		$network = self::$networks[$chainId];
+		$data = self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
+		$data[1] = gmp_intval(Q::ifset($data, 1, "value", null));
+		return $data;
+	}
+};
--- platform/plugins/Users/classes/Users/Web3.php
+++ platform/plugins/Users/classes/Users/Web3.php
@@ -63,7 +63,7 @@ class Users_Web3 extends Base_Users_Web3 {
 
 		$cache = null;
 		if ($caching !== false && $cacheDuration) {
-			$cache = self::getCache($chainId, $contractAddress, $methodName, $params, $cacheDuration);
+			$cache = self::getCache($contractAddress, $methodName, $params, $cacheDuration);
 			if ($cache->wasRetrieved()) {
 				return Q::json_decode($cache->result);
 			}
@@ -157,9 +157,9 @@ class Users_Web3 extends Base_Users_Web3 {
 	 * @param {String} $chainId
 	 * @static
 	 */
-	private static function construct($chainId) {
+	protected static function construct($chainId) {
 		if (self::$useCache === null) {
-			self::$useCache = Q_Config::get("Assets", "web3", "useCache", true);
+			self::$useCache = Q_Config::get("Users", "web3", "useCache", true);
 		}
 
 		if (self::$networks[$chainId]) {
@@ -190,177 +190,6 @@ class Users_Web3 extends Base_Users_Web3 {
 
 		$abi = file_get_contents($filePath);
 		self::$networks[$chainId]["contract"] = new Contract($rpcUrl, $abi);
-	}
-
-	/**
-	 * Get tokens by author
-	 * @method tokensByAuthor
-	 * @static
-	 * @param {String} $address Author wallet address
-	 * @param {String} $chainId
-	 * @return array
-	 */
-	static function tokensByAuthor ($address, $chainId) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		return self::execute($network["contract"], __FUNCTION__, $address);
-	}
-
-	/**
-	 * Get comission info by token
-	 * @method commissionInfo
-	 * @static
-	 * @param {String} $tokenId
-	 * @param {String} $chainId
-	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
-	 * @param {String} [$contractAddress=null] Custom contract address
-	 * @return array
-	 */
-	static function commissionInfo ($tokenId, $chainId, $updateCache=false) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		$data = self::execute($network["contract"],"getCommission", $tokenId, null, $updateCache);
-		$data["value"] = gmp_intval(Q::ifset($data, "r", "value", null));
-
-		return $data;
-	}
-
-	/**
-	 * Get tokens by owner
-	 * @method tokensByOwner
-	 * @static
-	 * @param {String} $address Owner wallet address
-	 * @param {String} $chainId
-	 * @return array
-	 */
-	static function tokensByOwner ($address, $chainId) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		return self::execute($network["contract"],__FUNCTION__, $address);
-	}
-
-	/**
-	 * Get author of token
-	 * @method authorOf
-	 * @static
-	 * @param {String} $tokenId
-	 * @param {String} $chainId
-	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
-	 * @param {String} [$contractAddress=null] Custom contract address
-	 * @return array
-	 */
-	static function authorOf ($tokenId, $chainId, $updateCache=false) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		return self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
-	}
-
-	/**
-	 * Get owner of token
-	 * @method ownerOf
-	 * @static
-	 * @param {String} $tokenId
-	 * @param {String} $chainId network symbol
-	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
-	 * @return array
-	 */
-	static function ownerOf ($tokenId, $chainId, $updateCache=false) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		return self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
-	}
-
-	/**
-	 * Get sale info by token
-	 * @method saleInfo
-	 * @static
-	 * @param {String} $tokenId
-	 * @param {String} $chainId
-	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
-	 * @return array
-	 */
-	static function saleInfo ($tokenId, $chainId, $updateCache=false) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		$data = self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
-		$data[1] = gmp_intval(Q::ifset($data, 1, "value", null));
-		return $data;
-	}
-
-	/**
-	 * Get currency by chainId and currency token
-	 * @method getCurrencyByChain
-	 * @static
-	 * @param {String} $chainId
-	 * @param {String} $currencyToken currency token
-	 * @return array
-	 */
-	static function getCurrencyByChain ($chainId, $currencyToken) {
-		$currencies = Q_Config::expect("Users", "web3", "currencies");
-		foreach ($currencies as $currency) {
-			if ($currency[$chainId] == $currencyToken) {
-				return $currency;
-			}
-		}
-	}
-
-	/**
-	 * Get available blockchain networks info (contact address, currency, rpcUrl, blockExplorerUrl)
-	 * @method getChains
-	 * @static
-	 * @return array
-	 */
-	static function getChains () {
-		$chains = Q_Config::get("Users", "apps", "web3", array());
-		$currencies = Q_Config::get("Users", "web3", "currencies", array());
-		$chainsClient = array();
-		foreach ($chains as $i => $chain) {
-			// if contract or rpcUrls undefined, skip this chain
-			$contract = Q::ifset($chain, "contracts", "NFT", "address", null);
-			$rpcUrl = Q::ifset($chain, "rpcUrl", null);
-			$infuraId = Q::ifset($chain, "providers", "walletconnect", "infura", "projectId", null);
-			$blockExplorerUrl = Q::ifset($chain, "blockExplorerUrl", null);
-			$chainId = Q::ifset($chain, "appId", null);
-
-			if (!$contract || !$rpcUrl) {
-				unset($chain[$i]);
-				continue;
-			}
-
-			$rpcUrl = Q::interpolate($rpcUrl, compact("infuraId"));
-			$temp = compact("chainId", "contract", "rpcUrl", "blockExplorerUrl");
-
-			foreach ($currencies as $currency) {
-				if ($currency[$chainId] == "0x0000000000000000000000000000000000000000") {
-					$temp["currency"] = $currency;
-					$temp["currency"]["token"] = $currency[$chainId];
-					break;
-				}
-			}
-
-			$temp["default"] = $i == Users::communityId();
-
-			$chainsClient[$chainId] = $temp;
-		}
-
-		return $chainsClient;
-	}
-
-	/**
-	 * Get sale info by token
-	 * @method saleInfo
-	 * @static
-	 * @param {String} $tokenId
-	 * @param {String} $chainId
-	 * @param {Boolean} [$updateCache=false] If true request blockchain to update cache
-	 * @return array
-	 */
-	static function getSaleInfo ($tokenId, $chainId, $updateCache=false) {
-		self::construct($chainId);
-		$network = self::$networks[$chainId];
-		$data = self::execute($network["contract"],__FUNCTION__, $tokenId, null, $updateCache);
-		$data[1] = gmp_intval(Q::ifset($data, 1, "value", null));
-		return $data;
 	}
 
 	/**
@@ -425,8 +254,7 @@ class Users_Web3 extends Base_Users_Web3 {
 	 * @return {Db_Row}
 	 */
 	static function getCache (
-		$chainId, 
-		$contract, 
+		$contract,
 		$methodName, 
 		$params, 
 		$cacheDuration)
@@ -435,7 +263,6 @@ class Users_Web3 extends Base_Users_Web3 {
 			$cacheDuration = Q::ifset($appInfo, 'cacheDuration', 3600);
 		}
 		$cached = new Users_Web3(array(
-			'chainId' => $chainId,
 			'contract' => $contract,
 			'methodName' => $methodName,
 			'params' => Q::json_encode($params),
