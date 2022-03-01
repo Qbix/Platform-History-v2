fc4b14379964ba43a391f05a5de8433da877c591
Andrey Tepaykin <andrey@qbix.com>
2022-03-01 19:09:22 +0300

    Removed Assets/NFTProfile and created getOwnerNFTs get handler. This handler get NFTs owned by given userId.

---
+++ platform/plugins/Assets/handlers/Assets/NFT/response/getOwnedNFTs.php
@@ -0,0 +1,36 @@
+<?php
+function Assets_NFT_response_getOwnedNFTs ($params) {
+	$loggedInUser = Users::loggedInUser();
+	$request = array_merge($_REQUEST, $params);
+	$userId = Q::ifset($request, 'userId', $loggedInUser->id);
+
+	$chains = Assets_NFT::getChains();
+	$wallet = Users_Web3::getWalletById($userId, true);
+	$tokenJSON = array();
+
+	// get tokens by owner
+	foreach ($chains as $chain) {
+		$tokens = (int)Users_Web3::execute($chain["contract"], "balanceOf", $wallet, $chain["chainId"]);
+		//$tokens = Users_Web3::execute($chain["contract"], "tokensByOwner", [$wallet, 100], $chain["chainId"]);
+		for ($i = 0; $i < $tokens; $i++) {
+		//foreach ($tokens as $token) {
+			try {
+				$tokenId = (int)Users_Web3::execute($chain["contract"], "tokenOfOwnerByIndex", array($wallet, $i), $chain["chainId"]);
+				$tokenURI = Users_Web3::execute($chain["contract"], "tokenURI", $tokenId, $chain["chainId"]);
+
+				// try to request token URI, if response if not valid json - continue
+				$dataJson = Assets_NFT::getJson($chain["chainId"], $chain["contract"], $tokenURI);
+			} catch (Exception $e) {
+				continue;
+			}
+
+			$tokenJSON[] = array(
+				"tokenId" => $tokenId,
+				"chainId" => $chain["chainId"],
+				"data" => $dataJson
+			);
+		}
+	}
+
+	return $tokenJSON;
+}
