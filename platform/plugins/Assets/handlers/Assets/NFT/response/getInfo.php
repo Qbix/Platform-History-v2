3b59f82f872e3f2620c2bab9d967bc526e3d4753
Andrey Tepaykin <andrey@qbix.com>
2022-03-02 18:20:56 +0300

    Updated Assets/NFT/getInfo handler. Some contract methods set cacheDuration one year, because theydoesn't updated: authorOf, getCommission, tokenURI, getRemoteJSON.

--- platform/plugins/Assets/handlers/Assets/NFT/response/getInfo.php
+++ platform/plugins/Assets/handlers/Assets/NFT/response/getInfo.php
@@ -9,13 +9,15 @@ function Assets_NFT_response_getInfo ($params) {
 	if ($updateCache) {
 		$caching = null;
 		$cacheDuration = 0;
+		$longDuration = 0;
 	} else {
 		$caching = true;
 		$cacheDuration = null;
+		$longDuration = 31104000;
 	}
 	$contractAddress = Assets_NFT::getChains($chainId)["contract"];
 
-	$author = Users_Web3::execute($contractAddress, "authorOf", $tokenId, $chainId, $caching, $cacheDuration);
+	$author = Users_Web3::execute($contractAddress, "authorOf", $tokenId, $chainId, $caching, $longDuration);
 	$user = Users_ExternalTo::select()->where(array(
 		"xid" => $author
 	))->fetchDbRow();
@@ -25,10 +27,10 @@ function Assets_NFT_response_getInfo ($params) {
 
 	$saleInfo = Users_Web3::execute($contractAddress, "saleInfo", $tokenId, $chainId, $caching, $cacheDuration);
 
-	$commissionInfo = Users_Web3::execute($contractAddress, "getCommission", $tokenId, $chainId, $caching, $cacheDuration);
+	$commissionInfo = Users_Web3::execute($contractAddress, "getCommission", $tokenId, $chainId, $caching, $longDuration);
 
-	$url = Users_Web3::execute($contractAddress, "tokenURI", $tokenId, $chainId, $caching, $cacheDuration);
-	$data = Q::event('Assets/NFT/response/getRemoteJSON', compact("url"));
+	$tokenURI = Users_Web3::execute($contractAddress, "tokenURI", $tokenId, $chainId, $caching, $longDuration);
+	$data = Q::event('Assets/NFT/response/getRemoteJSON', compact("chainId", "contractAddress", "tokenURI"));
 
 	return compact("author", "owner", "saleInfo", "commissionInfo", "data", "userId", "url");
 }
