2bbcff2a3ad396db4302cf45b67453e52c73a73b
Andrey Tepaykin <andrey@qbix.com>
2022-03-31 18:47:08 +0300

    Use handler Users/external/response/data to get users wallet address to use it in setSeriesInfo method as author.

--- platform/plugins/Assets/handlers/Assets/NFTseries/response/newItem.php
+++ platform/plugins/Assets/handlers/Assets/NFTseries/response/newItem.php
@@ -1,6 +1,9 @@
 <?php
 function Assets_NFTseries_response_newItem ($params) {
 	$req = array_merge($_REQUEST, $params);
-	$stream = Assets_NFT_Series::getComposerStream($req["chainId"], Q::ifset($req, "userId", null));
-	return array("publisherId" => $stream->publisherId, "streamName" => $stream->name);
+	$userId = Q::ifset($req, "userId", null);
+	$stream = Assets_NFT_Series::getComposerStream($req["chainId"], $userId);
+	$data = Q::event("Users/external/response/data", array("userId"));
+
+	return array("publisherId" => $stream->publisherId, "streamName" => $stream->name, "wallet" => $data["wallet"]);
 }
