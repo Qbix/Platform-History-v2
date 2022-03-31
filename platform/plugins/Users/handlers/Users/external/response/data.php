84fd281a67ea402e0f6198d827912d7cd9df059c
Andrey Tepaykin <andrey@qbix.com>
2022-03-31 18:44:16 +0300

    Created Users/external/response/data handler to get any user wallet address. Available only for admins.

---
+++ platform/plugins/Users/handlers/Users/external/response/data.php
@@ -0,0 +1,16 @@
+<?php
+
+function Users_external_response_data($params) {
+	$req = array_merge($_REQUEST, $params);
+	$loggedInUserId = Users::loggedInUser(true)->id;
+	$userId = Q::ifset($req, "userId", $loggedInUserId);
+	$adminLabels = Q_Config::get("Users", "communities", "admins", null);
+	// if user try to update align profile or is not an admin
+	if ($userId != $loggedInUserId && !(bool)Users::roles(null, $adminLabels, array(), $loggedInUserId)) {
+		throw new Users_Exception_NotAuthorized();
+	}
+
+	$wallet = Users_Web3::getWalletById($userId);
+
+	return compact("wallet");
+}
