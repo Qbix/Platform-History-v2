1e1f508a2ab822b3a954d84df9ea55e8553078c1
Andrey Tepaykin <andrey@qbix.com>
2022-04-04 20:36:32 +0300

    Updated 1.0.8-Assets.mysql.php installaion script. Added access rows for admins and fix title of global contract streams.

--- platform/plugins/Assets/scripts/Assets/1.0.8-Assets.mysql.php
+++ platform/plugins/Assets/scripts/Assets/1.0.8-Assets.mysql.php
@@ -2,14 +2,16 @@
 $communityId = Users::communityId();
 $streamNameTemplate = "Assets/NFT/contract/{{chainId}}";
 $chains = Assets_NFT::getChains();
+$text = Q_Text::get("Assets/content");
 
 foreach ($chains as $chain) {
 	$streamName = Q::interpolate($streamNameTemplate, $chain);
 	$stream = Streams::fetchOne($communityId, $communityId, $streamName);
+	$title = Q::interpolate($text["NFT"]["contract"]["GlobalContractFor"], array("chainNetwork" => $chain["name"]));
 	if (!$stream) {
 		$stream = Streams::create($communityId, $communityId, 'Assets/NFT/contract', array(
 			'name' => $streamName,
-			'title' => "Global Assets/NFT/contract",
+			'title' => $title,
 			'readLevel' => 40,
 			'writeLevel' => 10,
 			'adminLevel' => 20
@@ -19,7 +21,23 @@ foreach ($chains as $chain) {
 
 $offset = 0;
 $i = 0;
-echo "Creating Assets/NFT/contracts categories".PHP_EOL;
+$categoryStreamName = "Assets/NFT/contracts";
+$adminLabels = Q_Config::get("Users", "communities", "admins", null);
+// create access row
+foreach ($adminLabels as $adminLabel) {
+	$access = new Streams_Access();
+	$access->publisherId = "";
+	$access->streamName = $categoryStreamName;
+	$access->ofContactLabel = $adminLabel;
+	if (!$access->retrieve()) {
+		$access->readLevel = 40;
+		$access->writeLevel = 40;
+		$access->adminLevel = 40;
+		$access->save();
+	}
+}
+
+echo "Creating $categoryStreamName categories".PHP_EOL;
 while (1) {
 	$users = Users_User::select()
 		->limit(100, $offset)
@@ -32,12 +50,12 @@ while (1) {
 			continue;
 		}
 
-		$stream = Streams::fetchOne($user->id, $user->id, "Assets/NFT/contracts");
+		$stream = Streams::fetchOne($user->id, $user->id, $categoryStreamName);
 		if ($stream) {
 			continue;
 		}
 		Streams::create($user->id, $user->id, "Streams/category", array(
-			'name' => "Assets/NFT/contracts",
+			'name' => $categoryStreamName,
 			'skipAccess' => true
 		));
 		++$i;
