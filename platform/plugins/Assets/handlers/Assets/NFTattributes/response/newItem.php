8824ca0f490b40453611f9b7b4d8b46ddb8cac66
Andrey Tepaykin <andrey@qbix.com>
2022-04-21 15:23:31 +0300

    Removed Assets/NFTattributes because it non used.

--- platform/plugins/Assets/handlers/Assets/NFTattributes/response/newItem.php
+++
@@ -1,6 +0,0 @@
-<?php
-function Assets_NFT_response_newItem ($params) {
-	$req = array_merge($_REQUEST, $params);
-	$stream = Assets_NFT::stream(Q::ifset($req, "userId", null));
-	return array("publisherId" => $stream->publisherId, "streamName" => $stream->name);
-}
