cbade51989d37e3a58730a80ff43008f5e2b984f
Andrey Tepaykin <andrey@qbix.com>
2022-06-02 18:51:44 +0300

    Updated getRemoteJSON handler. Make it get metadata using just tokenURI.

--- platform/plugins/Assets/handlers/Assets/NFT/response/getRemoteJSON.php
+++ platform/plugins/Assets/handlers/Assets/NFT/response/getRemoteJSON.php
@@ -3,10 +3,20 @@ function Assets_NFT_response_getRemoteJSON ($params) {
 	Q_Valid::nonce(true);
 
 	$request = array_merge($_REQUEST, $params);
-	$required = array('chainId', 'contractAddress', 'tokenURI');
+	$required = array('tokenURI');
 	Q_Valid::requireFields($required, $request, true);
+	$chainId = Q::ifset($request, "chainId", null);
+	$contractAddress = Q::ifset($request, "contractAddress", null);
+	$tokenURI = $request["tokenURI"];
 
-	$dataJson = Assets_NFT::getJson($request["chainId"], $request["contractAddress"], $request["tokenURI"]);
+	if ($chainId && $contractAddress) {
+		$dataJson = Assets_NFT::getJson($chainId, $contractAddress, $tokenURI);
+	} else {
+		$dataJson = Q_Utils::get($tokenURI, null, array(
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => false
+		));
+	}
 
 	return $dataJson;
 }
