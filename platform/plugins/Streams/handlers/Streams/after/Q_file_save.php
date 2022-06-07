570736e9231d4275a88e7d33445076bef7432a7e
Andrey Tepaykin <andrey@qbix.com>
2022-06-07 19:35:01 +0300

    Added Streams/after/Q_file_save event handler. Allow to use third party cloud uploader API to upload videos to. This works for every stream where uploaded video. Video available with videoUrl attribute.

--- platform/plugins/Streams/handlers/Streams/after/Q_file_save.php
+++ platform/plugins/Streams/handlers/Streams/after/Q_file_save.php
@@ -11,6 +11,10 @@ function Streams_after_Q_file_save($params)
 	if (empty($stream)) {
 		return;
 	}
+
+	$filePath = $writePath.$name;
+	$mimeType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $filePath);
+
 	$url = Q_Valid::url($tailUrl) ? $tailUrl : '{{baseUrl}}/'.$tailUrl;
 	$url = str_replace('\\', '/', $url);
 	$stream->setAttribute('Q.file.url', $url);
@@ -18,7 +22,7 @@ function Streams_after_Q_file_save($params)
 	if ($audio) {
 		include_once(Q_CLASSES_DIR.DS.'Audio'.DS.'getid3'.DS.'getid3.php');
 		$getID3 = new getID3;
-		$meta = $getID3->analyze($writePath.$name);
+		$meta = $getID3->analyze($filePath);
 		$bitrate = $meta['audio']['bitrate'];
 		$bits = $size * 8;
 		$duration = $bits / $bitrate;
@@ -43,5 +47,56 @@ function Streams_after_Q_file_save($params)
 		$stream->changed();
 	} else {
 		$stream->save();
+	}
+
+	// video files handler
+	if (preg_match("/^video/", $mimeType)) {
+		// copy Q.file.save attribute to videoUrl attribute to know that video
+		$stream->setAttribute("videoUrl", $url);
+		$stream->save();
+
+		// try to use video provider, if defined in config
+		$cloudUpload = Q_Config::get("Q", "video", "cloudUpload", null);
+		$uploadedToProvider = false;
+		if (!empty($cloudUpload)) {
+			$cloudUploadName = array_key_first($cloudUpload);
+			$className = "Q_Video_".ucfirst($cloudUploadName);
+			try {
+				$result = $className::upload($filePath);
+			} catch (Exception $e) {
+				$result = null;
+			}
+
+			if (Q::isAssociative($result)) {
+				$stream->setAttribute("uploadProvider", $cloudUpload);
+				$stream->setAttribute("videoId", $result["videoId"]);
+				$stream->setAttribute("videoUrl", $result["videoUrl"]);
+				$stream->clearAttribute("Q.file.url");
+				$stream->save();
+				$uploadedToProvider = true;
+			}
+		}
+
+		// convert to animated gif
+		if (Q_Config::get("Q", "environment", null) != "local") { // if send request from local env, the webhook failed and lead to disable on CoudConvert profile.
+			$options = Q_Config::get("Q", "video", "cloudConvert", "options", null);
+			if ($options) {
+				$tag = json_encode(array(
+					"publisherId" => $stream->publisherId,
+					"streamName" => $stream->name
+				));
+
+				try {
+					Q_Video_CloudConvert::convert($filePath, $tag, "gif", $options);
+				} catch (Exception $e) {
+
+				}
+			}
+		}
+
+		// remove local uploaded file if uploaded to video provider
+		if ($uploadedToProvider) {
+			@unlink($filePath);
+		}
 	}
 }
