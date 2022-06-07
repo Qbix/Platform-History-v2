8c6839b4f10d241649d0ec8b9a51f64768fb8058
Andrey Tepaykin <andrey@qbix.com>
2022-06-07 19:30:03 +0300

    Updated Assets/NFT/response/owned handler. Replace $GLOBALS with use($glob)

--- platform/plugins/Assets/handlers/Assets/NFT/response/owned.php
+++ platform/plugins/Assets/handlers/Assets/NFT/response/owned.php
@@ -13,20 +13,20 @@ function Assets_NFT_response_owned ($params) {
 	$loggedInUser = Users::loggedInUser();
 	$request = array_merge($_REQUEST, $params);
 	$userId = Q::ifset($request, 'userId', $loggedInUser->id);
-	$GLOBALS["Assets_NFT_response_owned"] = array();
-	$GLOBALS["Assets_NFT_response_owned"]["offset"] = (int)Q::ifset($request, 'offset', 0);
-	$GLOBALS["Assets_NFT_response_owned"]["limit"] = (int)Q::ifset($request, 'limit', 1000);
+	$glob["Assets_NFT_response_owned"] = array();
+	$glob["Assets_NFT_response_owned"]["offset"] = (int)Q::ifset($request, 'offset', 0);
+	$glob["Assets_NFT_response_owned"]["limit"] = (int)Q::ifset($request, 'limit', 1000);
 	$countNFTs = 0;
-	$GLOBALS["Assets_NFT_response_owned"]["secondsInYear"] = 31104000;
+	$glob["Assets_NFT_response_owned"]["secondsInYear"] = 31104000;
 	$tokensByOwnerLimit = Q_Config::get("Assets", "NFT", "methods", "tokensByOwner", "limit", 100);
 
 	$chains = Assets_NFT::getChains();
 	$wallet = Users_Web3::getWalletById($userId, true);
 	$tokenJSON = array();
 
-	function _Assets_NFT_response_owned_json ($tokenId, $chain, &$tokenJSON, &$countNFTs) {
+	$_Assets_NFT_response_owned_json = function ($tokenId, $chain, &$tokenJSON, &$countNFTs) use($glob) {
 		try {
-			$tokenURI = Users_Web3::execute($chain["contract"], "tokenURI", $tokenId, $chain["chainId"], true, $GLOBALS["Assets_NFT_response_owned"]["secondsInYear"]);
+			$tokenURI = Users_Web3::execute($chain["contract"], "tokenURI", $tokenId, $chain["chainId"], true, $glob["Assets_NFT_response_owned"]["secondsInYear"]);
 
 			// try to request token URI, if response if not valid json - continue
 			$dataJson = Assets_NFT::metadata($chain["chainId"], $chain["contract"], $tokenURI);
@@ -36,9 +36,9 @@ function Assets_NFT_response_owned ($params) {
 
 		$countNFTs++;
 
-		if ($countNFTs <= $GLOBALS["Assets_NFT_response_owned"]["offset"]) {
+		if ($countNFTs <= $glob["Assets_NFT_response_owned"]["offset"]) {
 			return null;
-		} elseif ($countNFTs > $GLOBALS["Assets_NFT_response_owned"]["offset"] + $GLOBALS["Assets_NFT_response_owned"]["limit"]) {
+		} elseif ($countNFTs > $glob["Assets_NFT_response_owned"]["offset"] + $glob["Assets_NFT_response_owned"]["limit"]) {
 			return false;
 		}
 
@@ -49,13 +49,15 @@ function Assets_NFT_response_owned ($params) {
 		);
 
 		return null;
-	}
+	};
 
 	// get tokens by owner
 	foreach ($chains as $chain) {
 		if (empty($chain["contract"])) {
 			continue;
 		}
+
+		//TODO: use balanceOf and tokenOfOwnerByIndex if method tokensByOwner absent
 
 		try {
 			$tokens = Users_Web3::execute($chain["contract"], "tokensByOwner", [$wallet, $tokensByOwnerLimit], $chain["chainId"]);
@@ -68,7 +70,7 @@ function Assets_NFT_response_owned ($params) {
 		}
 
 		foreach ($tokens as $tokenId) {
-			if (_Assets_NFT_response_owned_json($tokenId, $chain, $tokenJSON, $countNFTs) === false){
+			if ($_Assets_NFT_response_owned_json($tokenId, $chain, $tokenJSON, $countNFTs) === false){
 				break;
 			}
 		}
