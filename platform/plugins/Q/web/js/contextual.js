9e18d8be8007e9eab450776d32637392952bbe92
Greg Magarshak <greg@qbix.com>
2022-08-09 01:51:39 -0400

    Hide contextual on mouseout, works over iframes

--- platform/plugins/Q/web/js/contextual.js
+++ platform/plugins/Q/web/js/contextual.js
@@ -402,7 +402,13 @@
 						Q.Contextual.hide();
 					}
 				};
+				Q.Contextual.leaveEventHandler = function (e) {
+					if (e.target.className.split(' ').indexOf('Q_contextual') >= 0) {
+						Q.Contextual.hide();
+					}
+				};
 				$(document.body).on(Q.Pointer.enter, Q.Contextual.enterEventHandler);
+				$(document.body).on(Q.Pointer.leave, Q.Contextual.leaveEventHandler);
 			
 				Q.Contextual.endEventHandler = function(e)
 				{
