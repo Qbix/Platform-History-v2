79015f64913f9d12824b796211e0be1fa8f973c3
Andrey Tepaykin <andrey@qbix.com>
2022-08-11 18:01:00 +0300

    Replaced installation script Assets/1.0.3-Assets.mysql.php with Assets/1.0.10-Streams.mysql.php

--- platform/plugins/Assets/scripts/Assets/1.0.10-Streams.mysql.php
+++ platform/plugins/Assets/scripts/Assets/1.0.10-Streams.mysql.php
@@ -1,6 +1,6 @@
 <?php
 
-function Assets_1_0_3()
+function Streams_1_0_10()
 {
 	$communityId = Users::communityId();
 	$streamName = "Assets/services";
@@ -32,4 +32,4 @@ function Assets_1_0_3()
 	}
 }
 
-Assets_1_0_3();
+Streams_1_0_10();
