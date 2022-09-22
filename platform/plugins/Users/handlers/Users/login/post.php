7fcd5f792c63e06f24f9886722a9f7520dbd4c1b
Greg Magarshak <greg@qbix.com>
2022-09-22 12:17:10 -0400

    Fixed handling of hashed passphrase

--- platform/plugins/Users/handlers/Users/login/post.php
+++ platform/plugins/Users/handlers/Users/login/post.php
@@ -3,15 +3,14 @@
 function Users_login_post()
 {
 	$identifier = Users::requestedIdentifier();
-	if (!empty($_REQUEST['passphrase_hashed'])) {
-		$isHashed = true;
-		$passphrase = $_REQUEST['passphrase_hashed'];
-	} else {
-		$isHashed = !empty($_REQUEST['isHashed']) ? $_REQUEST['isHashed'] : false;
+	if (!empty($_REQUEST['passphrase'])) {
 		$passphrase = $_REQUEST['passphrase'];
-		if (empty($passphrase)) {
-			throw new Q_Exception("Please enter your pass phrase", 'passphrase');
-		}
+		$isHashed = !empty($_REQUEST['isHashed']) ? $_REQUEST['isHashed'] : false;
+	} else if (!empty($_REQUEST['passphrase_hashed'])) {
+		$passphrase = $_REQUEST['passphrase_hashed'];
+		$isHashed = true;
+	} else {
+		throw new Q_Exception("Please enter your pass phrase", 'passphrase');
 	}
 	$user = Users::login($identifier, $passphrase, $isHashed);
 	Users::$cache['user'] = $user;
