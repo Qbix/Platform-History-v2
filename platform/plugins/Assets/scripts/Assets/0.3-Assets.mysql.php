80301e577db75d366d47e21913af57471212e370
Andrey Tepaykin <andrey@qbix.com>
2022-09-28 16:32:05 +0300

    Updated installation script where Assets/plans category created. Create Assets/access rows for admins.

--- platform/plugins/Assets/scripts/Assets/0.3-Assets.mysql.php
+++ platform/plugins/Assets/scripts/Assets/0.3-Assets.mysql.php
@@ -10,9 +10,24 @@ function Assets_0_3()
 		return;
 	}
 
-	Streams::create($app, $app, 'Streams/category', 
-		array('name' => $streamName, 'title' => 'Subscription Plans')
+	Streams::create($app, $app, 'Streams/category',
+		array('name' => $streamName)
 	);
+
+	$adminLabels = array("Users/owners", "Users/admins", "Assets/admins");
+	foreach ($adminLabels as $adminLabel) {
+		$access = new Streams_Access();
+		$access->publisherId = "";
+		$access->streamName = $streamName;
+		$access->ofContactLabel = $adminLabel;
+		if ($access->retrieve()) {
+			continue;
+		}
+		$access->readLevel = 40;
+		$access->writeLevel = 40;
+		$access->adminLevel = 40;
+		$access->save();
+	}
 }
 
 Assets_0_3();
