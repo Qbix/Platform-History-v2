9399e21702e6d76ea51dd8191acc06fc365387d7
Andrey Tepaykin <andrey@qbix.com>
2022-09-29 17:47:07 +0300

    Wrap $email->sendMessage and $mobile->sendMessage to try construction because it can throw excetion and hence break all payment process.

--- platform/plugins/Assets/handlers/Assets/after/Assets_charge.php
+++ platform/plugins/Assets/handlers/Assets/after/Assets_charge.php
@@ -18,7 +18,6 @@ function Assets_after_Assets_charge($params)
 		"token" => Q::ifset($options, 'token', null)
 	));
 
-	//TODO: as we come to use credits system, need to change this to send email with amount of credits bought
 	$text = Q_Text::get('Assets/content', array('language' => Users::getLanguage($user->id)));
 	$description = Q::interpolate(Q::ifset($text, 'credits', 'forMessages', 'BoughtCredits', Q::ifset($text, 'credits', 'BoughtCredits', 'Bought {{amount}} credits')), array('amount' => $credits));
 	$stream = Q::ifset($options, 'stream', null);
@@ -38,7 +37,6 @@ function Assets_after_Assets_charge($params)
 		$publisherId = Users::communityId();
 		$publisher = Users_User::fetch($publisherId, true);
 	}
-	$description = Q::ifset($options, 'description', null);
 	list($currencyName, $symbol) = Assets::currency($currency);
 	$displayAmount = Assets::display($currency, $amount);
 	$communityId = Users::communityId();
@@ -62,14 +60,18 @@ function Assets_after_Assets_charge($params)
 		$emailSubject = Q_Config::get('Assets', 'transactional', 'charged', 'subject', false);
 		$emailView = Q_Config::get('Assets', 'transactional', 'charged', 'body', false);
 		if ($emailSubject !== false and $emailView) {
-			$email->sendMessage($emailSubject, $emailView, $fields);
+			try {
+				$email->sendMessage($emailSubject, $emailView, $fields);
+			} catch (Exception $e) {}
 		}
 	} else if ($user->mobileNumber) {
 		$mobile = new Users_Mobile();
 		$mobile->number = $user->mobileNumber;
 		$mobile->retrieve(true);
 		if ($mobileView = Q_Config::get('Assets', 'transactional', 'charged', 'sms', false)) {
-			$mobile->sendMessage($mobileView, $fields);
+			try {
+				$mobile->sendMessage($mobileView, $fields);
+			} catch (Exception $e) {}
 		}
 	}
 
@@ -80,14 +82,18 @@ function Assets_after_Assets_charge($params)
 		$emailSubject = Q_Config::get('Assets', 'transactional', 'charge', 'subject', false);
 		$emailView = Q_Config::get('Assets', 'transactional', 'charge', 'body', false);
 		if ($emailSubject !== false and $emailView) {
-			$email->sendMessage($emailSubject, $emailView, $fields);
+			try {
+				$email->sendMessage($emailSubject, $emailView, $fields);
+			} catch (Exception $e) {}
 		}
 	} else if ($publisher->mobileNumber) {
 		$mobile = new Users_Mobile();
 		$mobile->number = $publisher->mobileNumber;
 		$mobile->retrieve(true);
 		if ($mobileView = Q_Config::get('Assets', 'transactional', 'charge', 'sms', false)) {
-			$mobile->sendMessage($mobileView, $fields);
+			try {
+				$mobile->sendMessage($mobileView, $fields);
+			} catch (Exception $e) {}
 		}
 	}
 }
