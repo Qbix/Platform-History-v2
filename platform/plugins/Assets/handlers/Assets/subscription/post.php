3a480bd915feb7aaf49733c7d088f4cc9dc6fae8
Andrey Tepaykin <andrey@qbix.com>
2022-10-03 17:19:22 +0300

    Updated Assets/subscription/post handler: 1) Check if subscription stream is current, means already paid. 2) Call Assets/payment/post handler with appropriate params to try charge funds immediately. And return reult to client.

--- platform/plugins/Assets/handlers/Assets/subscription/post.php
+++ platform/plugins/Assets/handlers/Assets/subscription/post.php
@@ -12,20 +12,34 @@
  * @param {string} $_REQUEST.payments Required. Should be either "authnet" or "stripe"
  *  @param {String} $_REQUEST.planStreamName the name of the subscription plan's stream
  *  @param {String} [$_REQUEST.planPublisherId=Users::communityId()] the publisher of the subscription plan's stream
- *  @param {String} [$_REQUEST.token=null] if using stripe, pass the token here
  */
 function Assets_subscription_post($params = array())
 {
     $req = array_merge($_REQUEST, $params);
-	Q_Valid::requireFields(array('payments'), $req, true);
+	Q_Valid::requireFields(array('payments', 'planStreamName'), $req, true);
 	
 	// to be safe, we only start subscriptions from existing plans
 	$planPublisherId = Q::ifset($req, 'planPublisherId', Users::communityId());
-	$plan = Streams_Stream::fetch($planPublisherId, $planPublisherId, $req['planStreamName'], true);
-	
+	$plan = Streams::fetchOne($planPublisherId, $planPublisherId, $req['planStreamName'], true);
+
+	$subscriptionStream = Assets_Subscription::getStream($plan);
+
+	// check if subscription already paid
+	if ($subscriptionStream && Assets_Subscription::isCurrent($subscriptionStream)) {
+		throw new Exception("This subscription already paid");
+	}
+
+	// try to charge
+	$subscription = Q::event('Assets/payment/post', array(
+		'payments' => $req['payments'],
+		'amount' => $plan->getAttribute('amount'),
+		'currency' => $plan->getAttribute('currency', 'USD'),
+		'description' => $plan->title,
+		'publisherId' => $planPublisherId,
+		'streamName' => $req['planStreamName']
+	));
+
 	// the currency will always be assumed to be "USD" for now
 	// and the amount will always be assumed to be in dollars, for now
-	$token = Q::ifset($req, 'token', null);
-	$subscription = Assets_Subscription::start($plan, $req['payments'], @compact('token'));
 	Q_Response::setSlot('subscription', $subscription);
 }
