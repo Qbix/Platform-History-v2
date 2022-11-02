666cabd28de29e4b8a6c64aaddc006e3fdae9a4e
Andrey Tepaykin <andrey@qbix.com>
2022-11-02 16:44:23 +0300

    Updated Assets/crypto/currencies tool. Added onStateChanged("chainId") to allow change chan and refresh tool. And added method getValue which return currently selected currency object.

--- platform/plugins/Assets/web/js/tools/crypto/currencies.js
+++ platform/plugins/Assets/web/js/tools/crypto/currencies.js
@@ -29,6 +29,10 @@ Q.Tool.define("Assets/crypto/currencies", function (options) {
 	});
 
 	Q.addStylesheet('{{Assets}}/css/tools/crypto/currencies.css', pipe.fill("styles"), { slotName: 'Assets' });
+
+	tool.Q.onStateChanged('chainId').set(function (name) {
+		tool.refresh();
+	}, tool);
 },
 
 { // default options here
@@ -37,17 +41,22 @@ Q.Tool.define("Assets/crypto/currencies", function (options) {
 },
 
 { // methods go here
+	/**
+	 * Refresh tool
+	 * @static
+	 * @method refresh
+	 */
 	refresh: function () {
 		var tool = this;
 		var state = this.state;
-		var currencies = {};
+		tool.currencies = {};
 		Q.each(NFT.currencies, function (i, obj) {
 			var token = Q.getObject(state.chainId, obj);
 			if (!token) {
 				return;
 			}
 
-			currencies[obj.symbol] = {
+			tool.currencies[obj.symbol] = {
 				symbol: obj.symbol,
 				name: obj.name,
 				decimals: obj.decimals,
@@ -56,16 +65,25 @@ Q.Tool.define("Assets/crypto/currencies", function (options) {
 		});
 
 		Q.Template.render("Assets/crypto/currencies", {
-			currencies: currencies
+			currencies: tool.currencies
 		}, function (err, html) {
 			tool.element.innerHTML = html;
 
 			var $select = $("select[name=currency]", tool.element);
 			$select.on("change", function () {
-				Q.handle(state.onChoose, tool, [null, currencies[$select.val()]]);
+				Q.handle(state.onChoose, tool, [null, tool.currencies[$select.val()]]);
 			});
-			Q.handle(state.onChoose, tool, [null, currencies[$select.val()]]);
+			Q.handle(state.onChoose, tool, [null, tool.currencies[$select.val()]]);
 		});
+	},
+	/**
+	 * Get currently selected currency
+	 * @static
+	 * @method getValue
+	 * @return {Object}
+	 */
+	getValue: function () {
+		return this.currencies[$("select[name=currency]", this.element).val()];
 	}
 });
 
