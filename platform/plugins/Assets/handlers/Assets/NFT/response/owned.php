0f8a7e78420fa33a220b46bb96d25d12cb9add5b
Andrey Tepaykin <andrey@qbix.com>
2022-11-16 16:08:30 +0300

    Updated Assets/NFT/owned handler. Added option "skipInfo", which if true skip getting info like "authorOf", "ownerOf", "saleInfo", "getCommission", "tokenURI", "metaData".

--- platform/plugins/Assets/handlers/Assets/NFT/response/owned.php
+++ platform/plugins/Assets/handlers/Assets/NFT/response/owned.php
@@ -13,11 +13,11 @@ function Assets_NFT_response_owned ($params) {
 	$loggedInUser = Users::loggedInUser();
 	$request = array_merge($_REQUEST, $params);
 	$userId = Q::ifset($request, 'userId', $loggedInUser->id);
-	$glob["Assets_NFT_response_owned"] = array();
-	$glob["Assets_NFT_response_owned"]["offset"] = (int)Q::ifset($request, 'offset', 0);
-	$glob["Assets_NFT_response_owned"]["limit"] = (int)Q::ifset($request, 'limit', 1000);
+	$glob = array();
+	$glob["offset"] = (int)Q::ifset($request, 'offset', 0);
+	$glob["limit"] = (int)Q::ifset($request, 'limit', 1000);
 	$countNFTs = 0;
-	$glob["Assets_NFT_response_owned"]["secondsInYear"] = 31104000;
+	$glob["secondsInYear"] = 31104000;
 	$tokensByOwnerLimit = Q_Config::get("Assets", "NFT", "methods", "tokensByOwner", "limit", 100);
 
 	$chains = Assets_NFT::getChains();
@@ -25,10 +25,16 @@ function Assets_NFT_response_owned ($params) {
 	$chainId = Q::ifset($request, 'chainId', null);
 	$contractAddress = Q::ifset($request, 'contractAddress', null);
 	$pathABI = Q::ifset($request, 'pathABI', "Assets/templates/R1/NFT/contract");
+	$glob["skipInfo"] = Q::ifset($request, 'skipInfo', false);
 
 	$tokenJSON = array();
 
 	$_Assets_NFT_response_owned_json = function ($tokenId, $chain, $ABI, &$tokenJSON, &$countNFTs) use($glob) {
+		if ($glob["skipInfo"]) {
+			$tokenJSON[] = compact("tokenId");
+			return null;
+		}
+
 		try {
 			$dataJson = Q::event('Assets/NFT/response/getInfo', array(
 				"tokenId" => $tokenId,
@@ -42,9 +48,9 @@ function Assets_NFT_response_owned ($params) {
 
 		$countNFTs++;
 
-		if ($countNFTs <= $glob["Assets_NFT_response_owned"]["offset"]) {
+		if ($countNFTs <= $glob["offset"]) {
 			return null;
-		} elseif ($countNFTs > $glob["Assets_NFT_response_owned"]["offset"] + $glob["Assets_NFT_response_owned"]["limit"]) {
+		} elseif ($countNFTs > $glob["offset"] + $glob["limit"]) {
 			return false;
 		}
 
@@ -94,7 +100,7 @@ function Assets_NFT_response_owned ($params) {
 		} elseif ($balanceOf && $tokenOfOwnerByIndex) {
 			$tokens = (int)Users_Web3::execute($pathABI, $chain["contract"], "balanceOf", $accountAddress, $chain["chainId"]);
 			for ($i = 0; $i < $tokens; $i++) {
-				$tokenId = (int)Users_Web3::execute($pathABI, $chain["contract"], "tokenOfOwnerByIndex", array($accountAddress, $i), $chain["chainId"], true, $glob["Assets_NFT_response_owned"]["secondsInYear"]);
+				$tokenId = (int)Users_Web3::execute($pathABI, $chain["contract"], "tokenOfOwnerByIndex", array($accountAddress, $i), $chain["chainId"], true, $glob["secondsInYear"]);
 				if ($_Assets_NFT_response_owned_json($tokenId, $chain, $ABI, $tokenJSON, $countNFTs) === false) {
 					break;
 				}
--- platform/plugins/Assets/views/Assets/column/NFTowned.php
+++ platform/plugins/Assets/views/Assets/column/NFTowned.php
@@ -2,7 +2,7 @@
 echo Q::tool("Assets/NFT/owned", array(
 	"userId" => $userId,
 	"accountAddress" => "0x6F1E437dBb8BA1709a14c17E8A6647D336Bf0C89",
-	"chainId" => "0x13881",
+	"chainId" => "0x13881"
 	//"contractAddress" => "0x6F1E437dBb8BA1709a14c17E8A6647D336Bf0C89"
 ))
 
