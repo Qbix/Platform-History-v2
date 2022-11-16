3a24f664366d0dddb743479df809778aba0010ed
Andrey Tepaykin <andrey@qbix.com>
2022-11-16 15:49:58 +0300

    Test with Assets/NFT/owned tool

--- platform/plugins/Assets/views/Assets/column/NFTowned.php
+++ platform/plugins/Assets/views/Assets/column/NFTowned.php
@@ -1,6 +1,16 @@
-<?=Q::tool("Assets/NFT/owned", array(
+<?php
+echo Q::tool("Assets/NFT/owned", array(
 	"userId" => $userId,
 	"accountAddress" => "0x6F1E437dBb8BA1709a14c17E8A6647D336Bf0C89",
 	"chainId" => "0x13881",
 	//"contractAddress" => "0x6F1E437dBb8BA1709a14c17E8A6647D336Bf0C89"
-), "2")?>
+))
+
+/*echo Q::tool("Assets/NFT/owned", array(
+	"userId" => $userId,
+	//"accountAddress" => "0x6F1E437dBb8BA1709a14c17E8A6647D336Bf0C89",
+	"chainId" => "0x61",
+	"contractAddress" => "0xf6bfca1c64e8124440795a9e654be8029ca91829",
+	"pathABI" => "ITR/templates/NFT/0xf6bfca1c64e8124440795a9e654be8029ca91829"
+))*/
+?>
