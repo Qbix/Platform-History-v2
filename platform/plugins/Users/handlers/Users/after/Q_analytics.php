12da95b21aeeeb3ca70b2596f37125d624c1dec2
Greg Magarshak <greg@qbix.com>
2022-11-22 00:17:07 -0600

    Fixed Q_analytics hook in Users

--- platform/plugins/Users/handlers/Users/after/Q_analytics.php
+++ platform/plugins/Users/handlers/Users/after/Q_analytics.php
@@ -2,8 +2,8 @@
 
 function Users_after_Q_analytics()
 {
-	$url = Q_Request::url();
+	$url = Q_Request::tail();
 
 	Users_Vote::saveActivity("app", "visited");
-	Users_Vote::saveActivity("page", base64_encode($url));
+	Users_Vote::saveActivity("page", substr($url, 120));
 }
