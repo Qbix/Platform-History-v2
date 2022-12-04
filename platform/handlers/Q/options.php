240f8ac3941af0abc5d303e63469f2d9de31d31a
Greg Magarshak <greg@qbix.com>
2022-12-04 01:56:39 -0500

    Implemented default OPTIONS handler for preflight requests

--- platform/handlers/Q/options.php
+++ platform/handlers/Q/options.php
@@ -6,7 +6,50 @@ function Q_options($params)
 	$module = $uri->module;
 	$action = $uri->action;
 	if (!Q::canHandle("$module/$action/options")) {
-		return null;
+
+		$origin = Q::ifset($_SERVER, "HTTP_ORIGIN", null);
+		$allowed = array('OPTIONS');
+
+		$allow = false;
+		if ($origin) {
+			$scheme = Q::ifset($_SERVER, "REQUEST_SCHEME", null);
+			$host = Q::ifset($_SERVER, "HTTP_HOST", null);
+			if ("$scheme://$host" === $origin) {
+				$allow = true;
+			} else {
+				$url = Q_Request::baseUrl();
+				$scheme2 = parse_url($url, PHP_URL_SCHEME);
+				$host2 = parse_url($url, PHP_URL_HOST);
+				if ($_SERVER["HTTP_ORIGIN"] === "$scheme2://$host2") {
+					$allow = true;
+				}
+			}
+		}
+
+		if ($allow) {
+			$METHODS = array(
+				'response' => 'GET', 
+				'post' => 'POST', 
+				'put' => 'PUT', 
+				'delete' => 'DELETE'
+			);
+			foreach ($METHODS as $m => $M) {
+				if (Q::canHandle("$module/$action/$m")) {
+					$allowed[] = $M;
+				}
+			}
+		}
+
+		echo <<<EOT
+
+HTTP/1.1 204 No Content
+Connection: keep-alive
+Access-Control-Allow-Origin: $origin
+Access-Control-Allow-Methods: $METHODS
+Access-Control-Max-Age: 86400
+
+EOT;
+		return true;
 	}
 	return Q::event("$module/$action/options", $params);
 }
