735bc0254fd9603f3028411f02dcbcedfb82e23c
Greg Magarshak <greg@qbix.com>
2022-12-04 03:04:33 -0500

    Made Q/clickable wait for animations to complete

--- platform/plugins/Q/web/js/fn/clickable.js
+++ platform/plugins/Q/web/js/fn/clickable.js
@@ -475,7 +475,7 @@ Q.Tool.jQuery('Q/clickable', function _Q_clickable(o) {
 		renderingInterval: 100,
 		waitingPeriod: 0,
 		waitingInterval: 100,
-		renderingDelay: 0
+		renderingDelay: 500 // wait for animations to complete
 	},
 	selectable: false,
 	allowCallout: false,
