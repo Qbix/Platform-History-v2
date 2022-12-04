9cea70b59d4fce6fab194abb32db404bbe24e750
Greg Magarshak <greg@qbix.com>
2022-12-04 03:30:52 -0500

    Debounce layout by 500ms

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -3224,7 +3224,7 @@ Q.onLayout = function (element) {
 			}
 		}
 	}, Q.onLayout.debounce || 0), 'Q');
-	return event.debounce(Q.onLayout.debounce);
+	return event.debounce(Q.onLayout.debounce, false, 'Q.onLayout');
 }
 Q.onLayout.debounce = 500;
 Q.onLayout().set(function () {
