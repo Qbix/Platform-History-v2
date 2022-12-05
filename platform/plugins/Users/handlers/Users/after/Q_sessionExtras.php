85b563cc1647204a688516bccfcdee240520b371
Greg Magarshak <greg@qbix.com>
2022-12-04 21:42:07 -0500

    Started defining Users.Labels.canX again

--- platform/plugins/Users/handlers/Users/after/Q_sessionExtras.php
+++ platform/plugins/Users/handlers/Users/after/Q_sessionExtras.php
@@ -12,6 +12,18 @@ function Users_after_Q_sessionExtras() {
 		Q_Response::addHtmlCssClass('Users_role-'.ucfirst(Q_Utils::normalize($role->label)));
 		Q_Response::setScriptData('Q.plugins.Users.roles.'.$label, $role->exportArray());
 	}
+	$config = Q_Config::get('Users', 'communities', 'roles', array());
+	foreach (array('canAdd', 'canRemove', 'canSee') as $can) {
+		$results = array();
+		foreach ($roles as $r => $contact) {
+			if ($info = Q::ifset($config, $r, null)) {
+				if ($more = Q::ifset($info, $can, false)) {
+					$results = array_merge($results, $more);
+				}
+			}
+		}
+		Q_Response::setScriptData("Q.plugins.Users.Label.$can", $results);
+	}
 	$user = Users::loggedInUser(false, false);
 	Q_Response::setScriptData("Q.plugins.Users.capability", Users::capability()->exportArray());
 	Q_Response::addHtmlCssClass($user ? 'Users_loggedIn' : 'Users_loggedOut');
