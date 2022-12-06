f52b937b8807ecb933992c2b25ff6fbd5f741e6c
Greg Magarshak <greg@qbix.com>
2022-12-06 10:57:32 -0500

    Made error reporting of missing methods more helpful

--- platform/plugins/Users/web/js/Users.js
+++ platform/plugins/Users/web/js/Users.js
@@ -4271,7 +4271,19 @@
 				contractAddress, 
 				function (err, contract) {
 					if (!contract[methodName]) {
-						return Q.handle(callback, null, ["Q.Users.Web3.execute: missing method " + methodName]);
+						var possibilities = [];
+						var m = methodName.match(/[A-Za-z1-9]+/);
+						if (m) {
+							for (var k in contract) {
+								if (k.startsWith(m[0])) {
+									possibilities.push(k);
+								}
+							}
+						}
+						var err = "Q.Users.Web3.execute: missing method " + methodName + "\n"
+							+ "But perhaps you meant these method names: \n" + possibilities.join("\n");
+						console.error(err);;
+						return Q.handle(callback, null, [err]);
 					}
 					contract[methodName].apply(null, params).then(function (result) {
 						Q.handle(callback, null, [null, result]);
