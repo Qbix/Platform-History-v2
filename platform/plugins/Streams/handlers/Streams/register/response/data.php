86b810d3dcd04a23fd795d70d71dce520f9b7b45
Greg Magarshak <greg@qbix.com>
2022-12-12 02:47:59 -0500

    Fixed data.php call user.id not user

--- platform/plugins/Streams/handlers/Streams/register/response/data.php
+++ platform/plugins/Streams/handlers/Streams/register/response/data.php
@@ -4,7 +4,7 @@ function Streams_register_response_data()
 {
 	$fields = Users::responseData();
 	if (!empty($fields['user'])) {
-		$fields['user']['displayName'] = Streams::displayName($fields['user']);
+		$fields['user']['displayName'] = Streams::displayName($fields['user']['id']);
 	}
 	return $fields;
 }
