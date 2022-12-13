1545d3733530b877d0231f568123b11bb2f80556
Andrey Tepaykin <andrey@qbix.com>
2022-12-13 18:07:52 +0300

    Make About page editable by admins.

--- MyApp/views/MyApp/content/about.php
+++ MyApp/views/MyApp/content/about.php
@@ -4,6 +4,11 @@
 			Users::communityName()
 		)) ?>
 	</h1>
-	<?php echo Q::text($about['CanWrite']) ?><br>
-	<?php echo Q::text($about['OrRemove']) ?>
+
+	<?=Q::tool("Streams/html", array(
+		"publisherId" => $communityId,
+		"streamName" => "Streams/community/about",
+		"field" => "content",
+		"placeholder" => Q::text($about['CanWrite'])."<br>".Q::text($about['OrRemove'])
+	))?>
 </div>
