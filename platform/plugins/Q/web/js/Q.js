60ce0fc614f76c98042aaf700e3e9348707518c0
Greg Magarshak <greg@qbix.com>
2022-12-15 18:19:51 -0500

    Handling fractional colors

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -15109,7 +15109,7 @@ Q.Camera = {
 	 */
 	toHex: function (r, g, b) {
 		return [r, g, b].map(x => {
-			const hex = x.toString(16)
+			const hex = Math.round(x).toString(16)
 			return hex.length === 1 ? '0' + hex : hex
 		  }).join('');
 	},
