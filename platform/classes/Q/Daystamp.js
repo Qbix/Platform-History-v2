5bf1e3db5ec8fba82a4c9ee180ae112b5c9d3b29
Greg Magarshak <greg@qbix.com>
2022-12-31 22:15:29 -0500

    fixes and comments

--- platform/classes/Q/Daystamp.js
+++ platform/classes/Q/Daystamp.js
@@ -7,39 +7,94 @@ var Q = require('../Q');
  * days since fictional epoch year=0, month=0, day=1.
  * You can store daystamps and do arithmetic with them.
  * @class Daystamp
- * @namespace Q
  */
 Q.Daystamp = {
+    /**
+     * Get daystamp from a Javascript milliseconds-based timestamp
+     * @method fromTimestamp
+     * @static
+     * @param {Number} timestamp 
+     * @return {Number}
+     */
 	fromTimestamp: function (timestamp) {
 		return Math.round(
 			(timestamp - Q.Daystamp.epoch) / Q.Daystamp.msPerDay
 		);
 	},
+    
+    /**
+     * Get daystamp from a Javascript Date object
+     * @method fromDate
+     * @static
+     * @param {Date} date 
+     * @return {Number}
+     */
 	fromDate: function (date) {
 		return Q.Daystamp.fromTimestamp(date.getTime());
 	},
+
+    /**
+     * Get daystamp from a string of the form "yyyy-mm-dd"
+     * or "yyyy-mm-dd hh:mm:ss"
+     * @method fromDateTime
+     * @static
+     * @param {String} datetime 
+     * @return {Number}
+     */
 	fromDateTime: function (datetime) {
-		var time = Date.parse(datetime + ' UTC');
-		return Math.round(
-			(new Date(time).getTime() - Q.Daystamp.epoch) / Q.Daystamp.msPerDay
-		);
+		return this.fromTimestamp(Date.parse(datetime + ' UTC'));
 	},
-	fromYMD: function (y, m, d) {
-		var date = new Date();
-		date.setUTCFullYear(y, m+1, d);
+
+    /**
+     * Get daystamp from a string of the form "yyyy-mm-dd"
+     * or "yyyy-mm-dd hh:mm:ss"
+     * @method fromYMD
+     * @static
+     * @param {Number} year 
+     * @param {Number} month January is 1
+     * @param {Number} day
+     * @return {Number}
+     */
+	fromYMD: function (year, month, day) {
+		const date = new Date();
+		date.setUTCFullYear(year, month-1, day);
 		date.setUTCHours(0, 0, 0);
 		return Math.round(
 			(date.getTime() - Q.Daystamp.epoch) / Q.Daystamp.msPerDay
 		);
 	},
+
+    /**
+     * Get Javascript milliseconds-based timestamp from a daystamp
+     * @method toTimestamp
+     * @static
+     * @param {Number} daystamp 
+     * @return {Number}
+     */
 	toTimestamp: function (daystamp) {
 		return Q.Daystamp.epoch + Q.Daystamp.msPerDay * daystamp;
 	},
+
+    /**
+     * Get Javascript Date from a daystamp
+     * @method toDate
+     * @static
+     * @param {Number} daystamp 
+     * @return {Date}
+     */
 	toDate: function (daystamp) {
 		return new Date(Q.Daystamp.toTimestamp(daystamp));
 	},
+
+    /**
+     * Get date-time string from a daystamp
+     * @method toDateTime
+     * @static
+     * @param {Number} daystamp 
+     * @return {String} String of the form "yyyy-mm-dd 00:00:00"
+     */
 	toDateTime(daystamp, separator) {
-		var date = Q.Daystamp.toDate(daystamp);
+		const date = Q.Daystamp.toDate(daystamp);
 		if (separator === undefined) {
 			separator = ' ';
 		}
@@ -48,16 +103,29 @@ Q.Daystamp = {
 			+ '-' + String(date.getUTCDate()).padStart(2, 0)
 			+ separator + '00:00:00';
 	},
+
+    /**
+     * Get Javascript milliseconds-based timestamp from a daystamp
+     * @method toYMD
+     * @static
+     * @param {Number} daystamp 
+     * @return {Array} [year, month, date] with month, January is 1
+     */
 	toYMD: function (daystamp) {
-		var date = Q.Daystamp.toDate(daystamp);
+		const date = Q.Daystamp.toDate(daystamp);
 		return [
 			date.getUTCFullYear(),
-			date.getUTCMonth(),
+			date.getUTCMonth() + 1,
 			date.getUTCDate()
 		];
 	}
 };
 
+/**
+ * The daystamp epoch as a timestamp
+ * @property epoch
+ * @static
+ */
 Object.defineProperty(Q.Daystamp, 'epoch', {
 	value: -62167219200000,
 	configurable: false,
@@ -65,6 +133,11 @@ Object.defineProperty(Q.Daystamp, 'epoch', {
 	enumerable: true
 });
 
+/**
+ * Number of milliseconds in a day
+ * @property msPerDay
+ * @static
+ */
 Object.defineProperty(Q.Daystamp, 'msPerDay', {
 	value: 8.64e7,
 	configurable: false,
