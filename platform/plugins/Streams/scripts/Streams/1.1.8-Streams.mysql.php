d8aaeefc9d89ae355fe1249cca15eb92d9fce644
Andrey Tepaykin <andrey@qbix.com>
2023-02-23 15:49:28 +0300

    Updated install script 1.1.8-Streams.mysql.php Set access for Streams/question too.

--- platform/plugins/Streams/scripts/Streams/1.1.8-Streams.mysql.php
+++ platform/plugins/Streams/scripts/Streams/1.1.8-Streams.mysql.php
@@ -2,20 +2,20 @@
 	
 function Streams_1_1_8_Streams()
 {
-	echo "Adding access for Streams/answer stream type".PHP_EOL;
+	echo "Adding access for Streams/question and Streams/answer stream type".PHP_EOL;
 
-	$streamType = "Streams/answer";
-	$adminLabels = Streams_Stream::getConfigField($streamType, 'admins', array());
-	foreach ($adminLabels as $adminLabel) {
-		$access = new Streams_Access();
-		$access->publisherId = "";
-		$access->streamName = $streamType."*";
-		$access->ofContactLabel = $adminLabel;
-		if (!$access->retrieve()) {
+	$streamTypes = array("Streams/question", "Streams/answer");
+	foreach ($streamTypes as $streamType) {
+		$adminLabels = Streams_Stream::getConfigField($streamType, 'admins', array());
+		foreach ($adminLabels as $adminLabel) {
+			$access = new Streams_Access();
+			$access->publisherId = "";
+			$access->streamName = $streamType."*";
+			$access->ofContactLabel = $adminLabel;
 			$access->readLevel = 40;
 			$access->writeLevel = 40;
 			$access->adminLevel = 40;
-			$access->save();
+			$access->save(true);
 		}
 	}
 
