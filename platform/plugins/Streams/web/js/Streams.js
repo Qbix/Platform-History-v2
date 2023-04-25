6c9c8171793636a8afac71557f3ea016862efbe7
Andrey Tepaykin <andrey@qbix.com>
2023-04-25 20:02:04 +0300

    Updated method _setPhoto for multiple users invitations. Don't close Dialog_invite_photo_camera dialogs.

--- platform/plugins/Streams/web/js/Streams.js
+++ platform/plugins/Streams/web/js/Streams.js
@@ -1855,7 +1855,7 @@ Streams.invite = function (publisherId, streamName, options, callback) {
                     + '</div>',
                     onActivate: function (dialog) {
                         // fill QR code
-                        Q.addScript("{{Q}}/js/qrcode/qrcode.js", function(){
+                        Q.addScript("{{Q}}/js/qrcode/qrcode.js", function () {
                             var $qrIcon = $(".Streams_invite_QR_content", dialog);
                             new QRCode($qrIcon[0], {
                                 text: rsd.url,
@@ -1866,6 +1866,7 @@ Streams.invite = function (publisherId, streamName, options, callback) {
                                 correctLevel : QRCode.CorrectLevel.H
                             });
                             var _setPhoto = function (message) {
+                            	var dialogClassName = "Dialog_invite_photo_camera";
                             	var invitingUserId = Q.getObject("byUserId", message);
                             	var title = Q.getObject(['invite', 'dialog', 'photo'], text);
                             	if (invitingUserId) {
@@ -1876,11 +1877,17 @@ Streams.invite = function (publisherId, streamName, options, callback) {
 									subpath = invitingUserId.splitId() + '/icon/' + Math.floor(Date.now()/1000);
 								}
 
-                            	Q.Dialogs.pop();
+								if (Q.Dialogs.dialogs.length) {
+									var $lastDialog = Q.Dialogs.dialogs[Q.Dialogs.dialogs.length-1];
+									if ($lastDialog instanceof jQuery && !$lastDialog.hasClass(dialogClassName)) {
+										Q.Dialogs.pop();
+									}
+								}
+
 								Q.Dialogs.push({
 									title: title,
 									apply: true,
-									className: "Dialog_invite_photo_camera",
+									className: dialogClassName,
 									content:
 										'<div class="Streams_invite_photo_dialog">' +
 										'<p>'+ Q.getObject(['invite', 'dialog', 'photoInstruction'], text) +'</p>' +
@@ -1900,7 +1907,7 @@ Streams.invite = function (publisherId, streamName, options, callback) {
 											subpath: subpath,
 											saveSizeName: saveSizeName,
 											onFinish: function () {
-												Q.Dialogs.pop();
+												Q.Dialogs.close(dialog);
 											}
 										};
 										$('.Streams_invite_photo', dialog).plugin('Q/imagepicker', o);
