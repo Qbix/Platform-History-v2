d2a4be2874b54c4eaab08b63d2fe806d7541714a
Greg Magarshak <greg@qbix.com>
2023-05-28 22:18:40 -0400

    Implemented gating files in Q/uploads/Streams when served with NGinX
    Please see https://www.nginx.com/resources/wiki/start/topics/examples/xsendfile/

---
+++ MyApp/web/file.php
@@ -0,0 +1,16 @@
+<?php
+
+include('Q.inc.php');
+
+$uri = str_replace('/Q/uploads/Streams', '/internal', $_SERVER['REQUEST_URI']);
+
+$filename = Q::realPath(Q_Html::themedFilename(Q_Request::tail()));
+$can = Q_Utils::canReadFromPath($filename);
+if ($can) {
+	$mimeType = mime_content_type($filename);
+        header("Content-Type: $mimeType");
+        header("X-Accel-Redirect: $uri");
+} else {
+	Q_Text::setLanguageFromRequest();
+        throw new Users_Exception_NotAuthorized(compact('filename'));
+}
