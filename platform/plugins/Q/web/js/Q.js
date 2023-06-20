ecfa16ca59f31204915dcbaf08a914f4404f6219
Greg Magarshak <greg@qbix.com>
2023-06-21 00:21:34 +0400

    Made Q.js console.log.register work in more browsers

--- platform/plugins/Q/web/js/Q.js
+++ platform/plugins/Q/web/js/Q.js
@@ -620,49 +620,6 @@ Sp.decimalToHex = function () {
     return hex.join('');
 };
 
-Sp.consoleColor = function(color) {
-	var code;
-	if (color instanceof Array) {
-		code = '';
-		color.forEach(function (c) {
-			code += _consoleColors[c];
-		});
-	} else {
-		code = _consoleColors[color];
-	}
-	return code + this + _consoleColors.Reset;
-};
-
-var _consoleColors = {
-	Reset: "\x1b[0m",
-	Bright: "\x1b[1m",
-	Dim: "\x1b[2m",
-	Underscore: "\x1b[4m",
-	Blink: "\x1b[5m",
-	Reverse: "\x1b[7m",
-	Hidden: "\x1b[8m",
-
-	FgBlack: "\x1b[30m",
-	FgRed: "\x1b[31m",
-	FgGreen: "\x1b[32m",
-	FgYellow: "\x1b[33m",
-	FgBlue: "\x1b[34m",
-	FgMagenta: "\x1b[35m",
-	FgCyan: "\x1b[36m",
-	FgWhite: "\x1b[37m",
-	FgGray: "\x1b[90m",
-
-	BgBlack: "\x1b[40m",
-	BgRed: "\x1b[41m",
-	BgGreen: "\x1b[42m",
-	BgYellow: "\x1b[43m",
-	BgBlue: "\x1b[44m",
-	BgMagenta: "\x1b[45m",
-	BgCyan: "\x1b[46m",
-	BgWhite: "\x1b[47m",
-	BgGray: "\x1b[100m",
-};
-
 /**
  * @class Function
  * @description Q extended methods for Functions
@@ -14931,7 +14888,7 @@ if (!root.console) {
 root.console.log.register = function (name) {
 	return root.console.log[name] = function() {
 		var params = Array.prototype.slice.call(arguments);
-		params.unshift((name+':').consoleColor(['Bright', 'BgGray', 'FgWhite']));
+		params.unshift('%c' + name + ':', "background: gray; color: white; font-weight: bold;");
 		console.log.apply(console, params);
 	};
 };
