bf6c3a7798d58a07bcf918052969bb64c36e6a99
Greg Magarshak <greg@qbix.com>
2023-06-23 05:15:21 +0400

    related tool - observe stream once

--- platform/plugins/Streams/web/js/tools/related.js
+++ platform/plugins/Streams/web/js/tools/related.js
@@ -584,11 +584,6 @@ Q.Tool.define("Streams/related", function _Streams_related_tool (options) {
 		var dir = tool.state.isCategory ? 'To' : 'From';
 		var eventNames = ['onRelated'+dir, 'onUnrelated'+dir, 'onUpdatedRelate'+dir];
 		if (tool.state.realtime) {
-			// join user to category stream to allow get messages
-			if (Q.getObject("participant.state", result.stream) !== 'participating') {
-				result.stream.observe();
-			}
-
 			Q.each(eventNames, function (i, eventName) {
 				result.stream[eventName]().set(function (msg, fields) {
 					// TODO: REPLACE THIS WITH AN ANIMATED UPDATE BY LOOKING AT THE ARRAYS entering, exiting, updating
