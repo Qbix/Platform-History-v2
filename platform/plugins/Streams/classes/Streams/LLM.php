7fa80c0748c3303a22cce21e210cb20d4042c157
Greg Magarshak <greg@qbix.com>
2023-07-04 14:22:25 +0300

    Moved AI files to AI plugin

--- platform/plugins/Streams/classes/Streams/LLM.php
+++ platform/plugins/Streams/classes/Streams/LLM.php
@@ -1,11 +1,11 @@
 <?php
 
-interface Streams_LLM_Interface
+interface AI_LLM_Interface
 {
     
 }
 
-class Streams_LLM implements Streams_LLM_Interface
+class AI_LLM implements AI_LLM_Interface
 {
 
 }
