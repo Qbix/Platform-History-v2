9c60dd8d246a59bf8d50c2a4c45efd0ecb78fb28
Greg Magarshak <greg@qbix.com>
2023-07-09 21:05:00 +0400

    Streams/message extras slot will be filled when rendering other slots

--- platform/plugins/Streams/handlers/Streams/message/response.php
+++ platform/plugins/Streams/handlers/Streams/message/response.php
@@ -5,6 +5,9 @@ function Streams_message_response()
 	$slotNames = Q_Request::slotNames();
 	sort($slotNames);
 	foreach ($slotNames as $sn) {
+		if ($sn === 'extras') {
+			continue; // other slots will set it
+		}
 		if (Q_Response::getSlot($sn) === null) {
 			Q_Response::setSlot($sn, Q::event("Streams/message/response/$sn"));
 		}
