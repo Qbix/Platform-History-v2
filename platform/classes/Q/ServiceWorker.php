49e9cd457b4d9f6fe7cbf6611d5cb5157df48ae3
Greg Magarshak <greg@qbix.com>
2023-07-31 19:06:57 -0700

    Implemented Q/ServiceWorker.php

--- platform/classes/Q/ServiceWorker.php
+++ platform/classes/Q/ServiceWorker.php
@@ -12,8 +12,22 @@ class Q_ServiceWorker
     function inlineCode()
     {
         $config = Q_Config::get('Q', 'serviceWorkers', 'modules', array());
+        $js = array();
         foreach ($config as $plugin => $filename) {
-            
+            if (!file_exists($filename)) {
+                throw new Q_Exception_MissingFile(compact('filename'));
+            }
+            $mtime = filemtime($filename);
+            $preamble = <<<JS
+/**** Qbix: produced by Q_ServiceWorker::inlineCode()
+ * SOURCE: $filename
+ * TIME: $mtime
+ */
+
+JS;
+            $js[$plugin] = $preamble . file_get_contents($filename);
         }
+        $separator = "\n\n";
+        return implode($separator, $js);
     }
 }
