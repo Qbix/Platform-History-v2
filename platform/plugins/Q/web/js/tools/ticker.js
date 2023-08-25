e2113af3b76b2bfb22298c16d20722385e218746
Greg Magarshak <greg@qbix.com>
2023-08-25 03:24:16 -0400

    Modernized the ancient Q/ticker tool

--- platform/plugins/Q/web/js/tools/ticker.js
+++ platform/plugins/Q/web/js/tools/ticker.js
@@ -9,268 +9,75 @@
  * This renders a ticker that scrolls its items
  * @class Q ticker
  * @constructor
- * @param {Object} [options] This is an object of parameters for this function
- *  @param {Boolean} [options.vertical]
- *  @default true
- *  @param {Number} [options.speed]
- *  @default 1
- *  @param {Number} [options.pause_ms]
- *  @default 2000
- *  @param {Boolean} [options.scrollbars]
- *  @default true
- *  @param {Number} [options.scrollbars_pause_ms]
- *  @default 500
- *  @param {Number} [options.anim_ms]
- *  @default 100
+ * @param {Object} [this.state] This is an object of parameters for this function
+ *  @param {Boolean} [this.state.vertical=false]
+ *  @param {Number} [this.state.speed=1]
+ *  @param {Number} [this.state.pause=2000]
+ *  @param {Boolean} [this.state.scrollbars=true]
+ *  @param {Number} [this.state.scrollbarsPause=500]
+ *  @param {Number} [this.state.frameRate = 100]
  */
 
-Q.Tool.define("Q/ticker", function(options) {
-	// private variables:
-	var threshold = 5; // how many pixels until manual scrolling kicks in
+Q.Tool.define("Q/ticker", function() {
+	if (!('vertical' in this.state))
+		this.state.vertical = false;
 
-	if (!('vertical' in options))
-		options['vertical'] = true;
-
-	if (!('pause_ms_min' in options))
-		options['pause_ms_min'] = options['pause_ms'];
+	if (!('pauseMin' in this.state))
+		this.state['pauseMin'] = this.state['pause'];
 
 	// constructor
 	var me = this;
-	var $ticker = $('#'+this.prefix+'tool .Q_ticker');
-	var $container = $ticker.children();
-	var $children = $container.children();
-	if ($children.length && $children.get(0).tagName.toUpperCase() == 'TABLE') {
-		$children = $children.children();
+	var $te = $(this.element);
+	this.$children = $te.children();
+	if (this.$children.length && this.$children.get(0).tagName.toUpperCase() == 'TABLE') {
+		this.$children = this.$children.children();
 	}
-	if ($children.length && $children.get(0).tagName.toUpperCase() == 'TBODY') {
-		$children = $children.children();
-	}
-	if ($children.length && !options.vertical) {
-		$children = $children.children();
+	if (this.$children.length && this.$children.get(0).tagName.toUpperCase() == 'TBODY') {
+		this.$children = this.$children.children();
 	}
 	
-	Q.addStylesheet('{{Q}}/css/tools/ticker.css', { slotName: 'Q' });
+	Q.addStylesheet('{{Q}}/css/tools/ticker.css', { slotName: 'Q' })
 
-	this.onHitStart = function() {}; // override this to handle this event
-	this.onHitEnd = function() {}; // override this to handle this event
+	this.ease = ('ease' in this.state)
+			? Q.Animation.ease[this.state['ease']]
+			: Q.Animation.ease.smooth;
 
-	this.ready = function() {
-		if ($ticker.data('isTicker'))
-			return;
+	this.ready();
+},
 
-		$ticker.data('isTicker', true);
+{
+	vertical: false,
+	speed: 1,
+	pause: 2000,
+	scrollbars: true,
+	scrollbarsPause: 500,
+	frameRate: 100,
+	threshold: 5,
+	onHitStart: new Q.Event(),
+	onHitEnd: new Q.Event()
+},
 
-		if (!$ticker.hasClass('ticker'))
-			$ticker.addClass('ticker');
-
-		if (!$ticker.hasClass('vertical') && !$ticker.hasClass('horizontal'))
-			$ticker.addClass(options['vertical'] ? 'vertical' : 'horizontal');
-
-		me.setScrollbars(options['scrollbars']);
-
-		$container.css('visibility','hidden');
-
-		setTimeout(function(){
-			me.calculateDimensions();
-
-			if (options['speed'] < 0) {
-				if (options['vertical'])
-					$ticker.scrollTop($container.height());
-				else
-					$ticker.scrollLeft($container.width());
-			}
-
-			$container.css('visibility','visible');
-
-			me.anim_ms = options['anim_ms'] ? options['anim_ms'] : 100;
-
-			setInterval(me.autoScroll, me.anim_ms);
-		}, 1000);
-	};
-
-	this.setScrollbars = function(shouldShow) {
-		$ticker.toggleClass('scrollbars', shouldShow);
-	};
-
-	this.pause = function() {
-		me.scrollMode = 'paused';
-		me.msSincePaused = 0;
-		me.pause_ms = options['pause_ms_min'] + Math.random() * (options['pause_ms'] - options['pause_ms_min']);
-	};
-
-	this.resume = function(curScrollPos) {
-		me.scrollMode = 'auto';
-		me.frameIndex = 0;
-		me.startScrollPos = curScrollPos;
-		me.newScrollPos = me.getNextScrollPos(curScrollPos);
-	};
-
-	this.autoScroll = function () {
-		var curScrollPos = (options['vertical']) ? $ticker.scrollTop() : $ticker.scrollLeft();
-
-		// Raise scrolling events, if any
-		if (! ('lastScrollPos' in me) || me.lastScrollPos != curScrollPos)
-		{
-			me.raisedOnHitStart = false;
-			me.raisedOnHitEnd = false;
-		}
-
-		if (curScrollPos === 0)
-		{
-			if (!me.raisedOnHitStart)
-				me.onHitStart();
-
-			me.raisedOnHitStart = true;
-		}
-		else
-		{
-			if (options['vertical'])
-			{
-				if (curScrollPos == $ticker[0].scrollHeight - $ticker[0].clientHeight) //FIXME: check scrollHeight && clientHeight
-				{
-					if (!me.raisedOnHitEnd)
-						me.onHitEnd();
-					me.raisedOnHitEnd = true;
-				}
-			}
-			else
-			{
-				if (curScrollPos == $ticker[0].scrollWidth - $ticker[0].clientWidth) //FIXME: check scrollHeight && clientHeight
-				{
-					if (!me.raisedOnHitEnd)
-						me.onHitEnd();
-
-					me.raisedOnHitEnd = true;
-				}
-			}
-		}
-
-		// Handle the auto scrolling
-
-		if (! ('scrollMode' in me)) {
-			if ('initial_scroll_mode' in options) {
-				me.scrollMode = options.initial_scroll_mode;
-			} else {
-				me.scrollMode = 'auto';
-			}
-			if (me.scrollMode == 'auto') {
-				me.resume(curScrollPos);
-				me.newScrollPos = me.getFirstScrollPos(curScrollPos);
-			} else {
-				me.msSincePaused = 0;
-				me.pause_ms = options['pause_ms_min'] + Math.random() *
-						(options['pause_ms'] - options['pause_ms_min']);
-			}
-		}
-
-		if (speed === 0) {
-			return -1;
-		}
-
-		if (! ('frameIndex' in me)) {
-			me.frameIndex = 0;
-		}
-
-		if (! ('startScrollPos' in me)) {
-			me.startScrollPos = 0;
-		}
-
-		if (me.scrollMode != 'manual'
-				&& 'lastAutoScrollPos' in me
-				&& !isNaN(me.lastAutoScrollPos)
-				&& Math.abs(me.lastAutoScrollPos - curScrollPos) > threshold) {
-
-			// the scrollbar has started moving by some other means
-			// stop this function from executing, start waiting
-			// until it stops and options['scrollbars_pause_ms']
-			// milliseconds have elapsed since then.
-			me.scrollMode = 'manual';
-
-			// reset # of msec passed since manual scrolling
-			me.msSinceManual = 0;
-
-		} else if (me.scrollMode == 'manual') {
-
-			if ('lastScrollPos' in me
-					&& me.lastScrollPos != curScrollPos) {
-				// the scrollbar continues to move,
-				// reset # of msec to wait since manual scrolling
-				me.msSinceManual = 0;
-
-				// keep the scroll mode as 'manual'
-			} else {
-				// increment the # of msec passed since manual scrolling
-				me.msSinceManual += me.anim_ms;
-
-				if (options['scrollbars_pause_ms'] >= 0
-						&& me.msSinceManual > options['scrollbars_pause_ms']) {
-					me.resume(curScrollPos);
-				}
-			}
-
-		} else if (me.scrollMode == 'paused') {
-
-			// increment the # of msec passed since manual scrolling
-			me.msSincePaused += me.anim_ms;
-
-			if (me.pause_ms >= 0 && me.msSincePaused > me.pause_ms) {
-				me.resume(curScrollPos);
-			}
-		}
-
-		if (me.scrollMode == 'auto') {
-			var speed = options['speed'];
-			var fraction = me.frameIndex * (me.anim_ms / 1000 * Math.abs(speed));
-
-			var nextScrollPos;
-			if (fraction >= 1) {
-				me.pause();
-			}
-
-			nextScrollPos = me.startScrollPos +
-					(me.newScrollPos - me.startScrollPos) * me.ease(fraction);
-
-			if (options['vertical']) {
-				$ticker.scrollTop(nextScrollPos);
-				me.lastAutoScrollPos = $ticker.scrollTop();
-			} else {
-				$ticker.scrollLeft(nextScrollPos);
-				me.lastAutoScrollPos = $ticker.scrollLeft();
-			}
-
-			++me.frameIndex;
-		} else {
-			me.lastScrollPos = curScrollPos;
-		}
-
-		// This makes sure the scrolling events don't happen twice
-		if (! ('lastScrollPos' in me) || me.lastScrollPos != curScrollPos) {
-			me.lastScrollPos = curScrollPos;
-		}
-
-		return 0;
-	};
-
-	this.getFirstScrollPos = function(curScrollPos) {
-		var ticker = document.getElementById(options['element_id']);
-		var speed = options['speed'];
+{
+	getFirstScrollPos: function(curScrollPos) {
+		var ticker = document.getElementById(this.state['elementId']);
+		var speed = this.state['speed'];
 		var i;
 
-		me.calculateDimensions();
+		this.calculateDimensions();
 
 		if (speed >= 0) {
 			return 0;
 		}
 
-		if (options['vertical']) {
+		if (this.state.vertical) {
 			var bottom = 0;
 			var top = 0;
-			for (i = 0; i < me.heights.length; ++i) {
-				bottom += me.heights[i];
+			for (i = 0; i < this.heights.length; ++i) {
+				bottom += this.heights[i];
 			}
-			for (i = 0; i < me.heights.length; ++i) {
-				top += me.heights[i];
-				if (top + me.heights[i] > bottom - $ticker.innerHeight()) {
+			for (i = 0; i < this.heights.length; ++i) {
+				top += this.heights[i];
+				if (top + this.heights[i] > bottom - $(this.element).innerHeight()) {
 					return top;
 				}
 			}
@@ -278,85 +85,277 @@ Q.Tool.define("Q/ticker", function(options) {
 		} else {
 			var right = 0;
 			var left = 0;
-			for (i = 0; i < me.widths.length; ++i) {
-				right += me.widths[i];
+			for (i = 0; i < this.widths.length; ++i) {
+				right += this.widths[i];
 			}
-			for (i = 0; i < me.widths.length; ++i) {
-				left += me.widths[i];
-				if (left + me.widths[i] > right - $ticker.innerWidth()) {
+			for (i = 0; i < this.widths.length; ++i) {
+				left += this.widths[i];
+				if (left + this.widths[i] > right - $(this.element).innerWidth()) {
 					return left;
 				}
 			}
 			return left;
 		}
 		return -2;
-	};
+	},
 
-	this.getNextScrollPos = function(curScrollPos) {
-		var speed = options['speed'];
+	getNextScrollPos: function(curScrollPos) {
+		var speed = this.state['speed'];
 		var i;
 
-		me.calculateDimensions();
+		this.calculateDimensions();
 
 		if (speed === 0) {
 			return -1;
 		}
 
-		if (options['vertical']) {
+		if (this.state.vertical) {
 			var top = 0;
-			for (i = 0; i < me.heights.length; ++i) {
+			for (i = 0; i < this.heights.length; ++i) {
 				if (speed > 0) {
-					if (top + me.heights[i] > curScrollPos)
-						return top + me.heights[i];
+					if (top + this.heights[i] > curScrollPos + 1)
+						return top + this.heights[i];
 				} else {
-					if (top + me.heights[i] >= curScrollPos - 1)
+					if (top + this.heights[i] >= curScrollPos - 1)
 						return top;
 				}
-				top += me.heights[i];
+				top += this.heights[i];
 			}
 		} else {
 			var left = 0;
-			for (i = 0; i < me.widths.length; ++i) {
+			for (i = 0; i < this.widths.length; ++i) {
 				if (speed > 0) {
-					if (left + me.widths[i] > curScrollPos) {
-						return left + me.widths[i];
+					if (left + this.widths[i] > curScrollPos + 1) {
+						return left + this.widths[i];
 					}
 				} else {
-					if (left + me.widths[i] >= curScrollPos - 1) {
+					if (left + this.widths[i] >= curScrollPos - 1) {
 						return left;
 					}
 				}
-				left += me.widths[i];
+				left += this.widths[i];
 			}
 		}
 		return -2;
-	};
+	},
 
-	this.ease = ('ease' in options)
-			? Q.Animation.ease[options['ease']]
-			: Q.Animation.ease.smooth;
+	calculateDimensions: function () {
+		this.widths = [];
+		this.heights = [];
+		var tool = this;
 
-	this.calculateDimensions = function () {
-		me.widths = [];
-		me.heights = [];
-
-		$children.each(function(index){
+		this.$children.each(function(index){
 			var $el = $(this);
-			me.widths.push($el.outerWidth());
-			me.heights.push($el.outerHeight());
+			tool.widths.push($el.outerWidth(true));
+			tool.heights.push($el.outerHeight() + parseInt($el.css('margin-top')));
 		});
-	};
+	},
 
-	this.ready();
-},
+	ready: function() {
+		var $container = $(this.element);
+		if ($container.data('isTicker'))
+			return;
 
-{
-	vertical: true,
-	speed: 1,
-	pause_ms: 2000,
-	scrollbars: true,
-	scrollbars_pause_ms: 500,
-	anim_ms: 100
+		$container.data('isTicker', true);
+
+		if (!$container.hasClass('Q_ticker'))
+			$container.addClass('Q_ticker');
+
+		if (!$container.hasClass('Q_vertical') && !$container.hasClass('Q_horizontal'))
+			$container.addClass(this.state['Q_vertical'] ? 'vertical' : 'horizontal');
+
+		this.setScrollbars(this.state['scrollbars']);
+
+		$container.css('visibility','hidden');
+
+		var tool = this;
+		setTimeout(function(){
+			tool.calculateDimensions();
+
+			if (tool.state['speed'] < 0) {
+				if (tool.state.vertical)
+					$container.scrollTop($container.height());
+				else
+					$container.scrollLeft($container.width());
+			}
+
+			$container.css('visibility','visible');
+
+			tool.frameRate = tool.state['frameRate'] || 100;
+
+			setInterval(tool.autoScroll.bind(tool), tool.frameRate);
+		}, 1000);
+	},
+
+	setScrollbars: function(shouldShow) {
+		$(this.element).toggleClass('Q_scrollbars', shouldShow);
+	},
+
+	pause: function() {
+		this.scrollMode = 'paused';
+		this.msSincePaused = 0;
+		this.pauseDuration = this.state['pauseMin'] + Math.random() * (this.state['pause'] - this.state['pauseMin']);
+	},
+
+	resume: function(curScrollPos) {
+		this.scrollMode = 'auto';
+		this.frameIndex = 0;
+		this.startScrollPos = curScrollPos;
+		this.newScrollPos = this.getNextScrollPos(curScrollPos);
+	},
+
+	autoScroll: function () {
+		var $container = $(this.element);
+		var curScrollPos = (this.state.vertical) ? $(this.element).scrollTop() : $(this.element).scrollLeft();
+
+		// Raise scrolling events, if any
+		if (! ('lastScrollPos' in this) || this.lastScrollPos != curScrollPos)
+		{
+			this.raisedOnHitStart = false;
+			this.raisedOnHitEnd = false;
+		}
+
+		if (curScrollPos === 0)
+		{
+			if (!this.raisedOnHitStart)
+				Q.handle(this.state.onHitStart);
+
+			this.raisedOnHitStart = true;
+		}
+		else
+		{
+			if (this.state.vertical)
+			{
+				if (curScrollPos == $container[0].scrollHeight - $container[0].clientHeight) //FIXME: check scrollHeight && clientHeight
+				{
+					if (!this.raisedOnHitEnd)
+						Q.handle(this.state.onHitEnd);
+					this.raisedOnHitEnd = true;
+				}
+			}
+			else
+			{
+				if (curScrollPos == $container[0].scrollWidth - $container[0].clientWidth) //FIXME: check scrollHeight && clientHeight
+				{
+					if (!this.raisedOnHitEnd)
+						Q.handle(this.state.onHitEnd);
+
+					this.raisedOnHitEnd = true;
+				}
+			}
+		}
+
+		// Handle the auto scrolling
+
+		if (! ('scrollMode' in this)) {
+			if ('initialScrollMode' in this.state) {
+				this.scrollMode = this.state.initialScrollMode;
+			} else {
+				this.scrollMode = 'auto';
+			}
+			if (this.scrollMode == 'auto') {
+				this.resume(curScrollPos);
+				this.newScrollPos = this.getFirstScrollPos(curScrollPos);
+			} else {
+				this.msSincePaused = 0;
+				this.pauseDuration = this.state['pauseMin'] + Math.random() *
+						(this.state['pause'] - this.state['pauseMin']);
+			}
+		}
+
+		if (speed === 0) {
+			return -1;
+		}
+
+		if (! ('frameIndex' in this)) {
+			this.frameIndex = 0;
+		}
+
+		if (! ('startScrollPos' in this)) {
+			this.startScrollPos = 0;
+		}
+
+		if (this.scrollMode != 'manual'
+		&& 'lastAutoScrollPos' in this
+		&& !isNaN(this.lastAutoScrollPos)
+		&& Math.abs(this.lastAutoScrollPos - curScrollPos) > this.state.threshold) {
+
+			// the scrollbar has started moving by some other means
+			// stop this function from executing, start waiting
+			// until it stops and this.state['scrollbarsPause']
+			// milliseconds have elapsed since then.
+			this.scrollMode = 'manual';
+
+			// reset # of msec passed since manual scrolling
+			this.msSinceManual = 0;
+
+		} else if (this.scrollMode == 'manual') {
+
+			if ('lastScrollPos' in this
+			&& this.lastScrollPos != curScrollPos) {
+				// the scrollbar continues to move,
+				// reset # of msec to wait since manual scrolling
+				this.msSinceManual = 0;
+
+				// keep the scroll mode as 'manual'
+			} else {
+				// increment the # of msec passed since manual scrolling
+				this.msSinceManual += this.frameRate;
+
+				if (this.state['scrollbarsPause'] >= 0
+				&& this.msSinceManual > this.state['scrollbarsPause']) {
+					this.resume(curScrollPos);
+				}
+			}
+
+		} else if (this.scrollMode == 'paused') {
+
+			// increment the # of msec passed since manual scrolling
+			this.msSincePaused += this.frameRate;
+
+			if (this.pauseDuration >= 0 && this.msSincePaused > this.pauseDuration) {
+				this.resume(curScrollPos);
+			}
+		}
+
+		if (this.scrollMode == 'auto') {
+			var speed = this.state['speed'];
+			var fraction = this.frameIndex * (this.frameRate / 1000 * Math.abs(speed));
+
+			var nextScrollPos;
+			if (fraction >= 1) {
+				this.pause();
+			}
+
+			nextScrollPos = this.startScrollPos +
+					(this.newScrollPos - this.startScrollPos) * this.ease(fraction);
+
+			if ($container.hasClass('Q_noScrollbar')) {
+				setTimeout(function () {
+					$container.removeClass('Q_noScrollbar')
+				}, this.frameRate * 10);
+			}
+			$container.addClass('Q_noScrollbar');
+			if (this.state.vertical) {
+				$container.scrollTop(nextScrollPos);
+				this.lastAutoScrollPos = $container.scrollTop();
+			} else {
+				$container.scrollLeft(nextScrollPos);
+				this.lastAutoScrollPos = $container.scrollLeft();
+			}
+
+			++this.frameIndex;
+		} else {
+			this.lastScrollPos = curScrollPos;
+		}
+
+		// This makes sure the scrolling events don't happen twice
+		if (! ('lastScrollPos' in this) || this.lastScrollPos != curScrollPos) {
+			this.lastScrollPos = curScrollPos;
+		}
+
+		return 0;
+	}
 }
 
 );
