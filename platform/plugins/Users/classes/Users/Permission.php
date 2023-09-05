42d9b5c971b0968a88c354c5dc6d238a7a64f281
EGreg <greg@qbix.com>
2023-09-04 21:18:37 -0400

    Added extras methods to Users_Permission

--- platform/plugins/Users/classes/Users/Permission.php
+++ platform/plugins/Users/classes/Users/Permission.php
@@ -24,11 +24,71 @@ class Users_Permission extends Base_Users_Permission
 		// INSERT YOUR CODE HERE
 		// e.g. $this->hasMany(...) and stuff like that.
 	}
-
-	/*
-	 * Add any Users_Permission methods here, whether public or not
+	
+	/**
+	 * @method getAllExtras
+	 * @return {array} The array of all extras set in the stream
 	 */
-	 
+	function getAllExtras()
+	{
+		return empty($this->extra)
+			? array()
+			: json_decode($this->extra, true);
+	}
+
+	/**
+	 * @method getExtra
+	 * @param {string} $extraName The name of the extra to get
+	 * @param {mixed} $default The value to return if the extra is missing
+	 * @return {mixed} The value of the extra, or the default value, or null
+	 */
+	function getExtra($extraName, $default = null)
+	{
+		$attr = $this->getAllExtras();
+		return isset($attr[$extraName]) ? $attr[$extraName] : $default;
+	}
+
+	/**
+	 * @method setExtra
+	 * @param {string} $extraName The name of the extra to set,
+	 *  or an array of $extraName => $extraValue pairs
+	 * @param {mixed} $value The value to set the extra to
+	 * @return Streams_Invite
+	 */
+	function setExtra($extraName, $value = null)
+	{
+		$attr = $this->getAllExtras();
+		if (is_array($extraName)) {
+			foreach ($extraName as $k => $v) {
+				$attr[$k] = $v;
+			}
+		} else {
+			$attr[$extraName] = $value;
+		}
+		$this->extra = Q::json_encode($attr);
+
+		return $this;
+	}
+
+	/**
+	 * @method clearExtra
+	 * @param {string} $extraName The name of the extra to remove
+	 */
+	function clearExtra($extraName)
+	{
+		$attr = $this->getAllExtras();
+		unset($attr[$extraName]);
+		$this->extra = Q::json_encode($attr);
+	}
+
+	/**
+	 * @method clearAllExtras
+	 */
+	function clearAllExtras()
+	{
+		$this->extra = '{}';
+	}
+
 	/**
 	 * Implements the __set_state method, so it can work with
 	 * with var_export and be re-imported successfully.
