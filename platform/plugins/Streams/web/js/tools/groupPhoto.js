7b561b3dcbff87868f44ee5372bab709bc962dd1
Andrey Tepaykin <andrey@qbix.com>
2023-09-14 21:29:28 +0300

    Updated Streams/groupPhoto tool.

--- platform/plugins/Streams/web/js/tools/groupPhoto.js
+++ platform/plugins/Streams/web/js/tools/groupPhoto.js
@@ -39,63 +39,83 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 			var tool = this;
 			var state = this.state;
 
-			Q.Dialogs.push({
-				title: state.title,
-				className: "StreamsGroupPhoto",
-				template: {
-					name: "Streams/groupPhoto",
-					fields: {
-						photoUrl: state.photoUrl
+			Q.Template.render("Streams/groupPhoto", {
+				photoUrl: state.photoUrl
+			}, function (err, html) {
+				if (err) {
+					return;
+				}
+
+				Q.replace(tool.element, html);
+
+				var $input = $("img.Streams_groupPhoto", tool.element);
+				var input = $input[0];
+				var _getCurrentFaceDetectionNet = function () {
+					switch (state.faceDetector) {
+						case 'ssd_mobilenetv1':
+							return faceapi.nets.ssdMobilenetv1
+						case 'tiny_face_detector':
+							return faceapi.nets.tinyFaceDetector
+						default:
+							throw new Q.Error("unrecognised faceDetector");
 					}
-				},
-				onActivate: function ($dialog) {
-					var input = $("img.Streams_groupPhoto", $dialog)[0];
-					var _getCurrentFaceDetectionNet = function () {
-						switch (state.faceDetector) {
-							case 'ssd_mobilenetv1':
-								return faceapi.nets.ssdMobilenetv1
-							case 'tiny_face_detector':
-								return faceapi.nets.tinyFaceDetector
-							default:
-								throw new Q.Error("unrecognised faceDetector");
-						}
-					};
-					var _getFaceDetectorOptions = function () {
-						switch (state.faceDetector) {
-							case 'ssd_mobilenetv1':
-								return new faceapi.SsdMobilenetv1Options({ minConfidence: state.scoreThreshold })
-							case 'tiny_face_detector':
-								return new faceapi.TinyFaceDetectorOptions({ inputSize: state.inputSize, scoreThreshold: state.scoreThreshold })
-							default:
-								throw new Q.Error("unrecognised faceDetector");
-						}
-					};
-					var _isFaceDetectionModelLoaded = function () {
-						return !!_getCurrentFaceDetectionNet().params
-					};
-					var _run = function () {
-						$(".Streams_groupPhoto_loader", $dialog).hide()
-						var canvas = $("canvas", $dialog)[0];
-						faceapi.detectAllFaces(input, _getFaceDetectorOptions()).then(function (results) {
-							faceapi.matchDimensions(canvas, input);
-							faceapi.draw.drawDetections(canvas, faceapi.resizeResults(results, input), {withScore: false});
+				};
+				var _getFaceDetectorOptions = function () {
+					switch (state.faceDetector) {
+						case 'ssd_mobilenetv1':
+							return new faceapi.SsdMobilenetv1Options({ minConfidence: state.scoreThreshold })
+						case 'tiny_face_detector':
+							return new faceapi.TinyFaceDetectorOptions({ inputSize: state.inputSize, scoreThreshold: state.scoreThreshold })
+						default:
+							throw new Q.Error("unrecognised faceDetector");
+					}
+				};
+				var _isFaceDetectionModelLoaded = function () {
+					return !!_getCurrentFaceDetectionNet().params
+				};
+				var _run = function () {
+					$(".Streams_groupPhoto_loader", tool.element).hide()
+					var $canvas = $("canvas", tool.element);
+					$canvas.css({
+						left: $input.position().left,
+						top: $input.position().top
+					});
+					var canvas = $canvas[0];
+					faceapi.detectAllFaces(input, _getFaceDetectorOptions()).then(function (results) {
+						faceapi.matchDimensions(canvas, input);
+						var resizedResults = faceapi.resizeResults(results, input);
+						faceapi.draw.drawDetections(canvas, resizedResults, {withScore: false});
+						$canvas.on(Q.Pointer.fastclick, function (event) {
+							resizedResults.forEach(function (result, i) {
+								if (
+									event.offsetX < result._box.left || event.offsetX > result._box.left + result._box.width ||
+									event.offsetY < result._box.top || event.offsetY > result._box.top + result._box.height
+								) {
+									return;
+								}
+
+								faceapi.extractFaces(input, [results[i]]).then(function (canvases) {
+									canvases.forEach(cnv => {
+										var outputImage = $("<img>").insertAfter(input).prop("src", cnv.toDataURL());
+									})
+								});
+							});
 						});
-					};
-					var _inputLoaded = function () {
-						if (_isFaceDetectionModelLoaded()) {
-							_run();
-						} else {
-							$(".Streams_groupPhoto_loader", $dialog).show()
-							setTimeout(() => _getCurrentFaceDetectionNet().load(Q.url('{{Streams}}/js/face-api/weights/')).then(_run), 500);
-
-						}
-					};
-
-					if (input.complete) {
-						_inputLoaded()
+					});
+				};
+				var _inputLoaded = function () {
+					if (_isFaceDetectionModelLoaded()) {
+						setTimeout(_run, 500);
 					} else {
-						input.addEventListener('load', _inputLoaded);
+						$(".Streams_groupPhoto_loader", tool.element).show();
+						setTimeout(() => _getCurrentFaceDetectionNet().load(Q.url('{{Streams}}/js/face-api/weights/')).then(_run), 500);
 					}
+				};
+
+				if (input.complete) {
+					_inputLoaded()
+				} else {
+					input.addEventListener('load', _inputLoaded, {once: true});
 				}
 			});
 		}
@@ -104,7 +124,7 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 
 Q.Template.set("Streams/groupPhoto", `
 	<img alt="group photo" src="{{photoUrl}}" class="Streams_groupPhoto" />
-	<canvas />
+	<canvas></canvas>
 	<div class="Streams_groupPhoto_loader"></div>
 `);
 
