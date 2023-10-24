d5c4a32299070f1458f1e6b92d2af2bf9f233e0a
Greg Magarshak <greg@qbix.com>
2023-10-24 16:38:31 -0400

    Made Users_Label::can() throw exception if communityId isn't a community

--- platform/plugins/Users/classes/Users/Label.php
+++ platform/plugins/Users/classes/Users/Label.php
@@ -338,6 +338,9 @@ class Users_Label extends Base_Users_Label
 			}
 			$userId = $user->id;
 		}
+		if (!Users::isCommunityId($communityId)) {
+			throw new Users_Exception_NoSuchUser();
+		}
 		$userCommunityRoles = Users::roles($communityId, null, array(), $userId);
         $communityRoles = self::ofCommunity($communityId);
 		$communityLabels = Users_Label::fetch($communityId, "", array("skipAccess" => true));
