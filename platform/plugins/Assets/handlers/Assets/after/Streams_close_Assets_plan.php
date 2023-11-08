db89ded05ed75b0ad7f4ae28d905299431c93ed0
Andrey Tepaykin <andrey@qbix.com>
2023-11-08 16:37:51 +0300

    Created Streams/close hook for Assets/plan stream type. Remove label from Users_Label and remove contacts from Users_contact.

--- platform/plugins/Assets/handlers/Assets/after/Streams_close_Assets_plan.php
+++ platform/plugins/Assets/handlers/Assets/after/Streams_close_Assets_plan.php
@@ -3,5 +3,9 @@
 function Assets_after_Streams_close_Assets_plan ($params) {
 	$stream = $params['stream'];
 
-
+	Users_Label::removeLabel($stream->name, $stream->publisherId, null, true);
+	Users_Contact::delete()->where(array(
+		"userId" => $stream->publisherId,
+		"label" => $stream->name
+	))->execute();
 }
