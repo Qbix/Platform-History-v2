1bdf034f2c87db75a7424fd6499b73cba59dae2b
Andrey Tepaykin <andrey@qbix.com>
2023-12-13 20:09:18 +0300

    Updated Streams/groupPhoto tool. Load Streams/image/invite/{token} group photo image stream and imply that predictions already saved in attributes.

--- platform/plugins/Streams/web/js/tools/groupPhoto.js
+++ platform/plugins/Streams/web/js/tools/groupPhoto.js
@@ -10,6 +10,8 @@
  * @constructor
  * @param {array} options
  * @param {string} options.photoUrl
+ * @param {string} options.publisherId - group photo stream publisher id
+ * @param {string} options.streamName - group photo stream name (Streams/image/invite/{invitation token})
  * @param {string} options.title - dialog title
  * @param {float} options.scoreThreshold
  * @param {string} options.faceDetector - neural network used to detect faces. Can be 'blazeface', 'ssd_mobilenetv1', 'tiny_face_detector'
@@ -25,17 +27,22 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 		if (!state.photoUrl || !Q.url(state.photoUrl).matchTypes('url').length) {
 			return console.warn("invalid photo url");
 		}
+		if (!state.publisherId) {
+			return console.warn("group photo stream publisherId empty");
+		}
+		if (!state.streamName) {
+			return console.warn("group photo stream name empty");
+		}
 
 		tool.refresh();
 	},
 
 	{
 		photoUrl: null,
+		publisherId: null,
+		streamName: null,
 		title: "Group photo",
-		scoreThreshold: 0.2,
-		rectCoefficient: 1.5,
-		inputSize: 512,
-		faceDetector: 'ssd_mobilenetv1',
+		rectCoefficient: 0.2,
 		rectangle: {
 			external: {
 				color: "rgba(0,0,0,1)",
@@ -71,7 +78,29 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 				var $input = $("img.Streams_groupPhoto", tool.element);
 				var input = $input[0];
 
-				var _inputLoaded = function () {
+				var pipe = new Q.Pipe(["image", "stream"], function (params) {
+					tool.hideLoader();
+					if (params.stream[0]) {
+						return;
+					}
+					var stream = params.stream[1];
+					var predictions = stream.getAttribute("predictions");
+
+					if (Q.isEmpty(predictions)) {
+						return console.warn("predictions empty");
+					}
+
+					var resizedRatio = input.naturalWidth/input.width;
+					predictions = predictions.map(function (prediction) {
+						return {
+							topLeft: prediction.topLeft.map(function (x, i) {
+								return (x - (prediction.bottomRight[i] - x)*state.rectCoefficient)/resizedRatio;
+							}),
+							bottomRight: prediction.bottomRight.map(function (x, i) {
+								return (x + (x - prediction.topLeft[i])*state.rectCoefficient)/resizedRatio;
+							})
+						};
+					});
 					tool.$canvas = $("canvas", tool.element);
 					tool.$canvas.css({
 						left: $input.position().left,
@@ -82,69 +111,51 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 					tool.canvas.height = input.height;
 					tool.canvasClientRect = tool.canvas.getBoundingClientRect();
 					var ctx = tool.canvas.getContext("2d");
-					//ctx.drawImage(input, 0, 0, input.width, input.height);
+					predictions.forEach((prediction) => {
+						prediction.width = prediction.bottomRight[0] - prediction.topLeft[0];
+						prediction.height = prediction.bottomRight[1] - prediction.topLeft[1];
+						["external", "internal"].forEach((param) => {
+							// draw the rectangle enclosing the face
+							ctx.lineWidth = state.rectangle[param].width;
+							ctx.strokeStyle = state.rectangle[param].color;
+							var k = param === 'external' ? state.rectangle.internal.width : 0;
+							ctx.strokeRect(
+								prediction.topLeft[0] - k,
+								prediction.topLeft[1] + k,
+								prediction.width + k,
+								prediction.height - k
+							);
+						})
+					});
 
-					tool.startTime = Date.now();
-					if (state.faceDetector === "blazeface") {
-						Q.ensure('Q.Users.Faces', function () {
-							tool.usersFaces = new Q.Users.Faces();
-							tool.usersFaces.detect(input, function (predictions) {
-								console.log("Time passed: " + (Date.now() - tool.startTime)/1000)
-								tool.hideLoader()
+					tool.$canvas.on(Q.Pointer.fastclick, function (event) {
+						predictions.forEach(function (prediction, i) {
+							event.offsetX = event.offsetX || Q.Pointer.getX(event) - tool.canvasClientRect.x;
+							event.offsetY = event.offsetY || Q.Pointer.getY(event) - tool.canvasClientRect.y;
+							if (
+								event.offsetX < prediction.topLeft[0] || event.offsetX > prediction.topLeft[0] + prediction.width ||
+								event.offsetY < prediction.topLeft[1] || event.offsetY > prediction.topLeft[1] + prediction.height
+							) {
+								return;
+							}
 
-								if (!predictions.length) {
-									return;
-								}
-
-								predictions.forEach((prediction) => {
-									prediction.width = prediction.bottomRight[0] - prediction.topLeft[0];
-									prediction.height = prediction.bottomRight[1] - prediction.topLeft[1];
-									["external", "internal"].forEach((param) => {
-										// draw the rectangle enclosing the face
-										ctx.lineWidth = state.rectangle[param].width;
-										ctx.strokeStyle = state.rectangle[param].color;
-										var k = param === 'external' ? state.rectangle.internal.width : 0;
-										ctx.strokeRect(
-											prediction.topLeft[0] - k,
-											prediction.topLeft[1] + k,
-											prediction.width + k,
-											prediction.height - k
-										);
-									})
-								});
-
-								tool.$canvas.on(Q.Pointer.fastclick, function (event) {
-									predictions.forEach(function (prediction, i) {
-										event.offsetX = event.offsetX || Q.Pointer.getX(event) - tool.canvasClientRect.x;
-										event.offsetY = event.offsetY || Q.Pointer.getY(event) - tool.canvasClientRect.y;
-										if (
-											event.offsetX < prediction.topLeft[0] || event.offsetX > prediction.topLeft[0] + prediction.width ||
-											event.offsetY < prediction.topLeft[1] || event.offsetY > prediction.topLeft[1] + prediction.height
-										) {
-											return;
-										}
-
-										var canvas = document.createElement("canvas");
-										var resizedRatio = input.naturalWidth/input.width;
-										canvas.width = prediction.width * resizedRatio;
-										canvas.height = prediction.height * resizedRatio;
-										var context = canvas.getContext("2d");
-										context.drawImage(input, prediction.topLeft[0] * resizedRatio, prediction.topLeft[1] * resizedRatio, canvas.width, canvas.width, 0, 0, canvas.width, canvas.width);
-										Q.handle(state.onChoose, tool, [canvas.toDataURL()]);
-									});
-								});
-							});
+							var canvas = document.createElement("canvas");
+							canvas.width = prediction.width * resizedRatio;
+							canvas.height = prediction.height * resizedRatio;
+							var context = canvas.getContext("2d");
+							context.drawImage(input, prediction.topLeft[0] * resizedRatio, prediction.topLeft[1] * resizedRatio, canvas.width, canvas.height, 0, 0, canvas.width, canvas.height);
+							//tool.element.append(canvas);
+							Q.handle(state.onChoose, tool, [canvas.toDataURL()]);
 						});
-					} else {
-						tool.faceApi($input);
-					}
-				};
+					});
+				});
 
 				if (input.complete) {
-					_inputLoaded()
+					pipe.fill("image")();
 				} else {
-					input.addEventListener('load', _inputLoaded, {once: true});
+					input.addEventListener('load', pipe.fill("image"), {once: true});
 				}
+				Q.Streams.get(state.publisherId, state.streamName, pipe.fill("stream"));
 			});
 		},
 		showLoader: function () {
@@ -152,96 +163,6 @@ Q.Tool.define("Streams/groupPhoto", function (options) {
 		},
 		hideLoader: function () {
 			$(".Streams_groupPhoto_loader", this.element).hide();
-		},
-		faceApi: function ($input) {
-			var tool = this;
-			var state = this.state;
-			var input = $input[0];
-
-			var _getCurrentFaceDetectionNet = function () {
-				switch (state.faceDetector) {
-					case 'ssd_mobilenetv1':
-						return faceapi.nets.ssdMobilenetv1
-					case 'tiny_face_detector':
-						return faceapi.nets.tinyFaceDetector
-					default:
-						throw new Q.Error("unrecognised faceDetector");
-				}
-			};
-			var _getFaceDetectorOptions = function () {
-				switch (state.faceDetector) {
-					case 'ssd_mobilenetv1':
-						return new faceapi.SsdMobilenetv1Options({ minConfidence: state.scoreThreshold })
-					case 'tiny_face_detector':
-						return new faceapi.TinyFaceDetectorOptions({ inputSize: state.inputSize, scoreThreshold: state.scoreThreshold })
-					default:
-						throw new Q.Error("unrecognised faceDetector");
-				}
-			};
-			var _isFaceDetectionModelLoaded = function () {
-				return !!_getCurrentFaceDetectionNet().params
-			};
-			var _run = function () {
-				tool.hideLoader()
-				faceapi.detectAllFaces(input, _getFaceDetectorOptions()).then(function (results) {
-					console.log("Time passed: " + (Date.now() - tool.startTime)/1000)
-					faceapi.matchDimensions(tool.canvas, input);
-					// increase detected rectangles to state.rectCoefficient
-					results = results.map(function (result) {
-						result._box._x -= (result._box._width*state.rectCoefficient - result._box._width)/2;
-						result._box._y -= (result._box._height*state.rectCoefficient - result._box._height)/2;
-						result._box._width *= state.rectCoefficient;
-						result._box._height *= state.rectCoefficient;
-						return result;
-					});
-					var drawDetectionsOptions = {
-						withScore: false,
-						boxColor: state.rectangle.internal.color,
-						lineWidth: state.rectangle.internal.width
-					};
-					var resizedResults = faceapi.resizeResults(results, input);
-					faceapi.draw.drawDetections(tool.canvas, resizedResults, drawDetectionsOptions);
-
-					// draw black box around white
-					drawDetectionsOptions.boxColor = state.rectangle.external.color;
-					faceapi.draw.drawDetections(tool.canvas, faceapi.resizeResults(resizedResults.map(function (result) {
-						result._box._x -= drawDetectionsOptions.lineWidth;
-						result._box._y -= drawDetectionsOptions.lineWidth;
-						result._box._width += 2*drawDetectionsOptions.lineWidth;
-						result._box._height += 2*drawDetectionsOptions.lineWidth;
-						return result;
-					}), input), drawDetectionsOptions);
-
-					tool.$canvas.on(Q.Pointer.fastclick, function (event) {
-						resizedResults.forEach(function (result, i) {
-							event.offsetX = event.offsetX || Q.Pointer.getX(event) - tool.canvasClientRect.x;
-							event.offsetY = event.offsetY || Q.Pointer.getY(event) - tool.canvasClientRect.y;
-							if (
-								event.offsetX < result._box.left || event.offsetX > result._box.left + result._box.width ||
-								event.offsetY < result._box.top || event.offsetY > result._box.top + result._box.height
-							) {
-								return;
-							}
-
-							faceapi.extractFaces(input, [results[i]]).then(function (canvases) {
-								canvases.forEach(cnv => {
-									//var outputImage = $("<img>").insertAfter(input).prop("src", cnv.toDataURL());
-									Q.handle(state.onChoose, tool, [cnv.toDataURL()]);
-								})
-							});
-						});
-					});
-				});
-			};
-
-			Q.addScript(['{{Streams}}/js/face-api/dist/face-api.js'], function () {
-				if (_isFaceDetectionModelLoaded()) {
-					_run();
-				} else {
-					tool.showLoader();
-					_getCurrentFaceDetectionNet().load(Q.url('{{Streams}}/js/face-api/weights/')).then(_run);
-				}
-			});
 		}
 	}
 );
