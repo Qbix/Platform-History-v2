5bf925bfb5a14c039edd6891c1f44973e6c6a6a0
Andrey Tepaykin <andrey@qbix.com>
2023-12-27 15:44:17 +0300

    Fixed Streams/video/preview tool. For vimeo provider save attribute "available" if true to refuse request vimeo everytime.

--- platform/plugins/Streams/handlers/Streams/vimeo/response/info.php
+++ platform/plugins/Streams/handlers/Streams/vimeo/response/info.php
@@ -1,8 +1,17 @@
 <?php
-function Streams_vimeo_response_info () {
-	$videoId = $_REQUEST['videoId'];
+function Streams_vimeo_response_info ($params=[]) {
+	$request = Q::take(array_merge($_REQUEST, $params), ['videoId', 'publisherId', 'streamName']);
+	$videoId = $request['videoId'];
+	$publisherId = $request['publisherId'];
+	$streamName = $request['streamName'];
+
 	$vimeo = new Q_Video_Vimeo();
 	$info = $vimeo->getInfo($videoId);
 
+	if ($info['status'] == 'available' && $publisherId && $streamName) {
+		$stream = Streams::fetchOne($publisherId, $publisherId, $streamName);
+		$stream->setAttribute('available', true);
+		$stream->changed();
+	}
 	return Q::take($info, array("status", "duration", "width", "height", "type"));
 }
