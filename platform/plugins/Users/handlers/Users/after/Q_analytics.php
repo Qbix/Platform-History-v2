8ad6477760f8d4cd1633e78c3634b06eab3e9f1d
Greg Magarshak <greg@qbix.com>
2023-12-27 05:07:29 -0500

    Turned off analytics by default

--- platform/plugins/Users/handlers/Users/after/Q_analytics.php
+++ platform/plugins/Users/handlers/Users/after/Q_analytics.php
@@ -4,8 +4,10 @@ function Users_after_Q_analytics()
 {
 	$url = Q_Request::tail();
 
-	if ((string)Q_Dispatcher::uri() != 'Users/logout') {
-		Users_Vote::saveActivity("app", "visited");
-		Users_Vote::saveActivity("page", substr($url, 0, 120));
+	if (Q_Config::get('Q', 'analytics', 'gather', false)) {
+		if ((string)Q_Dispatcher::uri() != 'Users/logout') {
+			Users_Vote::saveActivity("app", "visited");
+			Users_Vote::saveActivity("action", substr($url, 0, 120));
+		}
 	}
 }
