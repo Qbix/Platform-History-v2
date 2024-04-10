614445c44527edc942a9fd8741a45586d9c9a58f
Greg Magarshak <greg@qbix.com>
2024-04-10 14:12:31 -0400

    Improved handling emojis for delivering messages

--- platform/plugins/Streams/handlers/Streams/deliver/Streams/relatedTo.js
+++ platform/plugins/Streams/handlers/Streams/deliver/Streams/relatedTo.js
@@ -1,12 +1,19 @@
 module.exports = function (options, callback) {
 	var Q = require('Q');
 	var p, f = options.fields;
-	options.fields.streamTitleEmojiPrefix = Q.Config.get([
+	f.streamTitleEmojiPrefix = Q.Config.get([
 		'Streams', 'types', f.stream.fields.type, 'emoji'
 	], null) || '';
-	var p = options.fields.fromTitleEmojiPrefix = Q.Config.get([
+	f.fromTitleEmojiPrefix = Q.Config.get([
 		'Streams', 'types', f.fromType, 'emoji'
 	], null) || '';
-	options.subject = p + this.getInstruction('description') || this.fields.content;
+	if (f.streamTitleEmojiPrefix) {
+		f.streamTitleEmojiPrefix += ' ';
+	}
+	if (f.fromTitleEmojiPrefix) {
+		f.fromTitleEmojiPrefix += ' ';
+	}
+	options.subject = f.fromTitleEmojiPrefix
+		+ (this.getInstruction('description') || this.fields.content);
 	callback();
 }
