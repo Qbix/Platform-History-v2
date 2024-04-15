0f8e4e27a91b41316c3396bbbbb3fdf878fcd86e
Greg Magarshak <greg@qbix.com>
2024-04-15 12:55:39 -0400

    Refactored service worker to only handle cache not fetch

--- platform/plugins/Q/handlers/Q/serviceWorker/response.php
+++ platform/plugins/Q/handlers/Q/serviceWorker/response.php
@@ -41,17 +41,15 @@ var Q = {
 				}
 			));
 		}
-		return event.respondWith(
-			caches.match(event.request)
-			.then(function (response) {
-				if (response) {
-					console.log('cached: ' + event.request.url);
-					return response;
-				}
-				// otherwise, attach some headers
-				return fetch(event.request);
-			})
-		);
+		return caches.match(event.request)
+		.then(function (response) {
+			if (response) {
+				console.log('cached: ' + event.request.url);
+				return event.respondWith(response);
+			}
+			// otherwise, attach some headers
+			// return event.respondWith(fetch(event.request));
+		})
 	});
 	self.addEventListener("install", (event) => {
 		self.skipWaiting();
