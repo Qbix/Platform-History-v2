05c5945be151795f3c7bce579c2565e287471605
Greg Magarshak <greg@qbix.com>
2024-04-21 03:53:32 -0400

    Made backward compatibility better

--- platform/plugins/Users/handlers/Users/after/Q_Plugin_install.php
+++ platform/plugins/Users/handlers/Users/after/Q_Plugin_install.php
@@ -12,10 +12,25 @@ function Users_after_Q_Plugin_install($params)
 	
 	// for backwards compatibility
 	if (Q::isAssociative($labelsToInstall)) {
-		$labelsToInstall = array_keys($labelsToInstall);
+		$arr = array();
+		foreach ($labelsToInstall as $k => $v) {
+			if (is_integer($k)) {
+				$arr[] = $v;
+			} else {
+				$arr[] = $k;
+			}
+		}
+		$labelsToInstall = $arr;
 	}
 	if (Q::isAssociative($rolesToInstall)) {
-		$rolesToInstall = array_keys($rolesToInstall);
+		foreach ($rolesToInstall as $k => $v) {
+			if (is_integer($k)) {
+				$arr[] = $v;
+			} else {
+				$arr[] = $k;
+			}
+		}
+		$rolesToInstall = $arr;
 	}
 
 	// get labels already installed
@@ -72,116 +87,4 @@ function Users_after_Q_Plugin_install($params)
 	    $extra[$key] = array_values(array_unique(array_merge($extra[$key], $rolesToInstall)));
 	    return Q_Plugin::extra('Users', 'plugin', 'Users', @compact('extra'));
 	}
-
-
-    
-
-
-
-
-
-
-
-
-    	$plugin_name = $params['plugin_name'];
-	$result = Users_User::select('COUNT(1)')
-		->fetchAll(PDO::FETCH_NUM);
-	$c = $result[0][0];
-
-	echo "$plugin_name: inserting streams for users...";
-
-	// get stream names need to install
-	$onInsert = Q_Config::get('Streams', 'onInsert', array());
-	$streamsToInstall = array_merge(
-		Q::ifset($onInsert, 'user', array()),
-		Q::ifset($onInsert, 'person', array()),
-		Q::ifset($onInsert, 'community', array())
-	);
-	// get stream names already installed
-    $key = "Streams/User/onInsert";
-    $extra = Q_Plugin::extra('Streams', 'plugin', 'Streams');
-    $extra[$key] = Q::ifset($extra, $key, array());
-    $streamsInstalled = $extra[$key];
-
-	$streamsNeedToInstall = array();
-	foreach ($streamsToInstall as $streamToInstall) {
-		if (!in_array($streamToInstall, $streamsInstalled)) {
-			$streamsNeedToInstall[] = $streamToInstall;
-		}
-	}
-
-	// if all streams already inserted - exit
-	if (!count($streamsNeedToInstall)) {
-		echo "  already inserted".PHP_EOL;
-		return;
-	}
-
-	echo "\n  " . implode("\n  ", $streamsNeedToInstall) . "\n";
-	
-	echo PHP_EOL;
-
-	$offset = 0;
-	$batch = 1000;
-	for ($i=1; true; ++$i) {
-		$users = Users_User::select()
-			->orderBy('id')
-			->limit($batch, $offset)
-			->fetchDbRows();
-		if (!$users) {
-			break;
-		}
-		foreach ($users as $j => $user) {
-			$simulated = array(
-				'row' => $user,
-				'inserted' => true,
-				'modifiedFields' => $user->fields,
-				'query' => null
-			);
-			$user->set('Streams', 'skipExistingOnInsert', true);
-			Q::event('Db/Row/Users_User/saveExecute', $simulated, 'after');
-			echo "\033[100D";
-			echo "$plugin_name: processed streams for ".($j + $offset + 1)." of $c users"
-				. str_repeat(' ', 20);
-			gc_collect_cycles();
-		}
-		$offset += $batch;
-	}
-	
-	echo PHP_EOL;
-
-	// if new streams installed
-	if (count($streamsToInstall)) {
-		// save installed streams to table [plugin_name]_q_plugin extra field
-	    $extra = Q_Plugin::extra('Streams', 'plugin', 'Streams');
-	    $extra[$key] = isset($extra[$key]) && is_array($extra[$key]) ? $extra[$key] : array();
-	    $extra[$key] = array_values(array_unique(array_merge($extra[$key], $streamsToInstall)));
-	    return Q_Plugin::extra('Streams', 'plugin', 'Streams', @compact('extra'));
-	}
-
-
-
-
-
-
-
-
-
-
-    echo "Adding labels for users...";
-	$users = Users_User::select()->fetchDbRows();
-	$c = count($users);
-	$i = 0;
-	$labels = Q_Config::get('Users', 'onInsert', 'labels', array());
-	foreach ($users as $user) {
-		// skip labels for community
-		if (Users::isCommunityId($user->id)) {
-			continue;
-		}
-
-		Users_Label::addLabel($labels, $user->id, null, null, $user->id);
-		++$i;
-		echo "\033[100D";
-		echo "Added labels for $i of $c users";
-	}
-	echo "\n";
 }
