17fa7f1be769a7963836d80bf6ac9a72f51aa35a
Andrey Tepaykin <andrey@qbix.com>
2024-05-10 18:06:01 +0300

    Instead hardcoded image size use Q.largestSize method to find lagest size by stream type.

--- platform/plugins/Streams/web/js/methods/Streams/Tool/preloadByStream.js
+++ platform/plugins/Streams/web/js/methods/Streams/Tool/preloadByStream.js
@@ -28,7 +28,7 @@ Q.exports(function() {
 			var fields = Q.extend({}, stream.getAllAttributes(), {
 				publisherId: stream.fields.publisherId,
 				streamName: stream.fields.name,
-				url: stream.fileUrl() || stream.iconUrl('1000x')
+				url: stream.fileUrl() || stream.iconUrl(Q.largestSize(Q.image.sizes[streamType]))
 			});
 
 			Q.Streams.Tool.preload(toolName, fields, container);
