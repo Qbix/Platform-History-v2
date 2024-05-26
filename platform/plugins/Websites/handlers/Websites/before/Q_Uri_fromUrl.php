9e148fc06ddfeabe95b7ce36cf68fd0a30d9fc6e
Greg Magarshak <greg@qbix.com>
2024-05-26 04:39:50 -0400

    Skipping certain URLs for permalinks

--- platform/plugins/Websites/handlers/Websites/before/Q_Uri_fromUrl.php
+++ platform/plugins/Websites/handlers/Websites/before/Q_Uri_fromUrl.php
@@ -3,8 +3,9 @@
 function Websites_before_Q_Uri_fromUrl($params, &$result)
 {
 	$enabled = Q_Config::get('Websites', 'permalinks', 'regex', false);
-	if (!$enabled || !Q::$bootstrapped) {
-		return; // we probably haven't even loaded the database configuration yet.
+	if (!$enabled || !Q::$bootstrapped
+	|| $params['url'] === Q_Request::serviceWorkerURL()) {
+		return;
 	}
 	if ($regex = Q_Config::get('Websites', 'permalinks', 'regex', null)) {
 		$baseUrl = Q_Request::baseUrl();
