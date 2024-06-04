ff1e4520fbe20c866edc570a6c25da964c56fd40
Greg Magarshak <greg@qbix.com>
2024-06-04 14:48:04 -0400

    Updated file.php in MyApp to tell shared caches not to store

--- MyApp/web/file.php
+++ MyApp/web/file.php
@@ -10,6 +10,7 @@ if ($can) {
 	$mimeType = mime_content_type($filename);
         header("Content-Type: $mimeType");
         header("X-Accel-Redirect: $uri");
+        header("Cache-Control: no-cache, s-maxage=0");
 } else {
 	Q_Text::setLanguageFromRequest();
         throw new Users_Exception_NotAuthorized(compact('filename'));
