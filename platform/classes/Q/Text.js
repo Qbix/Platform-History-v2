406d6ad65af323fa3420088373236ca277a475b1
Greg Magarshak <greg@qbix.com>
2024-07-15 16:06:57 -0400

    Fixed a typo in Q.Text.get() recent refactor

--- platform/classes/Q/Text.js
+++ platform/classes/Q/Text.js
@@ -102,7 +102,7 @@ Text.get = function (name, options) {
 		}
 		var tree = new Q.Tree();
 		name.filter(onlyUnique).forEach(function (item) {
-			tree.merge(Text.get(name, options));
+			tree.merge(Text.get(item, options));
 		});
 		return tree.getAll();
 	}
