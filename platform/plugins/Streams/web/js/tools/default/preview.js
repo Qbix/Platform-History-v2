7849b6d5a76d63584eb31655e323579467b760d7
Greg Magarshak <greg@qbix.com>
2024-07-21 04:08:46 -0400

    Made default preview use String.prototype.encodeHTML instead of handlebars encode

--- platform/plugins/Streams/web/js/tools/default/preview.js
+++ platform/plugins/Streams/web/js/tools/default/preview.js
@@ -99,6 +99,7 @@ function _Streams_default_preview(options, preview) {
 		var fields = Q.extend({}, state.templates[mode].fields, f, {
 			alt: 'icon',
 			title: stream.fields.title,
+			title: stream.fields.title.encodeHTML(),
 			inplace: inplace
 		});
 		var tpl = (ps.editable !== false && editable) ? 'edit' : 'view';
@@ -147,7 +148,7 @@ Q.Template.set('Streams/default/preview/view',
 	'<div class="Streams_preview_container Streams_preview_view Q_clearfix">'
 	+ '<img alt="{{alt}}" class="Streams_preview_icon Q_square" src="">'
 	+ '<div class="Streams_preview_contents {{titleClass}}">'
-	+ '<{{titleTag}} class="Streams_preview_title Streams_preview_view">{{title}}</{{titleTag}}>'
+	+ '<{{titleTag}} class="Streams_preview_title Streams_preview_view">{{{titleEncoded}}}</{{titleTag}}>'
 	+ '</div></div>'
 );
 
