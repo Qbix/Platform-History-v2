f5662b58fbd5a33cc89697b31887182440f76b84
Greg Magarshak <greg@qbix.com>
2024-08-29 17:41:26 -0400

    Guarding against payload not being present in Q_Session::generateId()

--- platform/plugins/Users/handlers/Users/before/Q_session_generateId.php
+++ platform/plugins/Users/handlers/Users/before/Q_session_generateId.php
@@ -3,7 +3,8 @@
 function Users_before_Q_session_generateId($params, &$result)
 {
     $payload = $_REQUEST;
-    if ($publicKey = Users::verify($params['payload'], false)) {
+    if (isset($params['payload'])
+    and $publicKey = Users::verify($params['payload'], false)) {
         // valid payload and public key provided
         $result = $publicKey;
     }
